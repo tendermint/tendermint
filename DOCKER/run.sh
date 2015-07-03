812c5577e090fe7e3be67d9e57547d1afa63ab70
Ethan Buchman <ethan@coinculture.info>
2015-07-03 19:06:50 +0000

    fix docker fast sync bug

--- DOCKER/run.sh
+++ DOCKER/run.sh
@@ -4,7 +4,7 @@ if [[ $BARAK_SEED ]]; then
 	cat ./cmd/barak/$BARAK_SEED | ./build/barak &
 fi
 
-if [[ $FAST_SYNC ]]; then
+if [ "$FAST_SYNC" = "true" ]; then
 	./build/tendermint node --fast_sync
 else 
 	./build/tendermint node
