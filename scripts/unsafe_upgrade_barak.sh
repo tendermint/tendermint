e0c79475447c110d9f7a1b7d03c79466f7916b9f
Jae Kwon <jae@tendermint.com>
2015-07-08 12:58:38 -0700

    upgrade_barak script, still needs more work

---
+++ scripts/unsafe_upgrade_barak.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+debora open "[::]:46661"
+debora --group default.upgrade list # TODO replace with command to test with
+echo "Will shut down barak default port..."
+sleep 10
+debora --group default.upgrade close "[::]:46660"
+debora --group default.upgrade run -- bash -c "cd \$GOPATH/src/github.com/tendermint/tendermint; git pull origin develop; make"
+debora --group default.upgrade run -- bash -c "cd \$GOPATH/src/github.com/tendermint/tendermint; mkdir -p ~/.barak/logs"
+debora --group default.upgrade run --bg --label barak -- bash -c "cd \$GOPATH/src/github.com/tendermint/tendermint; barak --options-file=cmd/barak/seed0 | filelogger -outpath ~/.barak/logs/barak.log"
+echo "Testing new barak..."
+debora list
+sleep 10
+echo "Will shut down old barak..."
+debora --group default.upgrade quit
