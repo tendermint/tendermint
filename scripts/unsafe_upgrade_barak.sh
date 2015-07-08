e9ece359a968f5acc6a99f01f85dd60da4e0e261
Jae Kwon <jae@tendermint.com>
2015-07-08 14:49:56 -0700

    script output formatting

--- scripts/unsafe_upgrade_barak.sh
+++ scripts/unsafe_upgrade_barak.sh
@@ -4,15 +4,16 @@ IFS=$'\n\t'
 
 debora open "[::]:46661"
 debora --group default.upgrade status
-printf "\n\nWill shut down barak default port..."
+printf "\n\nShutting down barak default port...\n\n"
 sleep 3
 debora --group default.upgrade close "[::]:46660"
 debora --group default.upgrade run -- bash -c "cd \$GOPATH/src/github.com/tendermint/tendermint; git pull origin develop; make"
 debora --group default.upgrade run -- bash -c "cd \$GOPATH/src/github.com/tendermint/tendermint; mkdir -p ~/.barak/logs"
 debora --group default.upgrade run --bg --label barak -- bash -c "cd \$GOPATH/src/github.com/tendermint/tendermint; barak --config=cmd/barak/seed | stdinwriter -outpath ~/.barak/logs/barak.log"
-printf "\n\nTesting new barak..."
-debora status
+printf "\n\nTesting new barak...\n\n"
 sleep 3
-printf "\n\nWill shut down old barak..."
+debora status
+printf "\n\nShutting down old barak...\n\n"
+sleep 3
 debora --group default.upgrade quit
 printf "Done!"
