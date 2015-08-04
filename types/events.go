60310cc23f5a51d510efd19fe47c9f0f2c2a8e9b
Jae Kwon <jae@tendermint.com>
2015-08-04 13:15:10 -0700

    websocket server sends pings; added ws_client; events refactor

--- types/events.go
+++ types/events.go
@@ -6,58 +6,22 @@ import (
 
 // Functions to generate eventId strings
 
-func EventStringAccInput(addr []byte) string {
-	return fmt.Sprintf("Acc/%X/Input", addr)
-}
-
-func EventStringAccOutput(addr []byte) string {
-	return fmt.Sprintf("Acc/%X/Output", addr)
-}
-
-func EventStringAccCall(addr []byte) string {
-	return fmt.Sprintf("Acc/%X/Call", addr)
-}
-
-func EventStringLogEvent(addr []byte) string {
-	return fmt.Sprintf("Log/%X", addr)
-}
-
-func EventStringPermissions(name string) string {
-	return fmt.Sprintf("Permissions/%s", name)
-}
-
-func EventStringNameReg(name string) string {
-	return fmt.Sprintf("NameReg/%s", name)
-}
-
-func EventStringBond() string {
-	return "Bond"
-}
-
-func EventStringUnbond() string {
-	return "Unbond"
-}
-
-func EventStringRebond() string {
-	return "Rebond"
-}
-
-func EventStringDupeout() string {
-	return "Dupeout"
-}
-
-func EventStringNewBlock() string {
-	return "NewBlock"
-}
-
-func EventStringFork() string {
-	return "Fork"
-}
+func EventStringAccInput(addr []byte) string    { return fmt.Sprintf("Acc/%X/Input", addr) }
+func EventStringAccOutput(addr []byte) string   { return fmt.Sprintf("Acc/%X/Output", addr) }
+func EventStringAccCall(addr []byte) string     { return fmt.Sprintf("Acc/%X/Call", addr) }
+func EventStringLogEvent(addr []byte) string    { return fmt.Sprintf("Log/%X", addr) }
+func EventStringPermissions(name string) string { return fmt.Sprintf("Permissions/%s", name) }
+func EventStringBond() string                   { return "Bond" }
+func EventStringUnbond() string                 { return "Unbond" }
+func EventStringRebond() string                 { return "Rebond" }
+func EventStringDupeout() string                { return "Dupeout" }
+func EventStringNewBlock() string               { return "NewBlock" }
+func EventStringFork() string                   { return "Fork" }
 
 // Most event messages are basic types (a block, a transaction)
 // but some (an input to a call tx or a receive) are more exotic:
 
-type EventMsgCallTx struct {
+type EventMsgTx struct {
 	Tx        Tx     `json:"tx"`
 	Return    []byte `json:"return"`
 	Exception string `json:"exception"`
