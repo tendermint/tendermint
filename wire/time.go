c6e59485349732d1d34b53e128555f89744b5a6b
Jae Kwon <jae@tendermint.com>
2015-08-31 13:00:20 -0700

    time writes milliseconds using ISO8601 format

--- wire/time.go
+++ wire/time.go
@@ -3,15 +3,25 @@ package wire
 import (
 	"io"
 	"time"
+
+	. "github.com/tendermint/tendermint/common"
 )
 
-// Time
+/*
+Writes nanoseconds since epoch but with millisecond precision.
+This is to ease compatibility with Javascript etc.
+*/
 
 func WriteTime(t time.Time, w io.Writer, n *int64, err *error) {
-	WriteInt64(t.UnixNano(), w, n, err)
+	nanosecs := t.UnixNano()
+	millisecs := nanosecs / 1000000
+	WriteInt64(millisecs*1000000, w, n, err)
 }
 
 func ReadTime(r io.Reader, n *int64, err *error) time.Time {
 	t := ReadInt64(r, n, err)
+	if t%1000000 != 0 {
+		PanicSanity("Time cannot have sub-millisecond precision")
+	}
 	return time.Unix(0, t)
 }
