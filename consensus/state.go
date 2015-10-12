0a28cabd57c826a2cb875f4dc99639f923863e4e
Jae Kwon <jae@tendermint.com>
2015-10-12 09:42:53 -0700

    remove temporary limit on num block transactions

--- consensus/state.go
+++ consensus/state.go
@@ -657,10 +657,6 @@ func (cs *ConsensusState) createProposalBlock() (block *types.Block, blockParts 
 		return
 	}
 	txs := cs.mempoolReactor.Mempool.GetProposalTxs()
-	MaxTxsPerBlock := 100 // TODO
-	if len(txs) > MaxTxsPerBlock {
-		txs = txs[:MaxTxsPerBlock]
-	}
 	block = &types.Block{
 		Header: &types.Header{
 			ChainID:        cs.state.ChainID,
