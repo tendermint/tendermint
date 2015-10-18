cc6e8453644a18f4d65118a2df57aed4a3212aea
Jae Kwon <jae@tendermint.com>
2015-10-18 08:55:53 -0700

    go get -d for DOCKER

--- DOCKER/run.sh
+++ DOCKER/run.sh
@@ -4,6 +4,6 @@ mkdir -p $GOPATH/src/$TMREPO
 git clone https://$TMREPO.git .
 git fetch
 git reset --hard $TMHEAD
-go get $TMREPO/cmd/tendermint
+go get -d $TMREPO/cmd/tendermint
 make
 tendermint node
