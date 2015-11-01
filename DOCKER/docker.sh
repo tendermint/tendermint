1e5c66486124bd587b53e6a831ac6a7d7f3b056c
ebuchman <ethan@coinculture.info>
2015-11-01 14:44:22 +0200

    fix docker.sh --name
    fixes #162

--- DOCKER/docker.sh
+++ DOCKER/docker.sh
@@ -14,7 +14,7 @@ docker run --name tmdata --entrypoint /bin/echo tmbase Data-only container for t
 #   tar cf - . | docker run -i --rm --volumes-from mintdata mint tar xvf - -C /data/tendermint
 
 # Run tendermint node
-docker run name tmnode --volumes-from tmdata -d -p 46656:46656 -p 46657:46657 -e TMREPO="github.com/tendermint/tendermint" -e TMHEAD="origin/develop" tmbase
+docker run --name tmnode --volumes-from tmdata -d -p 46656:46656 -p 46657:46657 -e TMREPO="github.com/tendermint/tendermint" -e TMHEAD="origin/develop" tmbase
 
 # Cleanup
 #   docker rm -v -f tmdata tmnode; docker rmi -f tmbase
