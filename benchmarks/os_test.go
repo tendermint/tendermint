5bf77960047966936f96eb0257ec61a2de2ff153
Jae Kwon <jae@tendermint.com>
2015-12-20 11:37:54 -0800

    Add synchronous file write benchmark

---
+++ benchmarks/os_test.go
@@ -0,0 +1,29 @@
+package benchmarks
+
+import (
+	"os"
+	"testing"
+
+	. "github.com/tendermint/go-common"
+)
+
+func BenchmarkFileWrite(b *testing.B) {
+	b.StopTimer()
+	file, err := os.OpenFile("benchmark_file_write.out",
+		os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+	if err != nil {
+		b.Error(err)
+	}
+	testString := RandStr(200) + "\n"
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		file.Write([]byte(testString))
+	}
+
+	file.Close()
+	err = os.Remove("benchmark_file_write.out")
+	if err != nil {
+		b.Error(err)
+	}
+}
