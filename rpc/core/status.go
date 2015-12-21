6f5baa84140107dc51a9e05cc05cff149c1c4408
Ethan Buchman <ethan@coinculture.info>
2015-12-21 18:13:42 -0500

    rpc: add app_hash to /status

---
+++ rpc/core/status.go
@@ -0,0 +1,30 @@
+package core
+
+import (
+	ctypes "github.com/tendermint/tendermint/rpc/core/types"
+	"github.com/tendermint/tendermint/types"
+)
+
+func Status() (*ctypes.ResultStatus, error) {
+	latestHeight := blockStore.Height()
+	var (
+		latestBlockMeta *types.BlockMeta
+		latestBlockHash []byte
+		latestAppHash   []byte
+		latestBlockTime int64
+	)
+	if latestHeight != 0 {
+		latestBlockMeta = blockStore.LoadBlockMeta(latestHeight)
+		latestBlockHash = latestBlockMeta.Hash
+		latestAppHash = latestBlockMeta.Header.AppHash
+		latestBlockTime = latestBlockMeta.Header.Time.UnixNano()
+	}
+
+	return &ctypes.ResultStatus{
+		NodeInfo:          p2pSwitch.NodeInfo(),
+		PubKey:            privValidator.PubKey,
+		LatestBlockHash:   latestBlockHash,
+		LatestAppHash:     latestAppHash,
+		LatestBlockHeight: latestHeight,
+		LatestBlockTime:   latestBlockTime}, nil
+}
