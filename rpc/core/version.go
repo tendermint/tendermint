77a66f079fb43ad58a282a4b714279c84eaf88fb
Ethan Buchman <ethan@coinculture.info>
2016-01-20 16:25:26 -0500

    version folder, rpc core version

---
+++ rpc/core/version.go
@@ -0,0 +1,5 @@
+package core
+
+// a single integer is sufficient here
+
+const Version = "2" // add DialSeeds; re-organize type bytes
---
+++ version/version.go
@@ -0,0 +1,7 @@
+package version
+
+const Maj = "0"
+const Min = "6" // tmsp refactor
+const Fix = "0"
+
+const Version = Maj + "." + Min + "." + Fix
