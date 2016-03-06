1bc871162db7206c7ee0a775166dfe4c0705168b
Jae Kwon <jae@tendermint.com>
2016-03-06 12:33:07 -0800

    Conform to new go-config behavior; ApplyConfig not needed

--- config.go
+++ config.go
@@ -4,17 +4,14 @@ import (
 	cfg "github.com/tendermint/go-config"
 )
 
-// XXX: go-p2p requires ApplyConfig be called
 var config cfg.Config = nil
 
 func init() {
 	initConfigureable(dialTimeoutKey, 3)
 	initConfigureable(handshakeTimeoutKey, 20)
 	initConfigureable(maxNumPeersKey, 50)
-
 	initConfigureable(sendRateKey, 512000) // 500KB/s
 	initConfigureable(recvRateKey, 512000) // 500KB/s
-
 	initConfigureable(maxPayloadSizeKey, 1024)
 
 	cfg.OnConfig(func(newConfig cfg.Config) {
@@ -22,19 +19,14 @@ func init() {
 
 		// fill in any config values that might be missing
 		for key, value := range defaultConfigValues {
-			if !config.IsSet(key) {
-				config.Set(key, value)
-			}
+			config.SetDefault(key, value)
 		}
 	})
-	c := cfg.NewMapConfig(nil)
-	c.Set("log_level", "debug")
-	cfg.ApplyConfig(c)
 }
 
 // default config map
-var defaultConfigValues = make(map[string]int)
+var defaultConfigValues = make(map[string]interface{})
 
-func initConfigureable(key string, value int) {
+func initConfigureable(key string, value interface{}) {
 	defaultConfigValues[key] = value
 }
