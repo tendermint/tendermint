c7e578ac0d17d76bdf6a05ae4a1c46106d055fba
Ethan Buchman <ethan@coinculture.info>
2016-09-11 16:02:29 -0400

    check tmsp client err and set mustConnect=false

--- version/version.go
+++ version/version.go
@@ -2,6 +2,6 @@ package version
 
 const Maj = "0"
 const Min = "7" // tmsp useability (protobuf, unix); optimizations; broadcast_tx_commit
-const Fix = "1" // query conn, peer filter, fast sync fix
+const Fix = "2" // query conn, peer filter, fast sync fix (+hot fix to tmsp connecting)
 
 const Version = Maj + "." + Min + "." + Fix
