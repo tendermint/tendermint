ead192adbbbf85ac581cf775b18ae70d59f86457
Ethan Buchman <ethan@coinculture.info>
2016-09-10 20:10:59 -0400

    test: more verbosity

--- tests/test_cli/test.sh
+++ tests/test_cli/test.sh
@@ -9,13 +9,17 @@ function testExample() {
 	$APP &> /dev/null &
 	sleep 2
 	tmsp-cli --verbose batch < $INPUT > "${INPUT}.out.new"
-	killall "$APP" &> /dev/null
+	killall "$APP" 
 
 	pre=`shasum < "${INPUT}.out"`
 	post=`shasum < "${INPUT}.out.new"`
 
 	if [[ "$pre" != "$post" ]]; then
 		echo "You broke the tutorial"
+		echo "Got:" 
+		cat "${INPUT}.out.new"
+		echo "Expected:" 
+		cat "${INPUT}.out"
 		exit 1
 	fi
 
