1b37c8affdb06004ab0b185a67ccad710482ab12
Ethan Buchman <ethan@coinculture.info>
2016-10-12 00:24:24 -0400

    version: bump 0.7.3

--- version/version.go
+++ version/version.go
@@ -2,6 +2,6 @@ package version
 
 const Maj = "0"
 const Min = "7" // tmsp useability (protobuf, unix); optimizations; broadcast_tx_commit
-const Fix = "2" // query conn, peer filter, fast sync fix (+hot fix to tmsp connecting)
+const Fix = "3" // fixes to event safety, mempool deadlock, hvs race, replay non-empty blocks
 
 const Version = Maj + "." + Min + "." + Fix
