0b098a2eeef7e902fc6281a9bb0e7f0e7c9f57b3
Ethan Buchman <ethan@coinculture.info>
2016-11-15 18:05:38 -0500

    use glide in install_tmsp_apps.sh

--- scripts/install_tmsp_apps.sh
+++ scripts/install_tmsp_apps.sh
@@ -3,10 +3,11 @@
 go get github.com/tendermint/tmsp/...
 
 # get the tmsp commit used by tendermint
-COMMIT=`bash scripts/glide/parse.sh $(pwd)/glide.lock tmsp`
+COMMIT=`bash scripts/glide/parse.sh tmsp`
+
+echo "Checking out vendored commit for tmsp: $COMMIT"
 
 cd $GOPATH/src/github.com/tendermint/tmsp
 git checkout $COMMIT
+glide install
 go install ./cmd/...
-
-
