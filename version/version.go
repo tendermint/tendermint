74ff489e63429225bb84a0238e2de44b31d7502d
Ethan Buchman <ethan@coinculture.info>
2016-12-14 10:05:25 -0500

    shame: version bump 0.7.4

--- version/version.go
+++ version/version.go
@@ -2,6 +2,6 @@ package version
 
 const Maj = "0"
 const Min = "7" // tmsp useability (protobuf, unix); optimizations; broadcast_tx_commit
-const Fix = "3" // fixes to event safety, mempool deadlock, hvs race, replay non-empty blocks
+const Fix = "4" // --pex flag and less restricted /dial_seeds
 
 const Version = Maj + "." + Min + "." + Fix
