5eeafc161de0eabbf681986226074718c6e32d7e
Anton Kalyaev <anton.kalyaev@gmail.com>
2016-12-20 19:28:41 +0400

    rename COUNT to ID

--- test/p2p/fast_sync/test_peer.sh
+++ test/p2p/fast_sync/test_peer.sh
@@ -3,7 +3,7 @@ set -eu
 
 DOCKER_IMAGE=$1
 NETWORK_NAME=$2
-COUNT=$3
+ID=$3
 N=$4
 
 ###############################################################
@@ -14,11 +14,11 @@ N=$4
 ###############################################################
 
 
-echo "Testing fastsync on node $COUNT"
+echo "Testing fastsync on node $ID"
 
 # kill peer
 set +e # circle sigh :(
-docker rm -vf local_testnet_$COUNT
+docker rm -vf local_testnet_$ID
 set -e
 
 # restart peer - should have an empty blockchain
@@ -26,10 +26,10 @@ SEEDS="$(test/p2p/ip.sh 1):46656"
 for j in `seq 2 $N`; do
 	SEEDS="$SEEDS,$(test/p2p/ip.sh $j):46656"
 done
-bash test/p2p/peer.sh $DOCKER_IMAGE $NETWORK_NAME $COUNT $SEEDS
+bash test/p2p/peer.sh $DOCKER_IMAGE $NETWORK_NAME $ID $SEEDS
 
 # wait for peer to sync and check the app hash
-bash test/p2p/client.sh $DOCKER_IMAGE $NETWORK_NAME fs_$COUNT "test/p2p/fast_sync/check_peer.sh $COUNT"
+bash test/p2p/client.sh $DOCKER_IMAGE $NETWORK_NAME fs_$ID "test/p2p/fast_sync/check_peer.sh $ID"
 
 echo ""
 echo "PASS"
