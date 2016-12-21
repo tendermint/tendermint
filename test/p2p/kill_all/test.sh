f3166a32366c580264f17ba4117c39cacfeaeb34
Anton Kalyaev <anton.kalyaev@gmail.com>
2016-12-22 01:37:32 +0400

    start/stop container, no need for rm

--- test/p2p/kill_all/test.sh
+++ test/p2p/kill_all/test.sh
@@ -10,24 +10,15 @@ cd "$GOPATH/src/github.com/tendermint/tendermint"
 
 ###############################################################
 # NUM_OF_CRASHES times:
-# 	kill all peers
-# 	bring them back online
+# 	restart all peers
 # 	wait for them to sync and check that they are making progress
 ###############################################################
 
 for i in $(seq 1 "$NUM_OF_CRASHES"); do
-  # kill all peers
-  for i in $(seq 1 "$NUM_OF_PEERS"); do
-    docker rm -f "local_testnet_$i"
-  done
-
   # restart all peers
-  SEEDS="$(test/p2p/ip.sh 1):46656"
-  for j in $(seq 2 "$NUM_OF_PEERS"); do
-    SEEDS="$SEEDS,$(test/p2p/ip.sh "$j"):46656"
-  done
   for i in $(seq 1 "$NUM_OF_PEERS"); do
-    bash test/p2p/peer.sh "$DOCKER_IMAGE" "$NETWORK_NAME" "$i" "$SEEDS"
+    docker stop "local_testnet_$i"
+    docker start "local_testnet_$i"
   done
 
   bash test/p2p/client.sh "$DOCKER_IMAGE" "$NETWORK_NAME" kill_all "test/p2p/kill_all/check_peers.sh $NUM_OF_PEERS"
