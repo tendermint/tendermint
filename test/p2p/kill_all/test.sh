bd222d6e3c902345659cb366d88453a3761a8dbe
Ethan Buchman <ethan@coinculture.info>
2016-12-22 20:50:13 -0500

    test: more unique container names

--- test/p2p/kill_all/test.sh
+++ test/p2p/kill_all/test.sh
@@ -19,12 +19,12 @@ for i in $(seq 1 "$NUM_OF_CRASHES"); do
   echo "Restarting all peers! Take $i ..."
 
   # restart all peers
-  for i in $(seq 1 "$NUM_OF_PEERS"); do
-    docker stop "local_testnet_$i"
-    docker start "local_testnet_$i"
+  for j in $(seq 1 "$NUM_OF_PEERS"); do
+    docker stop "local_testnet_$j"
+    docker start "local_testnet_$j"
   done
 
-  bash test/p2p/client.sh "$DOCKER_IMAGE" "$NETWORK_NAME" kill_all "test/p2p/kill_all/check_peers.sh $NUM_OF_PEERS"
+  bash test/p2p/client.sh "$DOCKER_IMAGE" "$NETWORK_NAME" kill_all_$i "test/p2p/kill_all/check_peers.sh $NUM_OF_PEERS"
 done
 
 echo ""
