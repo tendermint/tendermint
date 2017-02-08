4896364952f5a24ef64525d1da1af082b184d3e0
Anton Kaliaev <anton.kalyaev@gmail.com>
2017-02-08 23:20:25 +0400

    [cli] cleanup gen_validator output (Fixes #396)
    so we can pipe result to a file:
    
    ```
    tendermint gen_validator > example.json
    ```
    
    before this we had to cut first 3 lines:
    
    ```
    tendermint gen_validator | sed 1,3d > example.json
    ```

--- cmd/tendermint/gen_validator.go
+++ cmd/tendermint/gen_validator.go
@@ -8,16 +8,8 @@ import (
 )
 
 func gen_validator() {
-
 	privValidator := types.GenPrivValidator()
 	privValidatorJSONBytes := wire.JSONBytesPretty(privValidator)
-	fmt.Printf(`Generated a new validator!
-Paste the following JSON into your %v file
-
-%v
-
-`,
-		config.GetString("priv_validator_file"),
-		string(privValidatorJSONBytes),
-	)
+	fmt.Printf(`%v
+`, string(privValidatorJSONBytes))
 }
