1a42f946dc6bcd88f9f58c7f2fb86f785584d793
Ethan Buchman <ethan@coinculture.info>
2017-04-19 00:05:18 -0400

    version bump

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package rpc
 
 const Maj = "0"
-const Min = "6" // 0x-prefixed string args handled as hex
-const Fix = "0" //
+const Min = "7"
+const Fix = "0"
 
 const Version = Maj + "." + Min + "." + Fix
