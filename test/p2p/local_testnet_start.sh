fb9d3842e4f460115bbdb261e5bb1c9b8c675cb8
Ethan Buchman <ethan@coinculture.info>
2017-05-05 01:28:43 -0400

    test: p2p.seeds and p2p.pex

--- test/p2p/local_testnet_start.sh
+++ test/p2p/local_testnet_start.sh
@@ -10,7 +10,7 @@ set +u
 SEEDS=$5
 if [[ "$SEEDS" != "" ]]; then
 	echo "Seeds: $SEEDS"
-	SEEDS="--seeds $SEEDS"
+	SEEDS="--p2p.seeds $SEEDS"
 fi
 set -u
 
@@ -20,5 +20,5 @@ cd "$GOPATH/src/github.com/tendermint/tendermint"
 docker network create --driver bridge --subnet 172.57.0.0/16 "$NETWORK_NAME"
 
 for i in $(seq 1 "$N"); do
-	bash test/p2p/peer.sh "$DOCKER_IMAGE" "$NETWORK_NAME" "$i" "$APP_PROXY" "$SEEDS --pex"
+	bash test/p2p/peer.sh "$DOCKER_IMAGE" "$NETWORK_NAME" "$i" "$APP_PROXY" "$SEEDS --p2p.pex"
 done
