eaec0c8ea51ae7304fcce75e1c7431d0d0881806
Anton Kaliaev <anton.kalyaev@gmail.com>
2017-05-26 13:24:32 +0200

    deduplicate tests in rpc/test and rpc/client (Refs #496)

--- rpc/client/event_test.go
+++ rpc/client/event_test.go
@@ -31,7 +31,39 @@ func TestHeaderEvents(t *testing.T) {
 	}
 }
 
-func TestTxEvents(t *testing.T) {
+func TestBlockEvents(t *testing.T) {
+	require := require.New(t)
+	for i, c := range GetClients() {
+		// start for this test it if it wasn't already running
+		if !c.IsRunning() {
+			// if so, then we start it, listen, and stop it.
+			st, err := c.Start()
+			require.Nil(err, "%d: %+v", i, err)
+			require.True(st, "%d", i)
+			defer c.Stop()
+		}
+
+		// listen for a new block; ensure height increases by 1
+		var firstBlockHeight int
+		for i := 0; i < 3; i++ {
+			evtTyp := types.EventStringNewBlock()
+			evt, err := client.WaitForOneEvent(c, evtTyp, 1*time.Second)
+			require.Nil(err, "%d: %+v", i, err)
+			blockEvent, ok := evt.Unwrap().(types.EventDataNewBlock)
+			require.True(ok, "%d: %#v", i, evt)
+
+			block := blockEvent.Block
+			if i == 0 {
+				firstBlockHeight = block.Header.Height
+				continue
+			}
+
+			require.Equal(block.Header.Height, firstBlockHeight+i)
+		}
+	}
+}
+
+func TestTxEventsSentWithBroadcastTxAsync(t *testing.T) {
 	require := require.New(t)
 	for i, c := range GetClients() {
 		// start for this test it if it wasn't already running
@@ -49,6 +81,39 @@ func TestTxEvents(t *testing.T) {
 
 		// send async
 		txres, err := c.BroadcastTxAsync(tx)
+		require.Nil(err, "%+v", err)
+		require.True(txres.Code.IsOK())
+
+		// and wait for confirmation
+		evt, err := client.WaitForOneEvent(c, evtTyp, 1*time.Second)
+		require.Nil(err, "%d: %+v", i, err)
+		// and make sure it has the proper info
+		txe, ok := evt.Unwrap().(types.EventDataTx)
+		require.True(ok, "%d: %#v", i, evt)
+		// make sure this is the proper tx
+		require.EqualValues(tx, txe.Tx)
+		require.True(txe.Code.IsOK())
+	}
+}
+
+func TestTxEventsSentWithBroadcastTxSync(t *testing.T) {
+	require := require.New(t)
+	for i, c := range GetClients() {
+		// start for this test it if it wasn't already running
+		if !c.IsRunning() {
+			// if so, then we start it, listen, and stop it.
+			st, err := c.Start()
+			require.Nil(err, "%d: %+v", i, err)
+			require.True(st, "%d", i)
+			defer c.Stop()
+		}
+
+		// make the tx
+		_, _, tx := merktest.MakeTxKV()
+		evtTyp := types.EventStringTx(types.Tx(tx))
+
+		// send async
+		txres, err := c.BroadcastTxSync(tx)
 		require.Nil(err, "%+v", err)
 		require.True(txres.Code.IsOK())
 
