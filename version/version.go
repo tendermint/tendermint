630c6ef7b582910eaf87275adbd283f38b148e54
Ethan Buchman <ethan@coinculture.info>
2017-05-29 02:16:39 -0400

    bump version

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ const Fix = "0"
 
 var (
 	// The full version string
-	Version = "0.10.0-rc1"
+	Version = "0.10.0-rc2"
 
 	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
 	GitCommit string
