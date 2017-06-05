925f2b33504bc2f19932be1d474ade0c33619c96
rigel rozanski <rigel.rozanski@gmail.com>
2017-06-05 16:22:01 -0400

    golint corrections

--- common/string.go
+++ common/string.go
@@ -6,8 +6,10 @@ import (
 	"strings"
 )
 
+// Fmt shorthand, XXX DEPRECATED
 var Fmt = fmt.Sprintf
 
+// RightPadString adds spaces to the right of a string to make it length totalLength
 func RightPadString(s string, totalLength int) string {
 	remaining := totalLength - len(s)
 	if remaining > 0 {
@@ -16,6 +18,7 @@ func RightPadString(s string, totalLength int) string {
 	return s
 }
 
+// LeftPadString adds spaces to the left of a string to make it length totalLength
 func LeftPadString(s string, totalLength int) string {
 	remaining := totalLength - len(s)
 	if remaining > 0 {
@@ -24,7 +27,7 @@ func LeftPadString(s string, totalLength int) string {
 	return s
 }
 
-// Returns true for non-empty hex-string prefixed with "0x"
+// IsHex returns true for non-empty hex-string prefixed with "0x"
 func IsHex(s string) bool {
 	if len(s) > 2 && s[:2] == "0x" {
 		_, err := hex.DecodeString(s[2:])
@@ -36,6 +39,7 @@ func IsHex(s string) bool {
 	return false
 }
 
+// StripHex returns hex string without leading "0x"
 func StripHex(s string) string {
 	if IsHex(s) {
 		return s[2:]
