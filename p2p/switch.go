1c8f63082621fcd323f6f5ae1b383797ef5bfbe8
caojingqi <cjq1989@gmail.com>
2017-07-05 16:18:45 +0800

    Update switch.go
    sw.peers.List() will always be a empty list here

--- p2p/switch.go
+++ p2p/switch.go
@@ -166,10 +166,7 @@ func (sw *Switch) OnStart() error {
 			return err
 		}
 	}
-	// Start peers
-	for _, peer := range sw.peers.List() {
-		sw.startInitPeer(peer)
-	}
+	
 	// Start listeners
 	for _, listener := range sw.listeners {
 		go sw.listenerRoutine(listener)
