aa7d63f2ffd156ff6b46a5375a82266d015b3be2
Ethan Buchman <ethan@coinculture.info>
2017-09-05 16:46:56 -0400

    bump version to 0.10.4

--- version/version.go
+++ version/version.go
@@ -2,11 +2,11 @@ package version
 
 const Maj = "0"
 const Min = "10"
-const Fix = "3"
+const Fix = "4"
 
 var (
 	// The full version string
-	Version = "0.10.3"
+	Version = "0.10.4"
 
 	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
 	GitCommit string
