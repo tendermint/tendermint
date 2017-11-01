acc161f7ccd722b217cbd1dbc440f3babf8ad4d8
Emmanuel Odeke <emm.odeke@gmail.com>
2017-11-01 07:02:33 -0700

    version: bump to v0.10.5

--- version/version.go
+++ version/version.go
@@ -2,11 +2,11 @@ package version
 
 const Maj = "0"
 const Min = "10"
-const Fix = "4"
+const Fix = "5"
 
 var (
 	// The full version string
-	Version = "0.10.4"
+	Version = "0.10.5"
 
 	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
 	GitCommit string
