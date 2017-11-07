2d4ad0235660039b7a3167b6a108faf63d1ec0c9
Anton Kaliaev <anton.kalyaev@gmail.com>
2017-11-07 15:02:42 -0500

    prefer tickers to time.Sleep (Refs #790)

--- node/node_test.go
+++ node/node_test.go
@@ -20,19 +20,23 @@ func TestNodeStartStop(t *testing.T) {
 	n.Start()
 	t.Logf("Started node %v", n.sw.NodeInfo())
 
-	// Wait a bit to initialize
-	// TODO remove time.Sleep(), make asynchronous.
-	time.Sleep(time.Second * 2)
+	ticker := time.NewTicker(10 * time.Millisecond)
+	select {
+	case <-ticker.C:
+		if n.IsRunning() {
+			return
+		}
+	case <-time.After(5 * time.Second):
+		t.Fatal("timed out waiting for start")
+	}
 
-	ch := make(chan struct{}, 1)
 	go func() {
 		n.Stop()
-		ch <- struct{}{}
 	}()
-	ticker := time.NewTicker(time.Second * 5)
+
 	select {
-	case <-ch:
-	case <-ticker.C:
+	case <-n.Quit:
+	case <-time.After(5 * time.Second):
 		t.Fatal("timed out waiting for shutdown")
 	}
 }
