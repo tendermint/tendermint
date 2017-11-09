432a7276e24afd0620d76d43110d35d0604802b9
Anton Kaliaev <anton.kalyaev@gmail.com>
2017-11-09 15:06:43 -0500

    [test_integrations] enable logs from peers by default (Refs #829)

--- test/p2p/peer.sh
+++ test/p2p/peer.sh
@@ -14,27 +14,15 @@ set +eu
 
 echo "starting tendermint peer ID=$ID"
 # start tendermint container on the network
-if [[ "$CIRCLECI" == true ]]; then
-	set -u
-	docker run -d \
-	  --net="$NETWORK_NAME" \
-	  --ip=$(test/p2p/ip.sh $ID) \
-	  --name "local_testnet_$ID" \
-	  --entrypoint tendermint \
-	  -e TMHOME="/go/src/github.com/tendermint/tendermint/test/p2p/data/mach$ID/core" \
-		--log-driver=syslog \
-		--log-opt syslog-address=udp://127.0.0.1:5514 \
-		--log-opt syslog-facility=daemon \
-		--log-opt tag="{{.Name}}" \
-  	  "$DOCKER_IMAGE" node $NODE_FLAGS --log_level=debug --proxy_app="$APP_PROXY"
-else
-	set -u
-	docker run -d \
-	  --net="$NETWORK_NAME" \
-	  --ip=$(test/p2p/ip.sh $ID) \
-	  --name "local_testnet_$ID" \
-	  --entrypoint tendermint \
-	  -e TMHOME="/go/src/github.com/tendermint/tendermint/test/p2p/data/mach$ID/core" \
-  	  "$DOCKER_IMAGE" node $NODE_FLAGS --log_level=info --proxy_app="$APP_PROXY"
-fi
-
+set -u
+docker run -d \
+	--net="$NETWORK_NAME" \
+	--ip=$(test/p2p/ip.sh "$ID") \
+	--name "local_testnet_$ID" \
+	--entrypoint tendermint \
+	-e TMHOME="/go/src/github.com/tendermint/tendermint/test/p2p/data/mach$ID/core" \
+	--log-driver=syslog \
+	--log-opt syslog-address=udp://127.0.0.1:5514 \
+	--log-opt syslog-facility=daemon \
+	--log-opt tag="{{.Name}}" \
+		"$DOCKER_IMAGE" node "$NODE_FLAGS" --log_level=debug --proxy_app="$APP_PROXY"
