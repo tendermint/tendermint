91efacfabc086f14a9d4fe4f2d661a0e647915c2
Anton Kaliaev <anton.kalyaev@gmail.com>
2017-11-22 17:44:39 -0600

    remove types.Result

--- types/result.go
+++ types/result.go
@@ -6,107 +6,6 @@ import (
 	"github.com/tendermint/go-wire/data"
 )
 
-// Result is a common result object for ABCI calls.
-// CONTRACT: a zero Result is OK.
-// DEPRECATED: prefer raw types instead.
-type Result struct {
-	Code CodeType   `json:"code"`
-	Data data.Bytes `json:"data"`
-	Log  string     `json:"log"` // Can be non-deterministic
-	Tags []*KVPair  `json:"tags"`
-}
-
-func NewResult(code CodeType, data []byte, log string) Result {
-	return Result{
-		Code: code,
-		Data: data,
-		Log:  log,
-	}
-}
-
-func (res Result) IsOK() bool {
-	return res.Code == CodeType_OK
-}
-
-func (res Result) IsErr() bool {
-	return res.Code != CodeType_OK
-}
-
-func (res Result) IsSameCode(compare Result) bool {
-	return res.Code == compare.Code
-}
-
-func (res Result) Error() string {
-	return fmt.Sprintf("ABCI{code:%v, data:%X, log:%v}", res.Code, res.Data, res.Log)
-}
-
-func (res Result) String() string {
-	return fmt.Sprintf("ABCI{code:%v, data:%X, log:%v}", res.Code, res.Data, res.Log)
-}
-
-func (res Result) PrependLog(log string) Result {
-	return Result{
-		Code: res.Code,
-		Data: res.Data,
-		Log:  log + ";" + res.Log,
-	}
-}
-
-func (res Result) AppendLog(log string) Result {
-	return Result{
-		Code: res.Code,
-		Data: res.Data,
-		Log:  res.Log + ";" + log,
-	}
-}
-
-func (res Result) SetLog(log string) Result {
-	return Result{
-		Code: res.Code,
-		Data: res.Data,
-		Log:  log,
-	}
-}
-
-func (res Result) SetData(data []byte) Result {
-	return Result{
-		Code: res.Code,
-		Data: data,
-		Log:  res.Log,
-	}
-}
-
-//----------------------------------------
-
-// NOTE: if data == nil and log == "", same as zero Result.
-func NewResultOK(data []byte, log string) Result {
-	return Result{
-		Code: CodeType_OK,
-		Data: data,
-		Log:  log,
-	}
-}
-
-func NewError(code CodeType, log string) Result {
-	return Result{
-		Code: code,
-		Log:  log,
-	}
-}
-
-//----------------------------------------
-// Convenience methods for turning the
-// pb type into one using data.Bytes
-
-// Convert ResponseCheckTx to standard Result
-func (r *ResponseCheckTx) Result() Result {
-	return Result{
-		Code: r.Code,
-		Data: r.Data,
-		Log:  r.Log,
-	}
-}
-
 // IsErr returns true if Code is something other than OK.
 func (r ResponseCheckTx) IsErr() bool {
 	return r.Code != CodeType_OK
@@ -117,16 +16,6 @@ func (r ResponseCheckTx) Error() string {
 	return fmtError(r.Code, r.Log)
 }
 
-// Convert ResponseDeliverTx to standard Result
-func (r *ResponseDeliverTx) Result() Result {
-	return Result{
-		Code: r.Code,
-		Data: r.Data,
-		Log:  r.Log,
-		Tags: r.Tags,
-	}
-}
-
 // IsErr returns true if Code is something other than OK.
 func (r ResponseDeliverTx) IsErr() bool {
 	return r.Code != CodeType_OK
@@ -135,28 +24,6 @@ func (r ResponseDeliverTx) IsErr() bool {
 // Error implements error interface by formatting response as string.
 func (r ResponseDeliverTx) Error() string {
 	return fmtError(r.Code, r.Log)
-}
-
-type ResultQuery struct {
-	Code   CodeType   `json:"code"`
-	Index  int64      `json:"index"`
-	Key    data.Bytes `json:"key"`
-	Value  data.Bytes `json:"value"`
-	Proof  data.Bytes `json:"proof"`
-	Height uint64     `json:"height"`
-	Log    string     `json:"log"`
-}
-
-func (r *ResponseQuery) Result() *ResultQuery {
-	return &ResultQuery{
-		Code:   r.Code,
-		Index:  r.Index,
-		Key:    r.Key,
-		Value:  r.Value,
-		Proof:  r.Proof,
-		Height: r.Height,
-		Log:    r.Log,
-	}
 }
 
 // IsErr returns true if Code is something other than OK.
@@ -175,5 +42,30 @@ func fmtError(code CodeType, log string) string {
 		return fmt.Sprintf("%s (%v): %s", codeAsStr, code, log)
 	} else {
 		return fmt.Sprintf("Unknown error (%v): %s", code, log)
+	}
+}
+
+// ResultQuery is a wrapper around ResponseQuery using data.Bytes instead of
+// raw byte slices.
+type ResultQuery struct {
+	Code   CodeType   `json:"code"`
+	Index  int64      `json:"index"`
+	Key    data.Bytes `json:"key"`
+	Value  data.Bytes `json:"value"`
+	Proof  data.Bytes `json:"proof"`
+	Height uint64     `json:"height"`
+	Log    string     `json:"log"`
+}
+
+// Result converts response query to ResultQuery.
+func (r *ResponseQuery) Result() *ResultQuery {
+	return &ResultQuery{
+		Code:   r.Code,
+		Index:  r.Index,
+		Key:    r.Key,
+		Value:  r.Value,
+		Proof:  r.Proof,
+		Height: r.Height,
+		Log:    r.Log,
 	}
 }
