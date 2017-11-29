57fea1335a7bf898349bcbc9861a05b91625bf35
Ethan Buchman <ethan@coinculture.info>
2017-11-29 03:05:20 +0000

    Makefile and linter

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-
 set -e
-echo "" > coverage.txt
 
+# run the linter
+make metalinter_test
+
+# run the unit tests with coverage
+echo "" > coverage.txt
 for d in $(go list ./... | grep -v vendor); do
     go test -race -coverprofile=profile.out -covermode=atomic "$d"
     if [ -f profile.out ]; then
