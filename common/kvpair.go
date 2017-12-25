f2a8e95248ca42cf29885a44bc1362cf78d364fe
Jae Kwon <jae@tendermint.com>
2017-12-25 11:40:05 -0800

    Add KI64Pair(s)

--- common/kvpair.go
+++ common/kvpair.go
@@ -5,6 +5,9 @@ import (
 	"sort"
 )
 
+//----------------------------------------
+// KVPair
+
 type KVPair struct {
 	Key   Bytes
 	Value Bytes
@@ -28,3 +31,30 @@ func (kvs KVPairs) Less(i, j int) bool {
 }
 func (kvs KVPairs) Swap(i, j int) { kvs[i], kvs[j] = kvs[j], kvs[i] }
 func (kvs KVPairs) Sort()         { sort.Sort(kvs) }
+
+//----------------------------------------
+// KI64Pair
+
+type KI64Pair struct {
+	Key   Bytes
+	Value int64
+}
+
+type KI64Pairs []KI64Pair
+
+// Sorting
+func (kvs KI64Pairs) Len() int { return len(kvs) }
+func (kvs KI64Pairs) Less(i, j int) bool {
+	switch bytes.Compare(kvs[i].Key, kvs[j].Key) {
+	case -1:
+		return true
+	case 0:
+		return kvs[i].Value < kvs[j].Value
+	case 1:
+		return false
+	default:
+		panic("invalid comparison result")
+	}
+}
+func (kvs KI64Pairs) Swap(i, j int) { kvs[i], kvs[j] = kvs[j], kvs[i] }
+func (kvs KI64Pairs) Sort()         { sort.Sort(kvs) }
