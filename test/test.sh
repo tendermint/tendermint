3c2c14ab0d233d35be142281f47e94de7480df82
Zach Ramsay <zach@bluecollarcoding.ca>
2017-12-28 22:17:37 +0000

    script?

--- test/test.sh
+++ test/test.sh
@@ -30,31 +30,31 @@ if [[ "$SKIP_BUILD" == "" ]]; then
 	bash "$DIR/docker/build.sh"
 fi
 
-echo
-echo "* [$(date +"%T")] running go tests and app tests in docker container"
-# sometimes its helpful to mount the local test folder
-# -v $DIR:/go/src/github.com/tendermint/tendermint/test
-if [[ "$CIRCLECI" == true ]]; then
-	docker run --name run_test -e CIRCLECI=true -t tester bash test/run_test.sh
-else
-	docker run --name run_test -t tester bash test/run_test.sh
-fi
-
-# copy the coverage results out of docker container
-docker cp run_test:/go/src/github.com/tendermint/tendermint/coverage.txt .
-
-# test basic network connectivity
-# by starting a local testnet and checking peers connect and make blocks
-echo
-echo "* [$(date +"%T")] running p2p tests on a local docker network"
-bash "$DIR/p2p/test.sh" tester
-
-# only run the cloud benchmark for releases
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [[ $(echo "$BRANCH" | grep "release-") != "" ]]; then
-	echo
-	echo "TODO: run network tests"
-	#echo "* branch $BRANCH; running mintnet/netmon throughput benchmark"
-	# TODO: replace mintnet
-	#bash "$DIR/net/test.sh"
-fi
+#echo
+#echo "* [$(date +"%T")] running go tests and app tests in docker container"
+## sometimes its helpful to mount the local test folder
+## -v $DIR:/go/src/github.com/tendermint/tendermint/test
+#if [[ "$CIRCLECI" == true ]]; then
+#	docker run --name run_test -e CIRCLECI=true -t tester bash test/run_test.sh
+#else
+#	docker run --name run_test -t tester bash test/run_test.sh
+#fi
+#
+## copy the coverage results out of docker container
+#docker cp run_test:/go/src/github.com/tendermint/tendermint/coverage.txt .
+#
+## test basic network connectivity
+## by starting a local testnet and checking peers connect and make blocks
+#echo
+#echo "* [$(date +"%T")] running p2p tests on a local docker network"
+#bash "$DIR/p2p/test.sh" tester
+#
+## only run the cloud benchmark for releases
+#BRANCH=$(git rev-parse --abbrev-ref HEAD)
+#if [[ $(echo "$BRANCH" | grep "release-") != "" ]]; then
+#	echo
+#	echo "TODO: run network tests"
+#	#echo "* branch $BRANCH; running mintnet/netmon throughput benchmark"
+#	# TODO: replace mintnet
+#	#bash "$DIR/net/test.sh"
+#fi
