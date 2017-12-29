71f13cc071258fbcfe3fb3a3438d1a9f0ee0f4e0
Ethan Buchman <ethan@coinculture.info>
2017-12-29 10:42:02 -0500

    drop metalinter

--- test.sh
+++ test.sh
@@ -2,14 +2,14 @@
 set -e
 
 # run the linter
-make metalinter_test
+# make metalinter_test
 
 # run the unit tests with coverage
 echo "" > coverage.txt
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic "$d"
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
+	go test -race -coverprofile=profile.out -covermode=atomic "$d"
+	if [ -f profile.out ]; then
+		cat profile.out >> coverage.txt
+		rm profile.out
+	fi
 done
