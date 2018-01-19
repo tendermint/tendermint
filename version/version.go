a5b7034d648d33ed6e740c05ad6b45d7688fec37
Adrian Brink <adrian@brink-holdings.com>
2018-01-19 16:40:46 +0100

    Bump version and fix dist_build.sh to new Makefile

--- version/version.go
+++ version/version.go
@@ -2,12 +2,12 @@ package version
 
 const Maj = "0"
 const Min = "15"
-const Fix = "0"
+const Fix = "1"
 
 var (
 	// Version is the current version of Tendermint
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "0.15.0"
+	Version = "0.15.1"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
