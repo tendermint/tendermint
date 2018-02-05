93202e1168b034873f6d7bc034f8973c4754567e
Zach Ramsay <zach.ramsay@gmail.com>
2018-02-05 18:46:54 +0000

    docker run: no :Z for circle?

--- test/p2p/client.sh
+++ test/p2p/client.sh
@@ -12,7 +12,7 @@ echo "starting test client container with CMD=$CMD"
 # run the test container on the local network
 docker run -t --rm \
 	# NOTE: the `:Z` at the end is needed for SELinux (on Jenkins) to allow writes to the mounted volume on host
-	-v "$GOPATH/src/github.com/tendermint/tendermint/test/p2p/:/go/src/github.com/tendermint/tendermint/test/p2p":Z \
+	-v "$GOPATH/src/github.com/tendermint/tendermint/test/p2p/:/go/src/github.com/tendermint/tendermint/test/p2p" \
 	--net="$NETWORK_NAME" \
 	--ip=$(test/p2p/ip.sh "-1") \
 	--name "$NAME" \
