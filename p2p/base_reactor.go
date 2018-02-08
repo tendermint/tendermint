2b2c233977591dadc3646844afe61f1c7f579d49
Anton Kaliaev <anton.kalyaev@gmail.com>
2018-02-08 13:07:40 +0400

    write docs for Reactor interface

--- p2p/base_reactor.go
+++ p2p/base_reactor.go
@@ -8,11 +8,23 @@ import (
 type Reactor interface {
 	cmn.Service // Start, Stop
 
+	// SetSwitch allows setting a switch.
 	SetSwitch(*Switch)
+
+	// GetChannels returns the list of channel descriptors.
 	GetChannels() []*conn.ChannelDescriptor
+
+	// AddPeer is called by the switch when a new peer is added.
 	AddPeer(peer Peer)
+
+	// RemovePeer is called by the switch when the peer is stopped (due to error
+	// or other reason).
 	RemovePeer(peer Peer, reason interface{})
-	Receive(chID byte, peer Peer, msgBytes []byte) // CONTRACT: msgBytes are not nil
+
+	// Receive is called when msgBytes is received from peer.
+	//
+	// CONTRACT: msgBytes are not nil
+	Receive(chID byte, peer Peer, msgBytes []byte)
 }
 
 //--------------------------------------
