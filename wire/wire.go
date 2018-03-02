fc35e3b8c52fa441747c0aaf883aed6d2deb8c5a
Ethan Buchman <ethan@coinculture.info>
2018-03-02 01:27:52 -0500

    wire: no codec yet

--- wire/wire.go
+++ wire/wire.go
@@ -1,37 +1,42 @@
 package wire
 
 import (
-	"github.com/tendermint/go-crypto"
 	"github.com/tendermint/go-wire"
 )
 
+/*
 // Expose access to a global wire codec
 // TODO: maybe introduce some Context object
 // containing logger, config, codec that can
 // be threaded through everything to avoid this global
-
 var cdc *wire.Codec
 
 func init() {
 	cdc = wire.NewCodec()
 	crypto.RegisterWire(cdc)
 }
+*/
+
+// Just a flow through to go-wire.
+// To be used later for the global codec
 
 func MarshalBinary(o interface{}) ([]byte, error) {
-	return cdc.MarshalBinary(o)
+	return wire.MarshalBinary(o)
 }
 
 func UnmarshalBinary(bz []byte, ptr interface{}) error {
-	return cdc.UnmarshalBinary(bz, ptr)
+	return wire.UnmarshalBinary(bz, ptr)
 }
 
 func MarshalJSON(o interface{}) ([]byte, error) {
-	return cdc.MarshalJSON(o)
+	return wire.MarshalJSON(o)
 }
 
 func UnmarshalJSON(jsonBz []byte, ptr interface{}) error {
-	return cdc.UnmarshalJSON(jsonBz, ptr)
+	return wire.UnmarshalJSON(jsonBz, ptr)
 }
+
+/*
 
 func RegisterInterface(ptr interface{}, opts *wire.InterfaceOptions) {
 	cdc.RegisterInterface(ptr, opts)
@@ -44,3 +49,4 @@ func RegisterConcrete(o interface{}, name string, opts *wire.ConcreteOptions) {
 //-------------------------------
 
 const RFC3339Millis = wire.RFC3339Millis
+*/
