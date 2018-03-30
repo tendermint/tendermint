ea16c14ef36b5e0befb9afe3e1c3b39071c9992c
Thomas Corbière <thomas.corbiere@tilkal.com>
2018-03-30 11:02:13 +0200

    use lsi as the receiver name for the LastSignedInfo type.

--- types/priv_validator/sign_info.go
+++ types/priv_validator/sign_info.go
@@ -49,30 +49,30 @@ func NewLastSignedInfo() *LastSignedInfo {
 	}
 }
 
-func (info *LastSignedInfo) String() string {
-	return fmt.Sprintf("LH:%v, LR:%v, LS:%v", info.Height, info.Round, info.Step)
+func (lsi *LastSignedInfo) String() string {
+	return fmt.Sprintf("LH:%v, LR:%v, LS:%v", lsi.Height, lsi.Round, lsi.Step)
 }
 
 // Verify returns an error if there is a height/round/step regression
 // or if the HRS matches but there are no LastSignBytes.
 // It returns true if HRS matches exactly and the LastSignature exists.
 // It panics if the HRS matches, the LastSignBytes are not empty, but the LastSignature is empty.
-func (info LastSignedInfo) Verify(height int64, round int, step int8) (bool, error) {
-	if info.Height > height {
+func (lsi LastSignedInfo) Verify(height int64, round int, step int8) (bool, error) {
+	if lsi.Height > height {
 		return false, errors.New("Height regression")
 	}
 
-	if info.Height == height {
-		if info.Round > round {
+	if lsi.Height == height {
+		if lsi.Round > round {
 			return false, errors.New("Round regression")
 		}
 
-		if info.Round == round {
-			if info.Step > step {
+		if lsi.Round == round {
+			if lsi.Step > step {
 				return false, errors.New("Step regression")
-			} else if info.Step == step {
-				if info.SignBytes != nil {
-					if info.Signature.Empty() {
+			} else if lsi.Step == step {
+				if lsi.SignBytes != nil {
+					if lsi.Signature.Empty() {
 						panic("info: LastSignature is nil but LastSignBytes is not!")
 					}
 					return true, nil
@@ -85,24 +85,24 @@ func (info LastSignedInfo) Verify(height int64, round int, step int8) (bool, err
 }
 
 // Set height/round/step and signature on the info
-func (info *LastSignedInfo) Set(height int64, round int, step int8,
+func (lsi *LastSignedInfo) Set(height int64, round int, step int8,
 	signBytes []byte, sig crypto.Signature) {
 
-	info.Height = height
-	info.Round = round
-	info.Step = step
-	info.Signature = sig
-	info.SignBytes = signBytes
+	lsi.Height = height
+	lsi.Round = round
+	lsi.Step = step
+	lsi.Signature = sig
+	lsi.SignBytes = signBytes
 }
 
 // Reset resets all the values.
 // XXX: Unsafe.
-func (info *LastSignedInfo) Reset() {
-	info.Height = 0
-	info.Round = 0
-	info.Step = 0
-	info.Signature = crypto.Signature{}
-	info.SignBytes = nil
+func (lsi *LastSignedInfo) Reset() {
+	lsi.Height = 0
+	lsi.Round = 0
+	lsi.Step = 0
+	lsi.Signature = crypto.Signature{}
+	lsi.SignBytes = nil
 }
 
 // SignVote checks the height/round/step (HRS) are greater than the latest state of the LastSignedInfo.
@@ -110,11 +110,11 @@ func (info *LastSignedInfo) Reset() {
 // If the HRS are equal and the only thing changed is the timestamp, it sets the vote.Timestamp to the previous
 // value and the Signature to the LastSignedInfo.Signature.
 // Else it returns an error.
-func (info *LastSignedInfo) SignVote(signer types.Signer, chainID string, vote *types.Vote) error {
+func (lsi *LastSignedInfo) SignVote(signer types.Signer, chainID string, vote *types.Vote) error {
 	height, round, step := vote.Height, vote.Round, voteToStep(vote)
 	signBytes := vote.SignBytes(chainID)
 
-	sameHRS, err := info.Verify(height, round, step)
+	sameHRS, err := lsi.Verify(height, round, step)
 	if err != nil {
 		return err
 	}
@@ -125,11 +125,11 @@ func (info *LastSignedInfo) SignVote(signer types.Signer, chainID string, vote *
 	// If they only differ by timestamp, use last timestamp and signature
 	// Otherwise, return error
 	if sameHRS {
-		if bytes.Equal(signBytes, info.SignBytes) {
-			vote.Signature = info.Signature
-		} else if timestamp, ok := checkVotesOnlyDifferByTimestamp(info.SignBytes, signBytes); ok {
+		if bytes.Equal(signBytes, lsi.SignBytes) {
+			vote.Signature = lsi.Signature
+		} else if timestamp, ok := checkVotesOnlyDifferByTimestamp(lsi.SignBytes, signBytes); ok {
 			vote.Timestamp = timestamp
-			vote.Signature = info.Signature
+			vote.Signature = lsi.Signature
 		} else {
 			err = fmt.Errorf("Conflicting data")
 		}
@@ -139,7 +139,7 @@ func (info *LastSignedInfo) SignVote(signer types.Signer, chainID string, vote *
 	if err != nil {
 		return err
 	}
-	info.Set(height, round, step, signBytes, sig)
+	lsi.Set(height, round, step, signBytes, sig)
 	vote.Signature = sig
 	return nil
 }
@@ -149,11 +149,11 @@ func (info *LastSignedInfo) SignVote(signer types.Signer, chainID string, vote *
 // If the HRS are equal and the only thing changed is the timestamp, it sets the timestamp to the previous
 // value and the Signature to the LastSignedInfo.Signature.
 // Else it returns an error.
-func (info *LastSignedInfo) SignProposal(signer types.Signer, chainID string, proposal *types.Proposal) error {
+func (lsi *LastSignedInfo) SignProposal(signer types.Signer, chainID string, proposal *types.Proposal) error {
 	height, round, step := proposal.Height, proposal.Round, stepPropose
 	signBytes := proposal.SignBytes(chainID)
 
-	sameHRS, err := info.Verify(height, round, step)
+	sameHRS, err := lsi.Verify(height, round, step)
 	if err != nil {
 		return err
 	}
@@ -164,11 +164,11 @@ func (info *LastSignedInfo) SignProposal(signer types.Signer, chainID string, pr
 	// If they only differ by timestamp, use last timestamp and signature
 	// Otherwise, return error
 	if sameHRS {
-		if bytes.Equal(signBytes, info.SignBytes) {
-			proposal.Signature = info.Signature
-		} else if timestamp, ok := checkProposalsOnlyDifferByTimestamp(info.SignBytes, signBytes); ok {
+		if bytes.Equal(signBytes, lsi.SignBytes) {
+			proposal.Signature = lsi.Signature
+		} else if timestamp, ok := checkProposalsOnlyDifferByTimestamp(lsi.SignBytes, signBytes); ok {
 			proposal.Timestamp = timestamp
-			proposal.Signature = info.Signature
+			proposal.Signature = lsi.Signature
 		} else {
 			err = fmt.Errorf("Conflicting data")
 		}
@@ -178,7 +178,7 @@ func (info *LastSignedInfo) SignProposal(signer types.Signer, chainID string, pr
 	if err != nil {
 		return err
 	}
-	info.Set(height, round, step, signBytes, sig)
+	lsi.Set(height, round, step, signBytes, sig)
 	proposal.Signature = sig
 	return nil
 }
