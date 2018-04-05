a1dd329d72e78d4770e602359bad5b7b1e8b72a3
Ethan Buchman <ethan@coinculture.info>
2018-04-05 16:17:32 +0300

    update version

--- version/version.go
+++ version/version.go
@@ -1,13 +1,13 @@
 package version
 
 const Maj = "0"
-const Min = "17"
-const Fix = "1"
+const Min = "18"
+const Fix = "0"
 
 var (
 	// Version is the current version of Tendermint
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "0.17.1"
+	Version = "0.18.0-rc1"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
