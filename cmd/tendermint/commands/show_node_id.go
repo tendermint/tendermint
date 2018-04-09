4cd82d29c4e43bc02eb44ce3c2ff47d84e7ecf9e
kidinamoto01 <kidinamoto@qq.com>
2018-04-09 18:33:26 +0800

    remove if

--- cmd/tendermint/commands/show_node_id.go
+++ cmd/tendermint/commands/show_node_id.go
@@ -7,8 +7,6 @@ import (
 
 	"github.com/tendermint/tendermint/p2p"
 
-	cmn "github.com/tendermint/tmlibs/common"
-
 )
 
 // ShowNodeIDCmd dumps node's ID to the standard output.
@@ -19,14 +17,12 @@ var ShowNodeIDCmd = &cobra.Command{
 }
 
 func showNodeID(cmd *cobra.Command, args []string) error {
-	if cmn.FileExists(config.GenesisFile())&&cmn.FileExists(config.PrivValidatorFile()) {
-		nodeKey, err := p2p.LoadNodeKey(config.NodeKeyFile())
-		
-		fmt.Println(nodeKey.ID())
-	} else {
 
-		return fmt.Errorf("Need to initialize first")
+	nodeKey, err := p2p.LoadNodeKey(config.NodeKeyFile())
+	if err != nil {
+		return err
 	}
+	fmt.Println(nodeKey.ID())
 
 	return nil
 }
