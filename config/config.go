0d93424c6a186e017c5268dc651753c3fb52ffe2
Anton Kaliaev <anton.kalyaev@gmail.com>
2018-05-10 14:03:05 +0400

    disable indexer by default

--- config/config.go
+++ config/config.go
@@ -515,7 +515,7 @@ type TxIndexConfig struct {
 // DefaultTxIndexConfig returns a default configuration for the transaction indexer.
 func DefaultTxIndexConfig() *TxIndexConfig {
 	return &TxIndexConfig{
-		Indexer:      "kv",
+		Indexer:      "null",
 		IndexTags:    "",
 		IndexAllTags: false,
 	}
