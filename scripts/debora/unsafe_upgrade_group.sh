126ddca1a63f12225d2fcb3d9506c2858ba20283
Zach Ramsay <zach.ramsay@gmail.com>
2018-05-23 08:01:07 -0400

    remove debora scripts (#1610)

--- scripts/debora/unsafe_upgrade_group.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-printf "Upgrading group $1...\n"
-sleep 3
-
-debora --group "$1" run -- bash -c "cd \$GOPATH/src/github.com/tendermint/tendermint; git pull origin develop; make"
-printf "Done\n"
