a56e8d65a0c54248a07ce424b0579c8f2422d6e4
Christopher Goes <cwgoes@protonmail.ch>
2018-06-13 01:47:49 +0200

    Update heartbeat test for new Amino version

--- types/heartbeat_test.go
+++ types/heartbeat_test.go
@@ -39,11 +39,11 @@ func TestHeartbeatWriteSignBytes(t *testing.T) {
 	hb := &Heartbeat{ValidatorIndex: 1, Height: 10, Round: 1}
 	bz := hb.SignBytes("0xdeadbeef")
 	// XXX HMMMMMMM
-	require.Equal(t, string(bz), `{"@chain_id":"0xdeadbeef","@type":"heartbeat","height":10,"round":1,"sequence":0,"validator_address":"","validator_index":1}`)
+	require.Equal(t, string(bz), `{"@chain_id":"0xdeadbeef","@type":"heartbeat","height":10,"round":1,"sequence":0,"validator_address":"","validator_index":"1"}`)
 
 	plainHb := &Heartbeat{}
 	bz = plainHb.SignBytes("0xdeadbeef")
-	require.Equal(t, string(bz), `{"@chain_id":"0xdeadbeef","@type":"heartbeat","height":0,"round":0,"sequence":0,"validator_address":"","validator_index":0}`)
+	require.Equal(t, string(bz), `{"@chain_id":"0xdeadbeef","@type":"heartbeat","height":0,"round":0,"sequence":0,"validator_address":"","validator_index":"0"}`)
 
 	require.Panics(t, func() {
 		var nilHb *Heartbeat
