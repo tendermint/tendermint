363146dacf803a065d7bbe35994b145fae746ef3
Jae Kwon <jae@tendermint.com>
2018-06-27 02:03:15 -0700

    just print node1

--- test/p2p/peer.sh
+++ test/p2p/peer.sh
@@ -14,14 +14,33 @@ echo "starting tendermint peer ID=$ID"
 # start tendermint container on the network
 # NOTE: $NODE_FLAGS should be unescaped (no quotes). otherwise it will be
 # treated as one flag.
-docker run \
-	--net="$NETWORK_NAME" \
-	--ip=$(test/p2p/ip.sh "$ID") \
-	--name "local_testnet_$ID" \
-	--entrypoint tendermint \
-	-e TMHOME="/go/src/github.com/tendermint/tendermint/test/p2p/data/mach$ID/core" \
-	--log-driver=syslog \
-	--log-opt syslog-address=udp://127.0.0.1:5514 \
-	--log-opt syslog-facility=daemon \
-	--log-opt tag="{{.Name}}" \
-	"$DOCKER_IMAGE" node $NODE_FLAGS --log_level=debug --proxy_app="$APP_PROXY" &
+
+
+
+if [[ "$ID" == "1" ]]; then
+	docker run \
+		--net="$NETWORK_NAME" \
+		--ip=$(test/p2p/ip.sh "$ID") \
+		--name "local_testnet_$ID" \
+		--entrypoint tendermint \
+		-e TMHOME="/go/src/github.com/tendermint/tendermint/test/p2p/data/mach$ID/core" \
+		-e GOMAXPROCS=1 \
+		--log-driver=syslog \
+		--log-opt syslog-address=udp://127.0.0.1:5514 \
+		--log-opt syslog-facility=daemon \
+		--log-opt tag="{{.Name}}" \
+		"$DOCKER_IMAGE" node $NODE_FLAGS --log_level=debug --proxy_app="$APP_PROXY" &
+else
+	docker run -d \
+		--net="$NETWORK_NAME" \
+		--ip=$(test/p2p/ip.sh "$ID") \
+		--name "local_testnet_$ID" \
+		--entrypoint tendermint \
+		-e TMHOME="/go/src/github.com/tendermint/tendermint/test/p2p/data/mach$ID/core" \
+		-e GOMAXPROCS=1 \
+		--log-driver=syslog \
+		--log-opt syslog-address=udp://127.0.0.1:5514 \
+		--log-opt syslog-facility=daemon \
+		--log-opt tag="{{.Name}}" \
+		"$DOCKER_IMAGE" node $NODE_FLAGS --log_level=debug --proxy_app="$APP_PROXY"
+fi
