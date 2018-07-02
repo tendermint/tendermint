233b35a2a914f6b1d724c9ebb87cfc7607655c93
Ethan Buchman <ethan@coinculture.info>
2018-07-01 22:36:13 -0400

    Revert "delete some things for the merge"
    This reverts commit 2902ab1a144b6c0bef9c8cdd605ef77a8e70a7cc.

---
+++ version/version.go
@@ -0,0 +1,23 @@
+package version
+
+// Version components
+const (
+	Maj = "0"
+	Min = "22"
+	Fix = "0"
+)
+
+var (
+	// Version is the current version of Tendermint
+	// Must be a string because scripts like dist.sh read this file.
+	Version = "0.22.0"
+
+	// GitCommit is the current HEAD set using ldflags.
+	GitCommit string
+)
+
+func init() {
+	if GitCommit != "" {
+		Version += "-" + GitCommit
+	}
+}
