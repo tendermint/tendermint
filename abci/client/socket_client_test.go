0ec94e12efea2bb503d34aaf3d65064a1f8d27c2
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-07-03 18:13:28 +0200

    Add test showing normal Sync call

--- abci/client/socket_client_test.go
+++ abci/client/socket_client_test.go
@@ -5,7 +5,11 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/abci/client"
+	"github.com/tendermint/tendermint/abci/server"
+	"github.com/tendermint/tendermint/abci/types"
 )
 
 func TestSocketClientStopForErrorDeadlock(t *testing.T) {
@@ -25,4 +29,35 @@ func TestSocketClientStopForErrorDeadlock(t *testing.T) {
 	case <-time.After(time.Second * 4):
 		t.Fatalf("Test took too long, potential deadlock still exists")
 	}
+}
+
+func TestProperSyncCalls(t *testing.T) {
+	port := ":29385"
+	app := new(types.BaseApplication)
+
+	s, err := server.NewServer(port, "socket", app)
+	require.NoError(t, err)
+	err = s.Start()
+	require.NoError(t, err)
+	defer s.Stop()
+
+	c := abcicli.NewSocketClient(port, true)
+	err = c.Start()
+	require.NoError(t, err)
+	defer c.Stop()
+
+	resp := make(chan error, 1)
+	go func() {
+		_, err := c.BeginBlockSync(types.RequestBeginBlock{})
+		resp <- err
+	}()
+
+	select {
+	case <-time.After(time.Second):
+		require.Fail(t, "No response arrived")
+	case err, ok := <-resp:
+		require.True(t, ok, "Must not close channel")
+		assert.NoError(t, err, "This should return success")
+	}
+
 }
