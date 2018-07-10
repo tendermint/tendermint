9d81a74429e093f3167875e0145ad957874c77d1
Ethan Buchman <ethan@coinculture.info>
2018-07-10 10:12:55 -0400

    dev version

--- version/version.go
+++ version/version.go
@@ -10,7 +10,7 @@ const (
 var (
 	// Version is the current version of Tendermint
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "0.22.2"
+	Version = "0.22.2-dev"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
