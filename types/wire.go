e1a561b5dd476b56ffa18c9d7815c56471e899ea
Hendrik Hofstadt <me@slamper.me>
2018-07-27 00:41:15 +0200

    Add test for MakePartSet with evidence

--- types/wire.go
+++ types/wire.go
@@ -2,7 +2,7 @@ package types
 
 import (
 	"github.com/tendermint/go-amino"
-	cryptoAmino "github.com/tendermint/tendermint/crypto/encoding/amino"
+	"github.com/tendermint/tendermint/crypto/encoding/amino"
 )
 
 var cdc = amino.NewCodec()
@@ -10,4 +10,14 @@ var cdc = amino.NewCodec()
 func init() {
 	cryptoAmino.RegisterAmino(cdc)
 	RegisterEvidences(cdc)
+	RegisterMockEvidences(cdc)
+}
+
+//-------------------------------------------
+
+func RegisterMockEvidences(cdc *amino.Codec) {
+	cdc.RegisterConcrete(MockGoodEvidence{},
+		"tendermint/MockGoodEvidence", nil)
+	cdc.RegisterConcrete(MockBadEvidence{},
+		"tendermint/MockBadEvidence", nil)
 }
