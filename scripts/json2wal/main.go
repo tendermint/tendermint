1b06a910a5e1d783aa138d81493897d0c6b49c1a
bradyjoestar <bradyjoestar@gmail.com>
2018-08-09 20:50:30 +0800

    Update main.go

--- scripts/json2wal/main.go
+++ scripts/json2wal/main.go
@@ -39,7 +39,7 @@ func main() {
 	}
 	defer f.Close()
 
-	walFile, err := os.OpenFile(os.Args[2],os.O_EXCL|os.O_WRONLY,0666)
+	walFile, err := os.OpenFile(os.Args[2],os.O_EXCL|os.O_WRONLY|os.O_CREATE,0666)
 	if err != nil {
 		panic(fmt.Errorf("failed to open WAL file: %v", err))
 	}
