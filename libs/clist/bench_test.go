8148702f188ee995193e0fbf62ece136b7aa6d7c
ValarDragon <dojha12@gmail.com>
2018-08-11 11:40:05 -0500

    Add more benchmarks

--- libs/clist/bench_test.go
+++ libs/clist/bench_test.go
@@ -19,3 +19,28 @@ func BenchmarkDetaching(b *testing.B) {
 		start = tmp
 	}
 }
+
+// This is used to benchmark the time of RMutex.
+func BenchmarkRemoved(b *testing.B) {
+	lst := New()
+	for i := 0; i < b.N+1; i++ {
+		lst.PushBack(i)
+	}
+	start := lst.Front()
+	nxt := start.Next()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		start.Removed()
+		tmp := nxt
+		nxt = nxt.Next()
+		start = tmp
+	}
+}
+
+func BenchmarkPushBack(b *testing.B) {
+	lst := New()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		lst.PushBack(i)
+	}
+}
