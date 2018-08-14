2fe34491ba7c6d90b11f0ac2b6261bc4e8c318b1
ValarDragon <dojha12@gmail.com>
2018-08-14 11:42:40 -0700

    (squash this) Fix build errors

--- crypto/multisig/compact_bit_array_test.go
+++ crypto/multisig/compact_bit_array_test.go
@@ -170,7 +170,7 @@ func TestCompactBitArrayNumOfTrueBitsBefore(t *testing.T) {
 			require.NoError(t, err)
 
 			for i := 0; i < len(tc.bAIndex); i++ {
-				require.Equal(t, tc.trueValueIndex[i], bA.NumOfTrueBitsBefore(tc.bAIndex[i]), "tc %d, i %d", tcIndex, i)
+				require.Equal(t, tc.trueValueIndex[i], bA.NumTrueBitsBefore(tc.bAIndex[i]), "tc %d, i %d", tcIndex, i)
 			}
 		})
 	}
