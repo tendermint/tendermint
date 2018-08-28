ad8006aedfa2704ffbf7e6343813baf2fa3f92ec
Alessio Treglia <quadrispro@ubuntu.com>
2018-08-28 11:07:19 +0100

    New NewGoLevelDBWithOpts() to pass opts down to goleveldb
    Closes: #2292

--- libs/db/go_level_db_test.go
+++ libs/db/go_level_db_test.go
@@ -6,8 +6,29 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/syndtr/goleveldb/leveldb/opt"
+
+	"github.com/stretchr/testify/assert"
 	cmn "github.com/tendermint/tendermint/libs/common"
 )
+
+func TestNewGoLevelDB(t *testing.T) {
+	name := fmt.Sprintf("test_%x", cmn.RandStr(12))
+	// Test write locks
+	db, err := NewGoLevelDB(name, "")
+	assert.Nil(t, err)
+	_, err = NewGoLevelDB(name, "")
+	assert.NotNil(t, err)
+	db.Close() // Close the db to release the lock
+
+	// Open the db twice in a row to test read-only locks
+	ro1, err := NewGoLevelDBWithOpts(name, "", &opt.Options{ReadOnly: true})
+	defer func() { ro1.Close() }()
+	assert.Nil(t, err)
+	ro2, err := NewGoLevelDBWithOpts(name, "", &opt.Options{ReadOnly: true})
+	defer func() { ro2.Close() }()
+	assert.Nil(t, err)
+}
 
 func BenchmarkRandomReadsWrites(b *testing.B) {
 	b.StopTimer()
