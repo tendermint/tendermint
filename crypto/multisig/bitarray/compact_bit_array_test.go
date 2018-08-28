38b401657e4ad7a7eeb3c30a3cbf512037df3740
Ethan Buchman <ethan@coinculture.info>
2018-08-28 00:41:40 -0400

    Cleanup up Multisig naming (#2255)
    * crypto/multisig: Pubkey -> PubKey
    
    * crypto/encoding/amino: use pkg vars for routes
    
    * crypto/multisig/bitarray
    
    * crypto/multisig: ThresholdMultiSignaturePubKey -> PubKeyMultisigThreshold
    
    * crypto/encoding/amino: add PubKeyMultisig to table
    
    * remove bA import alias
    
    https://github.com/tendermint/tendermint/pull/2255#discussion_r211900709

--- crypto/multisig/bitarray/compact_bit_array_test.go
+++ crypto/multisig/bitarray/compact_bit_array_test.go
@@ -1,4 +1,4 @@
-package multisig
+package bitarray
 
 import (
 	"encoding/json"
--- crypto/multisig/wire.go
+++ crypto/multisig/wire.go
@@ -10,15 +10,15 @@ import (
 // TODO: Figure out API for others to either add their own pubkey types, or
 // to make verify / marshal accept a cdc.
 const (
-	ThresholdPubkeyAminoRoute = "tendermint/PubkeyMultisigThreshold"
+	PubKeyMultisigThresholdAminoRoute = "tendermint/PubKeyMultisigThreshold"
 )
 
 var cdc = amino.NewCodec()
 
 func init() {
 	cdc.RegisterInterface((*crypto.PubKey)(nil), nil)
-	cdc.RegisterConcrete(ThresholdMultiSignaturePubKey{},
-		ThresholdPubkeyAminoRoute, nil)
+	cdc.RegisterConcrete(PubKeyMultisigThreshold{},
+		PubKeyMultisigThresholdAminoRoute, nil)
 	cdc.RegisterConcrete(ed25519.PubKeyEd25519{},
 		ed25519.PubKeyAminoRoute, nil)
 	cdc.RegisterConcrete(secp256k1.PubKeySecp256k1{},
