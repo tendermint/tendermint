fcba43d3bcaed0947c5da1b52edaa4b36d974b24
mossid <torecursedivine@gmail.com>
2018-09-03 02:21:01 -0700

    remove iavl dependency

--- lite/proxy/proof.go
+++ lite/proxy/proof.go
@@ -1,7 +1,6 @@
 package proxy
 
 import (
-	"github.com/tendermint/iavl"
 	"github.com/tendermint/tendermint/crypto/merkle"
 )
 
@@ -10,14 +9,6 @@ func defaultProofRuntime() *merkle.ProofRuntime {
 	prt.RegisterOpDecoder(
 		merkle.ProofOpSimpleValue,
 		merkle.SimpleValueOpDecoder,
-	)
-	prt.RegisterOpDecoder(
-		iavl.ProofOpIAVLValue,
-		iavl.IAVLValueOpDecoder,
-	)
-	prt.RegisterOpDecoder(
-		iavl.ProofOpIAVLAbsence,
-		iavl.IAVLAbsenceOpDecoder,
 	)
 	return prt
 }
