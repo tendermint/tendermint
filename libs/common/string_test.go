0d6b75bd53f2e513feee3aa640025d3884473f67
Dev Ojha <ValarDragon@users.noreply.github.com>
2018-09-20 02:12:42 -0700

    common: Delete unused functions (#2452)
    These functions were not used anywhere within tendermint, or the
    cosmos-sdk. (The functionality is already duplicated in the cosmos-sdk
    types package)
    
    * common: Delete unused functions within byteslice
    * remove more unused code from strings.go and int.go
    * Remove more unused code from int.go
    * Fix testcase

--- libs/common/string_test.go
+++ libs/common/string_test.go
@@ -13,30 +13,12 @@ func TestStringInSlice(t *testing.T) {
 	assert.False(t, StringInSlice("", []string{}))
 }
 
-func TestIsHex(t *testing.T) {
-	notHex := []string{
-		"", "   ", "a", "x", "0", "0x", "0X", "0x ", "0X ", "0X a",
-		"0xf ", "0x f", "0xp", "0x-",
-		"0xf", "0XBED", "0xF", "0xbed", // Odd lengths
-	}
-	for _, v := range notHex {
-		assert.False(t, IsHex(v), "%q is not hex", v)
-	}
-	hex := []string{
-		"0x00", "0x0a", "0x0F", "0xFFFFFF", "0Xdeadbeef", "0x0BED",
-		"0X12", "0X0A",
-	}
-	for _, v := range hex {
-		assert.True(t, IsHex(v), "%q is hex", v)
-	}
-}
-
 func TestIsASCIIText(t *testing.T) {
 	notASCIIText := []string{
 		"", "\xC2", "\xC2\xA2", "\xFF", "\x80", "\xF0", "\n", "\t",
 	}
 	for _, v := range notASCIIText {
-		assert.False(t, IsHex(v), "%q is not ascii-text", v)
+		assert.False(t, IsASCIIText(v), "%q is not ascii-text", v)
 	}
 	asciiText := []string{
 		" ", ".", "x", "$", "_", "abcdefg;", "-", "0x00", "0", "123",
