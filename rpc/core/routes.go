84b518b8d3dad245d2eb6131a186e1ea0fb8aa89
Aravind <aravindkp@outlook.in>
2018-09-20 18:01:20 +0530

    rpc: Add /consensus_params endpoint  (#2415)
    * Add /consensus_params endpoint
    * Incorporated change https://github.com/tendermint/tendermint/pull/2415#discussion_r219078049
    * Fixed an error in pervious commit

--- rpc/core/routes.go
+++ rpc/core/routes.go
@@ -26,6 +26,7 @@ var Routes = map[string]*rpc.RPCFunc{
 	"validators":           rpc.NewRPCFunc(Validators, "height"),
 	"dump_consensus_state": rpc.NewRPCFunc(DumpConsensusState, ""),
 	"consensus_state":      rpc.NewRPCFunc(ConsensusState, ""),
+	"consensus_params":     rpc.NewRPCFunc(ConsensusParams, "height"),
 	"unconfirmed_txs":      rpc.NewRPCFunc(UnconfirmedTxs, "limit"),
 	"num_unconfirmed_txs":  rpc.NewRPCFunc(NumUnconfirmedTxs, ""),
 
