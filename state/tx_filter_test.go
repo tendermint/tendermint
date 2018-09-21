5eb59bcb43ee4f59c1b0fb0cd18583c2be75eb1f
Anton Kaliaev <anton.kalyaev@gmail.com>
2018-09-21 09:56:38 +0400

    remove testdouble package

--- state/tx_filter_test.go
+++ state/tx_filter_test.go
@@ -7,14 +7,15 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/tendermint/tendermint/crypto/ed25519"
 	cmn "github.com/tendermint/tendermint/libs/common"
 	dbm "github.com/tendermint/tendermint/libs/db"
 	"github.com/tendermint/tendermint/types"
-	testd "github.com/tendermint/tendermint/types/testdouble"
+	tmtime "github.com/tendermint/tendermint/types/time"
 )
 
 func TestTxFilter(t *testing.T) {
-	genDoc := testd.RandomGenesisDoc()
+	genDoc := randomGenesisDoc()
 	genDoc.ConsensusParams.BlockSize.MaxBytes = 3000
 
 	testCases := []struct {
@@ -32,5 +33,15 @@ func TestTxFilter(t *testing.T) {
 
 		f := TxFilter(state)
 		assert.Equal(t, tc.isTxValid, f(tc.tx), "#%v", i)
+	}
+}
+
+func randomGenesisDoc() *types.GenesisDoc {
+	pubkey := ed25519.GenPrivKey().PubKey()
+	return &types.GenesisDoc{
+		GenesisTime:     tmtime.Now(),
+		ChainID:         "abc",
+		Validators:      []types.GenesisValidator{{pubkey.Address(), pubkey, 10, "myval"}},
+		ConsensusParams: types.DefaultConsensusParams(),
 	}
 }
