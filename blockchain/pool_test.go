009b4997d4a3a95e7936faf323765408424bc3a9
goolAdapter <267310165@qq.com>
2018-09-26 13:54:00 +0800

    add pool invalid input test.

--- blockchain/pool_test.go
+++ blockchain/pool_test.go
@@ -27,16 +27,18 @@ type inputData struct {
 	request BlockRequest
 }
 
-func (p testPeer) runInputRoutine() {
+func (p testPeer) runInputRoutine(f InputFunc) {
 	go func() {
 		for input := range p.inputChan {
-			p.simulateInput(input)
+			f(input)
 		}
 	}()
 }
 
+type InputFunc func(input inputData)
+
 // Request desired, pretend like we got the block immediately.
-func (p testPeer) simulateInput(input inputData) {
+func simulateInput(input inputData) {
 	block := &types.Block{Header: types.Header{Height: input.request.Height}}
 	input.pool.AddBlockHeader(input.request.PeerID, input.request.Height, 123)
 	input.pool.AddBlock(input.request.PeerID, block, 123)
@@ -45,9 +47,9 @@ func (p testPeer) simulateInput(input inputData) {
 
 type testPeers map[p2p.ID]testPeer
 
-func (ps testPeers) start() {
+func (ps testPeers) start(f InputFunc) {
 	for _, v := range ps {
-		v.runInputRoutine()
+		v.runInputRoutine(f)
 	}
 }
 
@@ -82,7 +84,7 @@ func TestBasic(t *testing.T) {
 
 	defer pool.Stop()
 
-	peers.start()
+	peers.start(simulateInput)
 	defer peers.stop()
 
 	// Introduce each peer.
@@ -121,6 +123,85 @@ func TestBasic(t *testing.T) {
 			peers[request.PeerID].inputChan <- inputData{t, pool, request}
 		}
 	}
+}
+
+func simulateInvalidInput(input inputData) {
+	val := cmn.RandInt63n(4)
+	switch val {
+	case 0:
+		input.pool.AddBlockHeader("invalid_id", input.request.Height, 123)
+	case 1:
+		input.pool.AddBlockHeader(input.request.PeerID, input.request.Height, 1024*1024*1024)
+		input.pool.AddBlockHeader(input.request.PeerID, input.request.Height, -1)
+	case 2:
+		block := &types.Block{Header: types.Header{Height: input.request.Height}}
+		input.pool.AddBlockHeader(input.request.PeerID, input.request.Height, 123)
+		input.pool.AddBlock(input.request.PeerID, block, 124)
+	case 3:
+		block := &types.Block{Header: types.Header{Height: 10000}}
+		input.pool.AddBlockHeader(input.request.PeerID, 10000, 123)
+		input.pool.AddBlock(input.request.PeerID, block, 123)
+	}
+
+	input.t.Logf("Added block from peer %v (height: %v)", input.request.PeerID, input.request.Height)
+}
+
+func TestInvalidInput(t *testing.T) {
+	start := int64(42)
+	peers := makePeers(10, start+1, 1000)
+	errorsCh := make(chan peerError, 1000)
+	requestsCh := make(chan BlockRequest, 1000)
+	pool := NewBlockPool(start, requestsCh, errorsCh)
+	pool.SetLogger(log.TestingLogger())
+
+	err := pool.Start()
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer pool.Stop()
+
+	peers.start(simulateInvalidInput)
+	defer peers.stop()
+
+	// Introduce each peer.
+	go func() {
+		for _, peer := range peers {
+			pool.SetPeerHeight(peer.id, peer.height)
+		}
+	}()
+
+	// Start a goroutine to pull blocks
+	go func() {
+		for {
+			if !pool.IsRunning() {
+				return
+			}
+			first, second := pool.PeekTwoBlocks()
+			if first != nil && second != nil {
+				panic("must not reach here.")
+			} else {
+				time.Sleep(1 * time.Millisecond)
+			}
+		}
+	}()
+
+	var peerErrors = make(map[p2p.ID]string, 10)
+	// Pull from channels
+	for {
+		select {
+		case err := <-errorsCh:
+			peerErrors[err.peerID] = err.err.Error()
+			if len(peerErrors) == 10 {
+				goto quit // Done!
+			}
+		case request := <-requestsCh:
+			peers[request.PeerID].inputChan <- inputData{t, pool, request}
+		}
+	}
+
+quit:
+	t.Logf("pool status %v", pool.debug())
 }
 
 func TestTimeout(t *testing.T) {
