f3d08f969dbd5a219eca472fe4eb9f91e460573f
Anton Kaliaev <anton.kalyaev@gmail.com>
2018-10-03 04:31:04 +0400

    [rpc] fix /abci_query: trusted was renamed to prove (#2531)

--- types/block_test.go
+++ types/block_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"github.com/tendermint/tendermint/crypto/tmhash"
 	cmn "github.com/tendermint/tendermint/libs/common"
 )
