f7055939f609e6e9779826131416f41282639af6
Ethan Buchman <ethan@coinculture.info>
2018-10-09 13:16:13 -0400

    fixes from review

--- state/validation_test.go
+++ state/validation_test.go
@@ -29,33 +29,33 @@ func TestValidateBlockHeader(t *testing.T) {
 
 	// Manipulation of any header field causes failure.
 	testCases := []struct {
-		name string
-		f    func(block *types.Block)
+		name          string
+		malleateBlock func(block *types.Block)
 	}{
-		{"ChainID", func(block *types.Block) { block.ChainID = "not-the-real-one" }}, // wrong chain id
-		{"Height", func(block *types.Block) { block.Height += 10 }},                  // wrong height
+		{"ChainID wrong", func(block *types.Block) { block.ChainID = "not-the-real-one" }}, // wrong chain id
+		{"Height wrong", func(block *types.Block) { block.Height += 10 }},                  // wrong height
 		// TODO(#2589) (#2587) : {"Time", func(block *types.Block) { block.Time.Add(-time.Second * 3600 * 24) }}, // wrong time
-		{"NumTxs", func(block *types.Block) { block.NumTxs += 10 }},     // wrong num txs
-		{"TotalTxs", func(block *types.Block) { block.TotalTxs += 10 }}, // wrong total txs
+		{"NumTxs wrong", func(block *types.Block) { block.NumTxs += 10 }},     // wrong num txs
+		{"TotalTxs wrong", func(block *types.Block) { block.TotalTxs += 10 }}, // wrong total txs
 
-		{"LastBlockID", func(block *types.Block) { block.LastBlockID.PartsHeader.Total += 10 }}, // wrong block id
-		{"LastCommitHash", func(block *types.Block) { block.LastCommitHash = wrongHash }},       // wrong commit hash
-		{"DataHash", func(block *types.Block) { block.DataHash = wrongHash }},                   // wrong data hash
+		{"LastBlockID wrong", func(block *types.Block) { block.LastBlockID.PartsHeader.Total += 10 }},
+		{"LastCommitHash wrong", func(block *types.Block) { block.LastCommitHash = wrongHash }},
+		{"DataHash wrong", func(block *types.Block) { block.DataHash = wrongHash }},
 
-		{"ValidatorsHash", func(block *types.Block) { block.ValidatorsHash = wrongHash }},         // wrong validators hash
-		{"NextValidatorsHash", func(block *types.Block) { block.NextValidatorsHash = wrongHash }}, // wrong next validators hash
-		{"ConsensusHash", func(block *types.Block) { block.ConsensusHash = wrongHash }},           // wrong consensus hash
-		{"AppHash", func(block *types.Block) { block.AppHash = wrongHash }},                       // wrong app hash
-		{"LastResultsHash", func(block *types.Block) { block.LastResultsHash = wrongHash }},       // wrong results hash
+		{"ValidatorsHash wrong", func(block *types.Block) { block.ValidatorsHash = wrongHash }},
+		{"NextValidatorsHash wrong", func(block *types.Block) { block.NextValidatorsHash = wrongHash }},
+		{"ConsensusHash wrong", func(block *types.Block) { block.ConsensusHash = wrongHash }},
+		{"AppHash wrong", func(block *types.Block) { block.AppHash = wrongHash }},
+		{"LastResultsHash wrong", func(block *types.Block) { block.LastResultsHash = wrongHash }},
 
-		{"EvidenceHash", func(block *types.Block) { block.EvidenceHash = wrongHash }},                                    // wrong evidence hash
-		{"Proposer wrong", func(block *types.Block) { block.ProposerAddress = ed25519.GenPrivKey().PubKey().Address() }}, // wrong proposer
-		{"Proposer invalid", func(block *types.Block) { block.ProposerAddress = []byte("wrong size") }},                  // invalid proposer
+		{"EvidenceHash wrong", func(block *types.Block) { block.EvidenceHash = wrongHash }},
+		{"Proposer wrong", func(block *types.Block) { block.ProposerAddress = ed25519.GenPrivKey().PubKey().Address() }},
+		{"Proposer invalid", func(block *types.Block) { block.ProposerAddress = []byte("wrong size") }},
 	}
 
 	for _, tc := range testCases {
 		block := makeBlock(state, height)
-		tc.f(block)
+		tc.malleateBlock(block)
 		err := blockExec.ValidateBlock(state, block)
 		require.Error(t, err, tc.name)
 	}
