b53a1e32b61e8a31195370dfc7cd75f184be5da9
Anton Kaliaev <anton.kalyaev@gmail.com>
2018-11-05 17:46:31 +0100

    comment out until someone decides to tackle #2285
    current code results in panic and we certainly don't want that.
    https://github.com/tendermint/tendermint/pull/2286#issuecomment-418281846

--- libs/autofile/autofile.go
+++ libs/autofile/autofile.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	cmn "github.com/tendermint/tendermint/libs/common"
-	"github.com/tendermint/tendermint/libs/errors"
 )
 
 /* AutoFile usage
@@ -147,13 +146,13 @@ func (af *AutoFile) openFile() error {
 	if err != nil {
 		return err
 	}
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return err
-	}
-	if fileInfo.Mode() != autoFilePerms {
-		return errors.NewErrPermissionsChanged(file.Name(), fileInfo.Mode(), autoFilePerms)
-	}
+	// fileInfo, err := file.Stat()
+	// if err != nil {
+	// 	return err
+	// }
+	// if fileInfo.Mode() != autoFilePerms {
+	// 	return errors.NewErrPermissionsChanged(file.Name(), fileInfo.Mode(), autoFilePerms)
+	// }
 	af.file = file
 	return nil
 }
