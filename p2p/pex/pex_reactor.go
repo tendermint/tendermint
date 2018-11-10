48ab899923c564bbf2fa2f1244c11cb930e28132
Jae Kwon <jae@tendermint.com>
2018-11-09 19:31:59 -0800

    Do not shut down peer immediately after sending pex addrs in SeedMode

--- p2p/pex/pex_reactor.go
+++ p2p/pex/pex_reactor.go
@@ -221,6 +221,7 @@ func (r *PEXReactor) Receive(chID byte, src Peer, msgBytes []byte) {
 		// 2) limit the output size
 		if r.config.SeedMode {
 			r.SendAddrs(src, r.book.GetSelectionWithBias(biasToSelectNewPeers))
+			time.Sleep(time.Second * 3) // TODO Rethink this.  Without it, above may not actually send.
 			r.Switch.StopPeerGracefully(src)
 		} else {
 			r.SendAddrs(src, r.book.GetSelection())
