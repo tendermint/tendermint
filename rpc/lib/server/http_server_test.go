64f7825541e4a0498c1f8aec374a3a11b775ea50
Alessio Treglia <alessio@tendermint.com>
2018-11-12 13:51:39 +0000

    Reinstate l.Close()

--- rpc/lib/server/http_server_test.go
+++ rpc/lib/server/http_server_test.go
@@ -32,6 +32,7 @@ func TestMaxOpenConnections(t *testing.T) {
 	})
 	l, err := Listen("tcp://127.0.0.1:0", Config{MaxOpenConnections: max})
 	require.NoError(t, err)
+	defer l.Close()
 	go StartHTTPServer(l, mux, log.TestingLogger())
 
 	// Make N GET calls to the server.
