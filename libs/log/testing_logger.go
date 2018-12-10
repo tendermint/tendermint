2b9f736e7e105927ceaac991377f4d0d65c5fba5
ValarDragon <dojha12@gmail.com>
2018-12-10 12:29:47 -0800

    Make testing logger that doesn't write to stdout

--- libs/log/testing_logger.go
+++ libs/log/testing_logger.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"io"
 	"os"
 	"testing"
 
@@ -25,6 +26,26 @@ func TestingLogger() Logger {
 
 	if testing.Verbose() {
 		_testingLogger = NewTMLogger(NewSyncWriter(os.Stdout))
+	} else {
+		_testingLogger = NewNopLogger()
+	}
+
+	return _testingLogger
+}
+
+// TestingLoggerWOutput returns a TMLogger which writes to (w io.Writer) if testing being run
+// with the verbose (-v) flag, NopLogger otherwise.
+//
+// Note that the call to TestingLoggerWOutput(w io.Writer) must be made
+// inside a test (not in the init func) because
+// verbose flag only set at the time of testing.
+func TestingLoggerWOutput(w io.Writer) Logger {
+	if _testingLogger != nil {
+		return _testingLogger
+	}
+
+	if testing.Verbose() {
+		_testingLogger = NewTMLogger(NewSyncWriter(w))
 	} else {
 		_testingLogger = NewNopLogger()
 	}
