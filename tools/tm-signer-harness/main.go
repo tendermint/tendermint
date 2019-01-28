c0319f30fca43e2ea15f68ff689ff4c4c3534c48
Thane Thomson <connect@thanethomson.com>
2019-01-28 11:09:02 +0200

    Refactors flag parsing to be more compact and less redundant

--- tools/tm-signer-harness/main.go
+++ tools/tm-signer-harness/main.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/tendermint/tendermint/crypto/ed25519"
@@ -27,68 +26,68 @@ const (
 
 var logger = log.NewTMLogger(log.NewSyncWriter(os.Stdout))
 
-func usage() string {
-	var b strings.Builder
-	b.WriteString("Remote signer test harness for Tendermint.\n\n")
-	b.WriteString("Usage:\n")
-	b.WriteString("  tm-signer-harness <command> [args]\n\n")
-	b.WriteString("Available Commands:\n")
-	b.WriteString("  extract_key        Extracts a signing key from a local Tendermint instance\n")
-	b.WriteString("  help               Help on the available commands\n")
-	b.WriteString("  run                Runs the test harness\n")
-	b.WriteString("  version            Display version information and exit\n\n")
-	b.WriteString("Use \"tm-signer-harness help <command>\" for more information on that command.\n")
-	return b.String()
-}
+// Command line flags
+var (
+	flagAcceptRetries int
+	flagBindAddr      string
+	flagTMHome        string
+	flagKeyOutputPath string
+)
 
-func cmdUsage(cmd string) string {
-	var b strings.Builder
-	switch cmd {
-	case "run":
-		b.WriteString("Runs the remote signer test harness for Tendermint.\n\n")
-		b.WriteString("Usage:\n")
-		b.WriteString("  tm-signer-harness run [args]\n\n")
-		b.WriteString("Flags:\n")
-		b.WriteString(
-			fmt.Sprintf(
-				"  -accept-retries <int>   The number of attempts to listen for incoming connections (default: %d)\n",
-				defaultAcceptRetries,
-			),
-		)
-		b.WriteString(
-			fmt.Sprintf(
-				"  -addr <string>          Bind to this address for the testing (default: \"%s\")\n",
-				defaultBindAddr,
-			),
-		)
-		b.WriteString(
-			fmt.Sprintf(
-				"  -tmhome <string>        Path to the Tendermint home directory (default: \"%s\")\n",
-				defaultTMHome,
-			),
-		)
-	case "extract_key":
-		b.WriteString("Extracts a signing key from a local Tendermint instance for use in the remote signer under test.\n\n")
-		b.WriteString("Usage:\n")
-		b.WriteString("  tm-signer-harness extract_key [args]\n\n")
-		b.WriteString("Flags:\n")
-		b.WriteString(
-			fmt.Sprintf(
-				"  -output <string>        Path to which the signing key should be written (default: \"%s\")\n",
-				defaultExtractKeyOutput,
-			),
-		)
-		b.WriteString(
-			fmt.Sprintf(
-				"  -tmhome <string>        Path to the Tendermint home directory (default: \"%s\")\n",
-				defaultTMHome,
-			),
-		)
-	default:
-		fmt.Printf("Unrecognised command: %s\n", cmd)
-		os.Exit(1)
+// Command line commands
+var (
+	rootCmd       *flag.FlagSet
+	runCmd        *flag.FlagSet
+	extractKeyCmd *flag.FlagSet
+)
+
+func init() {
+	rootCmd = flag.NewFlagSet("root", flag.ExitOnError)
+	rootCmd.Usage = func() {
+		fmt.Println(`Remote signer test harness for Tendermint.
+
+Usage:
+  tm-signer-harness <command> [flags]
+
+Available Commands:
+  extract_key        Extracts a signing key from a local Tendermint instance
+  help               Help on the available commands
+  run                Runs the test harness
+  version            Display version information and exit
+
+Use "tm-signer-harness help <command>" for more information about that command.`)
+		fmt.Println("")
 	}
-	return b.String()
+
+	runCmd = flag.NewFlagSet("run", flag.ExitOnError)
+	runCmd.IntVar(&flagAcceptRetries, "accept-retries", defaultAcceptRetries, "The number of attempts to listen for incoming connections")
+	runCmd.StringVar(&flagBindAddr, "addr", defaultBindAddr, "Bind to this address for the testing")
+	runCmd.StringVar(&flagTMHome, "tmhome", defaultTMHome, "Path to the Tendermint home directory")
+	runCmd.Usage = func() {
+		fmt.Println(`Runs the remote signer test harness for Tendermint.
+
+Usage:
+  tm-signer-harness run [flags]
+
+Flags:`)
+		runCmd.PrintDefaults()
+		fmt.Println("")
+	}
+
+	extractKeyCmd = flag.NewFlagSet("extract_key", flag.ExitOnError)
+	extractKeyCmd.StringVar(&flagKeyOutputPath, "output", defaultExtractKeyOutput, "Path to which signing key should be written")
+	extractKeyCmd.StringVar(&flagTMHome, "tmhome", defaultTMHome, "Path to the Tendermint home directory")
+	extractKeyCmd.Usage = func() {
+		fmt.Println(`Extracts a signing key from a local Tendermint instance for use in the remote
+signer under test.
+
+Usage:
+  tm-signer-harness extract_key [flags]
+
+Flags:`)
+		extractKeyCmd.PrintDefaults()
+		fmt.Println("")
+	}
 }
 
 func runTestHarness(acceptRetries int, bindAddr, tmhome string) {
@@ -128,39 +127,35 @@ func extractKey(tmhome, outputPath string) {
 }
 
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Println(usage())
+	flag.Parse()
+	if flag.NArg() == 0 || (flag.NArg() == 1 && flag.Arg(0) == "help") {
+		rootCmd.Usage()
 		os.Exit(0)
 	}
 
-	runCmd := flag.NewFlagSet("run", flag.ExitOnError)
-	flagRunAcceptRetries := runCmd.Int("accept-retries", defaultAcceptRetries, "")
-	flagRunBindAddr := runCmd.String("addr", defaultBindAddr, "")
-	flagRunTMHome := runCmd.String("tmhome", defaultTMHome, "")
-
-	extractKeyCmd := flag.NewFlagSet("extract_key", flag.ExitOnError)
-	flagExtractKeyTMHome := extractKeyCmd.String("tmhome", defaultTMHome, "")
-	flagExtractKeyOutputPath := extractKeyCmd.String("output", defaultExtractKeyOutput, "")
-
 	logger = log.NewFilter(logger, log.AllowInfo())
 
-	switch os.Args[1] {
+	switch flag.Arg(0) {
 	case "help":
-		if len(os.Args) > 2 {
-			fmt.Println(cmdUsage(os.Args[2]))
-		} else {
-			fmt.Println(usage())
+		switch flag.Arg(1) {
+		case "run":
+			runCmd.Usage()
+		case "extract_key":
+			extractKeyCmd.Usage()
+		default:
+			fmt.Printf("Unrecognized command: %s\n", flag.Arg(1))
+			os.Exit(1)
 		}
 	case "run":
 		runCmd.Parse(os.Args[2:])
-		runTestHarness(*flagRunAcceptRetries, *flagRunBindAddr, *flagRunTMHome)
+		runTestHarness(flagAcceptRetries, flagBindAddr, flagTMHome)
 	case "extract_key":
 		extractKeyCmd.Parse(os.Args[2:])
-		extractKey(*flagExtractKeyTMHome, *flagExtractKeyOutputPath)
+		extractKey(flagTMHome, flagKeyOutputPath)
 	case "version":
 		fmt.Println(version.Version)
 	default:
-		fmt.Printf("Unrecognised command: %s", os.Args[1])
+		fmt.Printf("Unrecognized command: %s\n", flag.Arg(0))
 		os.Exit(1)
 	}
 }
