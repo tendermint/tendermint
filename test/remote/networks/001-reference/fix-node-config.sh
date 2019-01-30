d779bd85a8ca3cf038975643b4abb30dba5e8173
Thane Thomson <connect@thanethomson.com>
2019-01-30 07:39:40 +0200

    Streamline node config alteration

--- test/remote/networks/001-reference/fix-node-config.sh
+++ test/remote/networks/001-reference/fix-node-config.sh
@@ -3,13 +3,13 @@ set -e
 
 for CFG_FILE in $(find /tmp/nodes -name 'config.toml'); do
     NODE_ID=$(basename $(dirname $(dirname ${CFG_FILE})))
-    cat ${CFG_FILE} | \
-        sed "s/t\([ioa]\)k\([0-9]*\):/t\1k\2.sredev.co:/g" | \
-        sed "s/^proxy_app = \(.*\)$/proxy_app = \"kvstore\"/" | \
-        sed "s/^moniker = \(.*\)$/moniker = \"${NODE_ID}\"/" | \
-        sed "s/^log_format = \(.*\)$/log_format = \"json\"/" | \
-        sed "s/^create_empty_blocks = \(.*\)$/create_empty_blocks = false/" | \
-        sed "s/^prometheus = \(.*\)$/prometheus = true/" \
-        > ${CFG_FILE}
+    sed -i '' \
+        -e "s/t\([ioa]\)k\([0-9]*\):/t\1k\2.sredev.co:/g" \
+        -e "s/^proxy_app = \(.*\)$/proxy_app = \"kvstore\"/" \
+        -e "s/^moniker = \(.*\)$/moniker = \"${NODE_ID}\"/" \
+        -e "s/^log_format = \(.*\)$/log_format = \"json\"/" \
+        -e "s/^create_empty_blocks = \(.*\)$/create_empty_blocks = false/" \
+        -e "s/^prometheus = \(.*\)$/prometheus = true/" \
+        ${CFG_FILE}
     echo "Rewrote \"config.toml\" for ${NODE_ID}"
 done
