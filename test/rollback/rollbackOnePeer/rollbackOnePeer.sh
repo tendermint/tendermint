eb76119d519316f7c7024e89cca870f805837dfe
wenbin.shi <swbsin@163.com>
2019-02-01 02:01:55 -0800

    add rollbackOnePeer & rollbackSomePeer TestCase

---
+++ test/rollback/rollbackOnePeer/rollbackOnePeer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# usage rollbackAllPeer.sh $rollbackHeight
+
+docker stop peer3
+docker rm peer3
+
+typeset -l address
+address=$(cat $PWD/newtm/node0/config/priv_validator_key.json | jq ".address" |sed 's/\"//g')
+echo $address
+
+rm -rf $PWD/newtm/node0/data/cs.wal
+docker run -tid --net=bridge --name=peer3 -p 26686:26656 -p 26687:26657 -p 26688:26658 -v $PWD/newtm/node3:/chaindata -v /usr/bin:/bin  ubuntu /bin/tendermint node --proxy_app=kvstore --p2p.persistent_peers=$address@172.17.0.1:46656 --log_level=info --home /chaindata  --rollback_data=true --rollback_height=$1 --rollback_height_flag=true
---
+++ test/rollback/rollbackSomePeer/rollbackSomePeer.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# usage rollbackAllPeer.sh $rollbackHeight
+
+docker stop peer1 && docker stop peer2
+docker rm peer1 && docker rm peer2
+
+# get the address of peer0
+typeset -l address
+address=$(cat $PWD/newtm/node0/config/priv_validator_key.json | jq ".address" |sed 's/\"//g')
+echo $address
+
+## rollback data and restart node
+for((i=1;i<3;i++))
+do
+	rm -rf $PWD/newtm/node$i/data/cs.wal
+    port2=$((26656 + $i * 10))
+    port3=$((26657 + $i * 10))
+    port4=$((26658 + $i * 10))
+    docker run -tid --net=bridge --name=peer$i -p $port2:26656 -p $port3:26657 -p $port4:26658 -v $PWD/newtm/node$i:/chaindata -v /usr/bin:/bin  ubuntu /bin/tendermint node --proxy_app=kvstore --p2p.pex=false --p2p.persistent_peers=$address@172.17.0.1:26656 --log_level=info --home /chaindata  --rollback_data=true --rollback_height=$1 --rollback_height_flag=true
+done
