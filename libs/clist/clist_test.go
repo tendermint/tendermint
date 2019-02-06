1386707ceb922f8cd390105c86c6a598c044f748
Anton Kaliaev <anton.kalyaev@gmail.com>
2019-02-06 18:36:54 +0400

    rename TestGCRandom to _TestGCRandom

--- libs/clist/clist_test.go
+++ libs/clist/clist_test.go
@@ -117,7 +117,7 @@ func _TestGCFifo(t *testing.T) {
 // This test is quite hacky because it relies on SetFinalizer
 // which isn't guaranteed to run at all.
 //nolint:unused,deadcode
-func TestGCRandom(t *testing.T) {
+func _TestGCRandom(t *testing.T) {
 	if runtime.GOARCH != "amd64" {
 		t.Skipf("Skipping on non-amd64 machine")
 	}
