8c43128df9d6d7300edd7cd8d816d64e8dd7cf88
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2019-02-11 17:40:07 +0100

    nolint usage of math/rand in test and add comment on its import
     - crypto/rand is slower and we do not need sth more secure in tests

--- crypto/merkle/proof_key_path_test.go
+++ crypto/merkle/proof_key_path_test.go
@@ -1,6 +1,8 @@
 package merkle
 
 import (
+	// it is ok here to use math/rand; we do not need cryptographically secure random
+	// number generator here and test run a bit faster
 	"math/rand"
 	"testing"
 
@@ -24,7 +26,7 @@ func TestKeyPath(t *testing.T) {
 					keys[i][j] = alphanum[rand.Intn(len(alphanum))]
 				}
 			case KeyEncodingHex:
-				rand.Read(keys[i])
+				rand.Read(keys[i]) //nolint: gosec
 			default:
 				panic("Unexpected encoding")
 			}
