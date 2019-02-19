fd9c40a4fe7a3afd8fb5c8ae30d7fc7d4c5888e0
wenbin.shi <swbsin@163.com>
2019-02-19 05:32:13 -0800

    fix lint error

--- cmd/tendermint/commands/rollback.go
+++ cmd/tendermint/commands/rollback.go
@@ -85,7 +85,7 @@ func rollbackData(cmd *cobra.Command, args []string) error {
 func loadGenesisDoc(db dbm.DB) (*types.GenesisDoc, error) {
 	bytes := db.Get(genesisDocKey)
 	if len(bytes) == 0 {
-		return nil, cmn.ErrorWrap(nil,"Genesis doc not found")
+		return nil, cmn.ErrorWrap(nil, "Genesis doc not found")
 	}
 	var genDoc *types.GenesisDoc
 	err := cdc.UnmarshalJSON(bytes, &genDoc)
