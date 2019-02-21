536bf108b3db2c5c3ec7a10efe032ec2303f1ec1
fudongbai <296179868@qq.com>
2019-02-21 19:26:32 +0800

    make lightd availbe
    1."abci_query": rpcserver.NewRPCFunc(c.ABCIQuery, "path,data,prove")
    "validators": rpcserver.NewRPCFunc(c.Validators, "height"),
    the parameters and function do not match, cause index out of range error.
    2. the prove of query is forced to be true, while default option is false.
    3. fix the wrong key of merkle

--- cmd/tendermint/commands/lite.go
+++ cmd/tendermint/commands/lite.go
@@ -43,7 +43,7 @@ func init() {
 	LiteCmd.Flags().IntVar(&cacheSize, "cache-size", 10, "Specify the memory trust store cache size")
 }
 
-func ensureAddrHasSchemeOrDefaultToTCP(addr string) (string, error) {
+func EnsureAddrHasSchemeOrDefaultToTCP(addr string) (string, error) {
 	u, err := url.Parse(addr)
 	if err != nil {
 		return "", err
@@ -64,11 +64,11 @@ func runProxy(cmd *cobra.Command, args []string) error {
 		// TODO: close up shop
 	})
 
-	nodeAddr, err := ensureAddrHasSchemeOrDefaultToTCP(nodeAddr)
+	nodeAddr, err := EnsureAddrHasSchemeOrDefaultToTCP(nodeAddr)
 	if err != nil {
 		return err
 	}
-	listenAddr, err := ensureAddrHasSchemeOrDefaultToTCP(listenAddr)
+	listenAddr, err := EnsureAddrHasSchemeOrDefaultToTCP(listenAddr)
 	if err != nil {
 		return err
 	}
