fe4d5bf29bbb9631dd47787d7eb25e983315e585
Anca Zamfir <ancazamfir>
2019-02-21 16:52:12 +0100

    review comments

--- types/validator_set_test.go
+++ types/validator_set_test.go
@@ -691,7 +691,7 @@ func verifyValidatorSet(t *testing.T, valSet *ValidatorSet) {
 	assert.Equal(t, len(valSet.Validators), cap(valSet.Validators))
 
 	// verify that the set's total voting power has been updated
-	tvp := valSet.TotalVotingPower()
+	tvp := valSet.totalVotingPower
 	valSet.updateTotalVotingPower()
 	expectedTvp := valSet.TotalVotingPower()
 	assert.Equal(t, expectedTvp, tvp,
