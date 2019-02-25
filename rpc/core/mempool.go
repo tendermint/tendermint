05687b9dc5b35d4c3d6e47487f0bd4bf776c5f09
Anton Kaliaev <anton.kalyaev@gmail.com>
2019-02-25 13:31:19 +0400

    explain broadcast_tx_* tx format
    Closes #536

--- rpc/core/mempool.go
+++ rpc/core/mempool.go
@@ -16,7 +16,8 @@ import (
 //-----------------------------------------------------------------------------
 // NOTE: tx should be signed, but this is only checked at the app level (not by Tendermint!)
 
-// Returns right away, with no response
+// Returns right away, with no response. Does not wait for CheckTx nor
+// DeliverTx results.
 //
 // ```shell
 // curl 'localhost:26657/broadcast_tx_async?tx="123"'
@@ -53,6 +54,50 @@ import (
 // | Parameter | Type | Default | Required | Description     |
 // |-----------+------+---------+----------+-----------------|
 // | tx        | Tx   | nil     | true     | The transaction |
+//
+// To send a UTF8 string byte array, you simply quote the value of the tx parameter on the GET request:
+//
+//     GET http://myserver:46657/broadcast_tx_async?tx="hello"
+//
+// This sends a 5 byte transaction "h e l l o" [68 65 6c 6c 6f]
+//
+//     GET http://myserver:46657/broadcast_tx_async?tx="5"
+//
+// This sends a 4 byte transaction "5" (UTF8) [e2 82 ac 35]
+//
+// To send as raw hex, you MUST omit the quotes AND prefix the hex string with 0x
+//
+//     GET http://myserver:46657/broadcast_tx_async?tx=0x01020304
+//
+// This sends a 4 byte transaction [01 02 03 04]
+//
+// If you prefer to POST your data as json, you have to Base64 encode your transaction byte array.
+//
+// ```
+// POST http://myserver:46657
+// {
+//   "method": "broadcast_tx_async",
+//   "jsonrpc": "2.0",
+//   "params": {"tx": "AQIDBA=="},
+//   "id": ""
+// }
+// ```
+//
+// This sends the same 4 byte transaction [01 02 03 04]
+//
+// Note that you CANNOT POST your transaction as hex like so:
+//
+// ```
+// {
+//   "method": "broadcast_tx_async",
+//   "jsonrpc": "2.0",
+//   "params": {"tx": "0x01020304"},
+//   "id": ""
+// }
+// ```
+//
+// You'll get a 400 Bad Request with error: "Error converting json params to
+// arguments: illegal base64 data at input byte 8"
 func BroadcastTxAsync(tx types.Tx) (*ctypes.ResultBroadcastTx, error) {
 	err := mempool.CheckTx(tx, nil)
 	if err != nil {
@@ -61,7 +106,7 @@ func BroadcastTxAsync(tx types.Tx) (*ctypes.ResultBroadcastTx, error) {
 	return &ctypes.ResultBroadcastTx{Hash: tx.Hash()}, nil
 }
 
-// Returns with the response from CheckTx.
+// Returns with the response from CheckTx. Does not wait for DeliverTx result.
 //
 // ```shell
 // curl 'localhost:26657/broadcast_tx_sync?tx="456"'
@@ -98,6 +143,50 @@ func BroadcastTxAsync(tx types.Tx) (*ctypes.ResultBroadcastTx, error) {
 // | Parameter | Type | Default | Required | Description     |
 // |-----------+------+---------+----------+-----------------|
 // | tx        | Tx   | nil     | true     | The transaction |
+//
+// To send a UTF8 string byte array, you simply quote the value of the tx parameter on the GET request:
+//
+//     GET http://myserver:46657/broadcast_tx_sync?tx="hello"
+//
+// This sends a 5 byte transaction "h e l l o" [68 65 6c 6c 6f]
+//
+//     GET http://myserver:46657/broadcast_tx_sync?tx="5"
+//
+// This sends a 4 byte transaction "5" (UTF8) [e2 82 ac 35]
+//
+// To send as raw hex, you MUST omit the quotes AND prefix the hex string with 0x
+//
+//     GET http://myserver:46657/broadcast_tx_sync?tx=0x01020304
+//
+// This sends a 4 byte transaction [01 02 03 04]
+//
+// If you prefer to POST your data as json, you have to Base64 encode your transaction byte array.
+//
+// ```
+// POST http://myserver:46657
+// {
+//   "method": "broadcast_tx_sync",
+//   "jsonrpc": "2.0",
+//   "params": {"tx": "AQIDBA=="},
+//   "id": ""
+// }
+// ```
+//
+// This sends the same 4 byte transaction [01 02 03 04]
+//
+// Note that you CANNOT POST your transaction as hex like so:
+//
+// ```
+// {
+//   "method": "broadcast_tx_sync",
+//   "jsonrpc": "2.0",
+//   "params": {"tx": "0x01020304"},
+//   "id": ""
+// }
+// ```
+//
+// You'll get a 400 Bad Request with error: "Error converting json params to
+// arguments: illegal base64 data at input byte 8"
 func BroadcastTxSync(tx types.Tx) (*ctypes.ResultBroadcastTx, error) {
 	resCh := make(chan *abci.Response, 1)
 	err := mempool.CheckTx(tx, func(res *abci.Response) {
@@ -116,6 +205,8 @@ func BroadcastTxSync(tx types.Tx) (*ctypes.ResultBroadcastTx, error) {
 	}, nil
 }
 
+// Returns with the responses from CheckTx and DeliverTx.
+//
 // CONTRACT: only returns error if mempool.CheckTx() errs or if we timeout
 // waiting for tx to commit.
 //
@@ -165,6 +256,50 @@ func BroadcastTxSync(tx types.Tx) (*ctypes.ResultBroadcastTx, error) {
 // | Parameter | Type | Default | Required | Description     |
 // |-----------+------+---------+----------+-----------------|
 // | tx        | Tx   | nil     | true     | The transaction |
+//
+// To send a UTF8 string byte array, you simply quote the value of the tx parameter on the GET request:
+//
+//     GET http://myserver:46657/broadcast_tx_commit?tx="hello"
+//
+// This sends a 5 byte transaction "h e l l o" [68 65 6c 6c 6f]
+//
+//     GET http://myserver:46657/broadcast_tx_commit?tx="5"
+//
+// This sends a 4 byte transaction "5" (UTF8) [e2 82 ac 35]
+//
+// To send as raw hex, you MUST omit the quotes AND prefix the hex string with 0x
+//
+//     GET http://myserver:46657/broadcast_tx_commit?tx=0x01020304
+//
+// This sends a 4 byte transaction [01 02 03 04]
+//
+// If you prefer to POST your data as json, you have to Base64 encode your transaction byte array.
+//
+// ```
+// POST http://myserver:46657
+// {
+//   "method": "broadcast_tx_commit",
+//   "jsonrpc": "2.0",
+//   "params": {"tx": "AQIDBA=="},
+//   "id": ""
+// }
+// ```
+//
+// This sends the same 4 byte transaction [01 02 03 04]
+//
+// Note that you CANNOT POST your transaction as hex like so:
+//
+// ```
+// {
+//   "method": "broadcast_tx_commit",
+//   "jsonrpc": "2.0",
+//   "params": {"tx": "0x01020304"},
+//   "id": ""
+// }
+// ```
+//
+// You'll get a 400 Bad Request with error: "Error converting json params to
+// arguments: illegal base64 data at input byte 8"
 func BroadcastTxCommit(tx types.Tx) (*ctypes.ResultBroadcastTxCommit, error) {
 	// Subscribe to tx being committed in block.
 	ctx, cancel := context.WithTimeout(context.Background(), subscribeTimeout)
