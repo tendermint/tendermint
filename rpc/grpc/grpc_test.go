f9974754a701dd29e3d331bae8dcc37da2174215
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2019-03-11 15:11:33 +0400

    Update rpc/grpc/grpc_test.go
    Co-Authored-By: melekes <anton.kalyaev@gmail.com>

--- rpc/grpc/grpc_test.go
+++ rpc/grpc/grpc_test.go
@@ -26,7 +26,7 @@ func TestMain(m *testing.M) {
 
 func TestBroadcastTx(t *testing.T) {
 	res, err := rpctest.GetGRPCClient().BroadcastTx(context.Background(), &core_grpc.RequestBroadcastTx{Tx: []byte("this is a tx")})
-	require.Nil(t, err, "%+v", err)
+	require.NoError(t, err)
 	require.EqualValues(t, 0, res.CheckTx.Code)
 	require.EqualValues(t, 0, res.DeliverTx.Code)
 }
