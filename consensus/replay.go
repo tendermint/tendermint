8b269a1065e64f82da4f0065b10d9efc51e92048
zander <jozzzero@gmail.com>
2019-03-21 21:39:52 +0800

    fix issue3460

--- consensus/replay.go
+++ consensus/replay.go
@@ -361,6 +361,16 @@ func (h *Handshaker) replayBlocks(state sm.State, proxyApp proxy.AppConns, appBl
 		if err != nil {
 			return nil, err
 		}
+		//#3460 enhance replayBlocks of checkAppHash
+		nextBlock := h.store.LoadBlock(i + 1)
+		if nextBlock != nil {
+			//Ehach block's apphash should be checked  not only the last block
+			err = checkBlockAppHash(appHash, nextBlock.AppHash, i)
+			if err != nil {
+				return nil, err
+			}
+
+		}
 
 		h.nBlocks++
 	}
@@ -393,6 +403,15 @@ func (h *Handshaker) replayBlock(state sm.State, height int64, proxyApp proxy.Ap
 	h.nBlocks++
 
 	return state, nil
+}
+
+//which block's apphash does't match should be known
+func checkBlockAppHash(appHash, blockAppHash []byte, height int64) error {
+
+	if !bytes.Equal(blockAppHash, appHash) {
+		panic(fmt.Errorf(" Replay block  AppHash does not match Tendermint next block.AppHash  after replay. Got %X, expected %X, height %X", appHash, blockAppHash, height).Error())
+	}
+	return nil
 }
 
 func checkAppHash(state sm.State, appHash []byte) error {
