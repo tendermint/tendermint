993cc0ba7a596e0aa8f72e048a6166449f928a27
zander <jozzzero@gmail.com>
2019-03-25 10:52:47 +0800

    fix issue3460

--- consensus/replay_test.go
+++ consensus/replay_test.go
@@ -622,7 +622,7 @@ func NewMockBlockStore(config *cfg.Config, params types.ConsensusParams) *mockBl
 }
 
 func (bs *mockBlockStore) Height() int64                       { return int64(len(bs.chain)) }
-func (bs *mockBlockStore) LoadBlock(height int64) *types.Block { return bs.chain[height-1] }
+func (bs *mockBlockStore) mutateState(height int64) *types.Block { return bs.chain[height-1] }
 func (bs *mockBlockStore) LoadBlockMeta(height int64) *types.BlockMeta {
 	block := bs.chain[height-1]
 	return &types.BlockMeta{
