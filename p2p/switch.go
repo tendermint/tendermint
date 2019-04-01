882622ec10ba3fdc9f360da65bfb2bbbd13193ed
Ethan Buchman <ethan@coinculture.info>
2019-04-01 19:59:57 -0400

    Fixes tendermint/tendermint#3522
    * OriginalAddr -> SocketAddr
    
    OriginalAddr records the originally dialed address for outbound peers,
    rather than the peer's self reported address. For inbound peers, it was
    nil. Here, we rename it to SocketAddr and for inbound peers, set it to
    the RemoteAddr of the connection.
    
    * use SocketAddr
    
    Numerous places in the code call peer.NodeInfo().NetAddress().
    However, this call to NetAddress() may perform a DNS lookup if the
    reported NodeInfo.ListenAddr includes a name. Failure of this lookup
    returns a nil address, which can lead to panics in the code.
    
    Instead, call peer.SocketAddr() to return the static address of the
    connection.
    
    * remove nodeInfo.NetAddress()
    
    Expose `transport.NetAddress()`, a static result determined
    when the transport is created. Removing NetAddress() from the nodeInfo
    prevents accidental DNS lookups.
    
    * fixes from review
    
    * linter
    
    * fixes from review

--- p2p/switch.go
+++ p2p/switch.go
@@ -86,6 +86,12 @@ type Switch struct {
 	metrics *Metrics
 }
 
+// NetAddress returns the address the switch is listening on.
+func (sw *Switch) NetAddress() *NetAddress {
+	addr := sw.transport.NetAddress()
+	return &addr
+}
+
 // SwitchOption sets an optional parameter on the Switch.
 type SwitchOption func(*Switch)
 
@@ -289,13 +295,7 @@ func (sw *Switch) StopPeerForError(peer Peer, reason interface{}) {
 	sw.stopAndRemovePeer(peer, reason)
 
 	if peer.IsPersistent() {
-		addr := peer.OriginalAddr()
-		if addr == nil {
-			// FIXME: persistent peers can't be inbound right now.
-			// self-reported address for inbound persistent peers
-			addr = peer.NodeInfo().NetAddress()
-		}
-		go sw.reconnectToPeer(addr)
+		go sw.reconnectToPeer(peer.SocketAddr())
 	}
 }
 
@@ -383,7 +383,7 @@ func (sw *Switch) SetAddrBook(addrBook AddrBook) {
 // like contributed to consensus.
 func (sw *Switch) MarkPeerAsGood(peer Peer) {
 	if sw.addrBook != nil {
-		sw.addrBook.MarkGood(peer.NodeInfo().NetAddress())
+		sw.addrBook.MarkGood(peer.SocketAddr())
 	}
 }
 
@@ -400,7 +400,7 @@ func (sw *Switch) DialPeersAsync(addrBook AddrBook, peers []string, persistent b
 		sw.Logger.Error("Error in peer's address", "err", err)
 	}
 
-	ourAddr := sw.nodeInfo.NetAddress()
+	ourAddr := sw.NetAddress()
 
 	// TODO: this code feels like it's in the wrong place.
 	// The integration tests depend on the addrBook being saved
@@ -536,7 +536,7 @@ func (sw *Switch) acceptRoutine() {
 		if in >= sw.config.MaxNumInboundPeers {
 			sw.Logger.Info(
 				"Ignoring inbound connection: already have enough inbound peers",
-				"address", p.NodeInfo().NetAddress().String(),
+				"address", p.SocketAddr(),
 				"have", in,
 				"max", sw.config.MaxNumInboundPeers,
 			)
@@ -653,7 +653,7 @@ func (sw *Switch) addPeer(p Peer) error {
 		return err
 	}
 
-	p.SetLogger(sw.Logger.With("peer", p.NodeInfo().NetAddress()))
+	p.SetLogger(sw.Logger.With("peer", p.SocketAddr()))
 
 	// Handle the shut down case where the switch has stopped but we're
 	// concurrently trying to add a peer.
