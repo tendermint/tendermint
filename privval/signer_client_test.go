6d68360e51694e0475aba30d6ae571a6b9b5c697
Anton Kaliaev <anton.kalyaev@gmail.com>
2019-04-03 14:26:25 +0200

    Update privval/signer_client_test.go
    Co-Authored-By: jleni <juan.leni@zondax.ch>

--- privval/signer_client_test.go
+++ privval/signer_client_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"github.com/tendermint/tendermint/libs/common"
 	"github.com/tendermint/tendermint/types"
 )
