b59aa2a576f29968ffc9576d99a25d8ba1405dc2
Sean Braithwaite <brapse@gmail.com>
2019-04-16 18:28:52 +0200

    [p2p] Test empty StoredPeerBehaviour

--- p2p/peer_behaviour_test.go
+++ p2p/peer_behaviour_test.go
@@ -8,22 +8,34 @@ import (
 func TestStoredPeerBehaviour(t *testing.T) {
 	peer := newMockPeer(net.IP{127, 0, 0, 1})
 	pb := NewStoredPeerBehaviour()
-	pb.Errored(peer.ID(), ErrorPeerBehaviourUnknown)
 
 	peerErrors := pb.GetErrorBehaviours(peer.ID())
+	if len(peerErrors) != 0 {
+		t.Errorf("Expected the peer have zero error behaviours")
+	}
+
+	pb.Errored(peer.ID(), ErrorPeerBehaviourUnknown)
+	peerErrors = pb.GetErrorBehaviours(peer.ID())
 	if len(peerErrors) != 1 {
 		t.Errorf("Expected the peer have one error behaviour")
 	}
+
 	if peerErrors[0] != ErrorPeerBehaviourUnknown {
 		t.Errorf("Expected error to be ErrorPeerBehaviourUnknown")
 	}
 
-	pb.Behaved(peer.ID(), GoodPeerBehaviourVote)
 	peerGoods := pb.GetGoodBehaviours(peer.ID())
-	if len(peerGoods) != 1 {
+	if len(peerGoods) != 0 {
+		t.Errorf("Expected the peer have zero error behaviour")
+	}
+
+	pb.Behaved(peer.ID(), GoodPeerBehaviourVote)
+	peerGoods = pb.GetGoodBehaviours(peer.ID())
+	if len(peerErrors) != 1 {
 		t.Errorf("Expected the peer have one good behaviour")
 	}
-	if peerGoods[0] != GoodPeerBehaviourVote {
-		t.Errorf("Expected the peer to have voted")
+
+	if peerErrors[0] != ErrorPeerBehaviourUnknown {
+		t.Errorf("Expected peer to have voted")
 	}
 }
