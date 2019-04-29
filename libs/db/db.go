bccbcc52b1a2495c989fe0d5d4d53c92bb0a72ae
CrocdileChan <CrocdileChan@github.com>
2019-04-29 23:38:57 +0800

    add boltdb for storage

--- libs/db/db.go
+++ libs/db/db.go
@@ -16,6 +16,7 @@ const (
 	GoLevelDBBackend DBBackendType = "goleveldb"
 	MemDBBackend     DBBackendType = "memdb"
 	FSDBBackend      DBBackendType = "fsdb" // using the filesystem naively
+	BoltDBBackend    DBBackendType = "boltdb"
 )
 
 type dbCreator func(name string, dir string) (DB, error)
