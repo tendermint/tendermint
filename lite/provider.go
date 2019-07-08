6810bab382c6f7d6753256a0760e32f4609446b4
Anton Kaliaev <anton.kalyaev@gmail.com>
2019-07-08 15:42:15 +0400

    refactor ConcurrentProvider

--- lite/provider.go
+++ lite/provider.go
@@ -40,13 +40,12 @@ type UpdatingProvider interface {
 	Provider
 
 	// Update internal information by fetching information somehow.
-	// UpdateToHeight will block until the request is complete,
-	// or returns an error if the request cannot complete.
-	// Generally, one must call UpdateToHeight(h) before LatestFullCommit(_,h,h)
-	// will return this height.
+	// UpdateToHeight will block until the request is complete, or returns an
+	// error if the request cannot complete. Generally, one must call
+	// UpdateToHeight(h) before LatestFullCommit(_,h,h) will return this height.
 	//
-	// NOTE: Behavior with concurrent requests is undefined.  To make
-	// concurrent calls safe, look at the struct `ConcurrentUpdatingProvider`.
+	// NOTE: Behavior with concurrent requests is undefined. To make concurrent
+	// calls safe, look at the struct `ConcurrentUpdatingProvider`.
 	UpdateToHeight(chainID string, height int64) error
 }
 
@@ -72,53 +71,56 @@ type pendingResult struct {
 	err  error // cached result.
 }
 
-func NewConcurrentUpdatingProvider(up UpdatingProvider) ConcurrentProvider {
-	return ConcurrentProvider{
+// NewConcurrentUpdatingProvider creates a ConcurrentProvider using the given
+// UpdatingProvider.
+func NewConcurrentUpdatingProvider(up UpdatingProvider) *ConcurrentProvider {
+	return &ConcurrentProvider{
 		UpdatingProvider:     up,
 		pendingVerifications: make(map[pendingKey]*pendingResult),
 	}
 }
 
 // Returns the unique pending request for all identical calls to
-// joinConcurrency(chainID,height), and returns true for isFirstCall only for the
-// first call, which should call the returned callback w/ results if any.
-// The callback must be called, otherwise there will be memory leaks.
-// Other subsequent calls should just return uniq.err.
-// NOTE: This is a separate function, primarily to make mtx unlocking more obviously safe via defer.
-func (cp ConcurrentProvider) joinConcurrency(chainID string, height int64) (uniq *pendingResult, isFirstCall bool, callback func(error)) {
+// joinConcurrency(chainID,height), and returns true for isFirstCall only for
+// the first call, which should call the returned callback w/ results if any.
+//
+// NOTE: The callback must be called, otherwise there will be memory leaks.
+//
+// Other subsequent calls should just return pr.err.
+// This is a separate function, primarily to make mtx unlocking more
+// obviously safe via defer.
+func (cp *ConcurrentProvider) joinConcurrency(chainID string, height int64) (pr *pendingResult, isFirstCall bool, callback func(error)) {
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
 
-	var pkey = pendingKey{chainID, height}
-	cp.mtx.Lock()
-	if uniq = cp.pendingVerifications[pkey]; uniq != nil {
+	pk := pendingKey{chainID, height}
+
+	if pr = cp.pendingVerifications[pk]; pr != nil {
+		<-pr.wait
+		return pr, false, nil
+	}
+
+	pr = &pendingResult{wait: make(chan struct{}), err: nil}
+	cp.pendingVerifications[pk] = pr
+
+	// The caller must call this, otherwise there will be memory leaks.
+	return pr, true, func(err error) {
+		// NOTE: other result parameters can be added here.
+		pr.err = err
+
+		// *After* setting the results, *then* call close(pr.wait).
+		close(pr.wait)
+
+		cp.mtx.Lock()
+		delete(cp.pendingVerifications, pk)
 		cp.mtx.Unlock()
-		<-uniq.wait // uniq.wait is of type `chan struct{}`
-		return uniq, false, nil
-	} else {
-		uniq = &pendingResult{wait: make(chan struct{}), err: nil}
-		cp.pendingVerifications[pkey] = uniq
-		cp.mtx.Unlock()
-		// The caller must call this, otherwise there will be memory leaks.
-		return uniq, true, func(err error) {
-			// NOTE: other result parameters can be added here.
-			uniq.err = err
-			// *After* setting the results, *then* call close(uniq.wait).
-			close(uniq.wait)
-			cp.mtx.Lock() // temporarily acquire lock to remove this item
-			delete(cp.pendingVerifications, pkey)
-			cp.mtx.Unlock() // and release lock
-		}
 	}
 }
 
+// UpdateToHeight implements UpdatingProvider.
 func (cp *ConcurrentProvider) UpdateToHeight(chainID string, height int64) error {
-
 	// Performs synchronization for multi-threads verification at the same height.
-	var presult *pendingResult
-	var isFirstCall bool
-	var callback func(error)
-	presult, isFirstCall, callback = cp.joinConcurrency(chainID, height)
+	pr, isFirstCall, callback := cp.joinConcurrency(chainID, height)
 
 	if isFirstCall {
 		var err error
@@ -131,8 +133,8 @@ func (cp *ConcurrentProvider) UpdateToHeight(chainID string, height int64) error
 		}()
 		err = cp.UpdatingProvider.UpdateToHeight(chainID, height)
 		return err
-	} else {
-		// Is not the first call, so return the error from previous concurrent calls.
-		return presult.err
 	}
+
+	// Is not the first call, so return the error from previous concurrent calls.
+	return pr.err
 }
