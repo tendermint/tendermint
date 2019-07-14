5d1459b5847d65069b776be3b4ba5e81bc933dcc
Marko <marbar3778@yahoo.com>
2019-07-14 15:50:33 +0200

    libs/fail: clean up `fail.go` (#3785)
    * Clean up `fail.go`
    
    - Clean up fail.go
    - Remove unneeded Func `FailRand()`
    
    closes #2729
    
    Signed-off-by: Marko Baricevic <marbar3778@yahoo.com>
    
    * add in commented lines

--- libs/fail/fail.go
+++ libs/fail/fail.go
@@ -2,36 +2,30 @@ package fail
 
 import (
 	"fmt"
-	"math/rand"
 	"os"
 	"strconv"
 )
 
-var callIndexToFail int
-
-func init() {
+func envSet() int {
 	callIndexToFailS := os.Getenv("FAIL_TEST_INDEX")
 
 	if callIndexToFailS == "" {
-		callIndexToFail = -1
+		return -1
 	} else {
 		var err error
-		callIndexToFail, err = strconv.Atoi(callIndexToFailS)
+		callIndexToFail, err := strconv.Atoi(callIndexToFailS)
 		if err != nil {
-			callIndexToFail = -1
+			return -1
 		}
+		return callIndexToFail
 	}
 }
 
 // Fail when FAIL_TEST_INDEX == callIndex
-var (
-	callIndex int //indexes Fail calls
-
-	callRandIndex       int  // indexes a run of FailRand calls
-	callRandIndexToFail = -1 // the callRandIndex to fail on in FailRand
-)
+var callIndex int //indexes Fail calls
 
 func Fail() {
+	callIndexToFail := envSet()
 	if callIndexToFail < 0 {
 		return
 	}
@@ -41,33 +35,6 @@ func Fail() {
 	}
 
 	callIndex += 1
-}
-
-// FailRand should be called n successive times.
-// It will fail on a random one of those calls
-// n must be greater than 0
-func FailRand(n int) {
-	if callIndexToFail < 0 {
-		return
-	}
-
-	if callRandIndexToFail < 0 {
-		// first call in the loop, pick a random index to fail at
-		callRandIndexToFail = rand.Intn(n)
-		callRandIndex = 0
-	}
-
-	if callIndex == callIndexToFail {
-		if callRandIndex == callRandIndexToFail {
-			Exit()
-		}
-	}
-
-	callRandIndex += 1
-
-	if callRandIndex == n {
-		callIndex += 1
-	}
 }
 
 func Exit() {
