8a282a5fee50fea850f722e6c8b6abf8bccf4d18
Marko <marbar3778@yahoo.com>
2019-08-11 19:03:40 +0200

    replace errors.go with github.com/pkg/errors (2/2) (#3890)
    * init of (2/2) common errors
    
    * Remove instances of cmn.Error (2/2)
    
    - Replace usage of cmnError and errorWrap
    - ref #3862
    
    Signed-off-by: Marko Baricevic <marbar3778@yahoo.com>
    
    * comment wording
    
    * simplify IsErrXXX functions
    
    * log panic along with stopping the MConnection

--- lite/proxy/errors.go
+++ lite/proxy/errors.go
@@ -1,7 +1,7 @@
 package proxy
 
 import (
-	cmn "github.com/tendermint/tendermint/libs/common"
+	"github.com/pkg/errors"
 )
 
 type errNoData struct{}
@@ -12,13 +12,10 @@ func (e errNoData) Error() string {
 
 // IsErrNoData checks whether an error is due to a query returning empty data
 func IsErrNoData(err error) bool {
-	if err_, ok := err.(cmn.Error); ok {
-		_, ok := err_.Data().(errNoData)
-		return ok
-	}
-	return false
+	_, ok := errors.Cause(err).(errNoData)
+	return ok
 }
 
 func ErrNoData() error {
-	return cmn.ErrorWrap(errNoData{}, "")
+	return errors.Wrap(errNoData{}, "")
 }
