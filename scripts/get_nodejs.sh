94bc7a3b3c3d81e154a188172ab8e250d63899a8
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-08-12 12:57:38 +0200

    xz not supported by cci image, let's keep it simple
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- scripts/get_nodejs.sh
+++ scripts/get_nodejs.sh
@@ -5,8 +5,8 @@ NODE_FULL=node-${VERSION}-linux-x64
 
 mkdir -p ~/.local/bin
 mkdir -p ~/.local/node
-wget http://nodejs.org/dist/${VERSION}/${NODE_FULL}.tar.xz -O ~/.local/node/${NODE_FULL}.tar.xz
-tar -xzf ~/.local/node/${NODE_FULL}.tar.xz -C ~/.local/node/
+wget http://nodejs.org/dist/${VERSION}/${NODE_FULL}.tar.gz -O ~/.local/node/${NODE_FULL}.tar.gz
+tar -xzf ~/.local/node/${NODE_FULL}.tar.gz -C ~/.local/node/
 ln -s ~/.local/node/${NODE_FULL}/bin/node ~/.local/bin/node
 ln -s ~/.local/node/${NODE_FULL}/bin/npm ~/.local/bin/npm
 export PATH=~/.local/bin:$PATH
