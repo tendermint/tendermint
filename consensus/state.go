7368ba358b0508ad5f37b6066fe9dcbef098e51a
Marko <marbar3778@yahoo.com>
2019-12-19 18:04:57 +0100

    prometheus/metrics: three new metrics for consensus (#4263)
    * prometheus/metrics: two new metrics for consensus
    
    - add consensus_validator_power metric so if a node is a validator it can see its own power in prometheus
    - add last_signed_height metric so if a node is a validator it can be aware of at which height the most recent time it signed.
    
    - closes: #3773
    - closes: #3083
    
    Signed-off-by: Marko Baricevic <marbar3778@yahoo.com>
    
    * check if signature is present
    
    * minor change and check if sig is not absent
    
    * add changelog entry
    
    * Update consensus/state.go
    
    Co-Authored-By: Anton Kaliaev <anton.kalyaev@gmail.com>
    
    * Update CHANGELOG_PENDING.md
    
    Co-Authored-By: Anton Kaliaev <anton.kalyaev@gmail.com>
    
    * Update CHANGELOG_PENDING.md
    
    * add label with validator address
    
    * change address to vali_address
    
    * add metric missed blocks
    
    * add changelog entry for missed blocks metric
    
    * address naming & add docs

--- consensus/state.go
+++ consensus/state.go
@@ -1486,6 +1486,19 @@ func (cs *State) recordMetrics(height int64, block *types.Block) {
 
 		for i, val := range cs.LastValidators.Validators {
 			commitSig := block.LastCommit.Signatures[i]
+			privValAddress := cs.privValidator.GetPubKey().Address()
+			if cs.privValidator != nil && bytes.Equal(val.Address, privValAddress) {
+				label := []string{
+					"validator_address", privValAddress.String(),
+				}
+				cs.metrics.ValidatorPower.With(label...).Set(float64(val.VotingPower))
+				if !commitSig.Absent() {
+					cs.metrics.ValidatorLastSignedHeight.With(label...).Set(float64(height))
+				} else {
+					cs.metrics.ValidatorMissedBlocks.With(label...).Add(float64(1))
+				}
+			}
+
 			if commitSig.Absent() {
 				missingValidators++
 				missingValidatorsPower += val.VotingPower
