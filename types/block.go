d373a8bd10f354f154b0e03804d6d86814193b79
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-01-07 11:39:23 +0400

    refactor CommitSig#ForBlock

--- types/block.go
+++ types/block.go
@@ -466,6 +466,11 @@ func NewCommitSigForBlock(signature []byte, valAddr Address, ts time.Time) Commi
 	}
 }
 
+// ForBlock returns true if CommitSig is for the block.
+func (cs CommitSig) ForBlock() bool {
+	return cs.BlockIDFlag == BlockIDFlagCommit
+}
+
 // NewCommitSigAbsent returns new CommitSig with BlockIDFlagAbsent. Other
 // fields are all empty.
 func NewCommitSigAbsent() CommitSig {
@@ -477,15 +482,6 @@ func NewCommitSigAbsent() CommitSig {
 // Absent returns true if CommitSig is absent.
 func (cs CommitSig) Absent() bool {
 	return cs.BlockIDFlag == BlockIDFlagAbsent
-}
-
-func (cs CommitSig) ForBlock() bool {
-	switch cs.BlockIDFlag {
-	case BlockIDFlagAbsent, BlockIDFlagNil:
-		return true
-	default:
-		return false
-	}
 }
 
 func (cs CommitSig) String() string {
