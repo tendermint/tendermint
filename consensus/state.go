36c86ff8972658dc7fd3dedbdcaaa7390354913f
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-01-09 14:05:16 +0400

    cs: check if cs.privValidator is nil
    before getting its address in recordMetrics
    
    Refs https://github.com/tendermint/tendermint/pull/4294#discussion_r364347586

--- consensus/state.go
+++ consensus/state.go
@@ -1486,10 +1486,14 @@ func (cs *State) recordMetrics(height int64, block *types.Block) {
 
 		for i, val := range cs.LastValidators.Validators {
 			commitSig := block.LastCommit.Signatures[i]
-			privValAddress := cs.privValidator.GetPubKey().Address()
-			if cs.privValidator != nil && bytes.Equal(val.Address, privValAddress) {
+			if commitSig.Absent() {
+				missingValidators++
+				missingValidatorsPower += val.VotingPower
+			}
+
+			if cs.privValidator != nil && bytes.Equal(val.Address, cs.privValidator.GetPubKey().Address()) {
 				label := []string{
-					"validator_address", privValAddress.String(),
+					"validator_address", val.Address.String(),
 				}
 				cs.metrics.ValidatorPower.With(label...).Set(float64(val.VotingPower))
 				if commitSig.ForBlock() {
@@ -1497,11 +1501,6 @@ func (cs *State) recordMetrics(height int64, block *types.Block) {
 				} else {
 					cs.metrics.ValidatorMissedBlocks.With(label...).Add(float64(1))
 				}
-			}
-
-			if commitSig.Absent() {
-				missingValidators++
-				missingValidatorsPower += val.VotingPower
 			}
 		}
 	}
