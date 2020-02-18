e385be7900bb5cf472a17def544db5254f38978d
Sean Braithwaite <brapse@gmail.com>
2020-02-18 17:14:52 +0100

    panic on duplicate block enqueued by processor

--- blockchain/v2/processor.go
+++ blockchain/v2/processor.go
@@ -87,6 +87,9 @@ func (state *pcState) synced() bool {
 }
 
 func (state *pcState) enqueue(peerID p2p.ID, block *types.Block, height int64) {
+	if _, ok := state.queue[height]; ok {
+		panic("duplicate block enqueued by processesor")
+	}
 	state.queue[height] = queueItem{block: block, peerID: peerID}
 }
 
