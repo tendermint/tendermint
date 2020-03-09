75bb4585fddd947de383d3d0c8f2cd1ef8366e0e
Marko Baricevic <marbar3778@yahoo.com>
2020-03-09 16:12:38 +0100

    change enum names

--- mempool/clist_mempool.go
+++ mempool/clist_mempool.go
@@ -602,7 +602,7 @@ func (mem *CListMempool) recheckTxs() {
 		memTx := e.Value.(*mempoolTx)
 		mem.proxyAppConn.CheckTxAsync(abci.RequestCheckTx{
 			Tx:   memTx.tx,
-			Type: abci.CheckTxType_Recheck,
+			Type: abci.CheckTxType_RECHECK,
 		})
 	}
 
