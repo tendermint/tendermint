9f546c1b9305190bbbe054274fbeda64e5957662
Marko <marbar3778@yahoo.com>
2020-03-13 14:41:06 +0100

    Apply suggestions from code review
    Co-Authored-By: Anton Kaliaev <anton.kalyaev@gmail.com>

--- crypto/ed25519/ed25519.go
+++ crypto/ed25519/ed25519.go
@@ -19,9 +19,9 @@ var _ crypto.PrivKey = PrivKey{}
 const (
 	PrivKeyAminoName = "tendermint/PrivKeyEd25519"
 	PubKeyAminoName  = "tendermint/PubKeyEd25519"
-	// PubKeySize is the number of bytes in an Ed25519 signature.
+	// PubKeySize is is the size, in bytes, of public keys as used in this package.
 	PubKeySize = 32
-	// PrivateKeySize is the size, in bytes, of private keys
+	// PrivateKeySize is the size, in bytes, of private keys as used in this package.
 	PrivateKeySize = 64
 	// Size of an Edwards25519 signature. Namely the size of a compressed
 	// Edwards25519 point, and a field element. Both of which are 32 bytes.
