790359f6f3d1ceb7d8b38a6a5c7b300322b6246f
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-03-16 17:16:06 +0400

    rpc/core: validate incoming evidence!

--- rpc/core/evidence.go
+++ rpc/core/evidence.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"github.com/pkg/errors"
+
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 	rpctypes "github.com/tendermint/tendermint/rpc/lib/types"
 	"github.com/tendermint/tendermint/types"
@@ -9,9 +11,13 @@ import (
 // BroadcastEvidence broadcasts evidence of the misbehavior.
 // More: https://docs.tendermint.com/master/rpc/#/Info/broadcast_evidence
 func BroadcastEvidence(ctx *rpctypes.Context, ev types.Evidence) (*ctypes.ResultBroadcastEvidence, error) {
-	err := evidencePool.AddEvidence(ev)
-	if err != nil {
+	if err := ev.ValidateBasic(); err != nil {
+		return nil, errors.Wrap(err, "evidence.ValidateBasic failed")
+	}
+
+	if err := evidencePool.AddEvidence(ev); err != nil {
 		return nil, err
 	}
+
 	return &ctypes.ResultBroadcastEvidence{Hash: ev.Hash()}, nil
 }
