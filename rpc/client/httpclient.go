bfa5973048d8065caa2e51e248a133d1ed1c29e4
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-03-16 14:10:13 +0400

    rpc/client: remove unnecessary error wrapping

--- rpc/client/httpclient.go
+++ rpc/client/httpclient.go
@@ -183,7 +183,7 @@ func (c *baseRPCClient) Status() (*ctypes.ResultStatus, error) {
 	result := new(ctypes.ResultStatus)
 	_, err := c.caller.Call("status", map[string]interface{}{}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "Status")
+		return nil, err
 	}
 	return result, nil
 }
@@ -192,7 +192,7 @@ func (c *baseRPCClient) ABCIInfo() (*ctypes.ResultABCIInfo, error) {
 	result := new(ctypes.ResultABCIInfo)
 	_, err := c.caller.Call("abci_info", map[string]interface{}{}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "ABCIInfo")
+		return nil, err
 	}
 	return result, nil
 }
@@ -210,7 +210,7 @@ func (c *baseRPCClient) ABCIQueryWithOptions(
 		map[string]interface{}{"path": path, "data": data, "height": opts.Height, "prove": opts.Prove},
 		result)
 	if err != nil {
-		return nil, errors.Wrap(err, "ABCIQuery")
+		return nil, err
 	}
 	return result, nil
 }
@@ -219,7 +219,7 @@ func (c *baseRPCClient) BroadcastTxCommit(tx types.Tx) (*ctypes.ResultBroadcastT
 	result := new(ctypes.ResultBroadcastTxCommit)
 	_, err := c.caller.Call("broadcast_tx_commit", map[string]interface{}{"tx": tx}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "broadcast_tx_commit")
+		return nil, err
 	}
 	return result, nil
 }
@@ -236,7 +236,7 @@ func (c *baseRPCClient) broadcastTX(route string, tx types.Tx) (*ctypes.ResultBr
 	result := new(ctypes.ResultBroadcastTx)
 	_, err := c.caller.Call(route, map[string]interface{}{"tx": tx}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, route)
+		return nil, err
 	}
 	return result, nil
 }
@@ -245,7 +245,7 @@ func (c *baseRPCClient) UnconfirmedTxs(limit int) (*ctypes.ResultUnconfirmedTxs,
 	result := new(ctypes.ResultUnconfirmedTxs)
 	_, err := c.caller.Call("unconfirmed_txs", map[string]interface{}{"limit": limit}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "unconfirmed_txs")
+		return nil, err
 	}
 	return result, nil
 }
@@ -254,7 +254,7 @@ func (c *baseRPCClient) NumUnconfirmedTxs() (*ctypes.ResultUnconfirmedTxs, error
 	result := new(ctypes.ResultUnconfirmedTxs)
 	_, err := c.caller.Call("num_unconfirmed_txs", map[string]interface{}{}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "num_unconfirmed_txs")
+		return nil, err
 	}
 	return result, nil
 }
@@ -263,7 +263,7 @@ func (c *baseRPCClient) NetInfo() (*ctypes.ResultNetInfo, error) {
 	result := new(ctypes.ResultNetInfo)
 	_, err := c.caller.Call("net_info", map[string]interface{}{}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "NetInfo")
+		return nil, err
 	}
 	return result, nil
 }
@@ -272,7 +272,7 @@ func (c *baseRPCClient) DumpConsensusState() (*ctypes.ResultDumpConsensusState, 
 	result := new(ctypes.ResultDumpConsensusState)
 	_, err := c.caller.Call("dump_consensus_state", map[string]interface{}{}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "DumpConsensusState")
+		return nil, err
 	}
 	return result, nil
 }
@@ -290,7 +290,7 @@ func (c *baseRPCClient) ConsensusParams(height *int64) (*ctypes.ResultConsensusP
 	result := new(ctypes.ResultConsensusParams)
 	_, err := c.caller.Call("consensus_params", map[string]interface{}{"height": height}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "ConsensusParams")
+		return nil, err
 	}
 	return result, nil
 }
@@ -299,7 +299,7 @@ func (c *baseRPCClient) Health() (*ctypes.ResultHealth, error) {
 	result := new(ctypes.ResultHealth)
 	_, err := c.caller.Call("health", map[string]interface{}{}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "Health")
+		return nil, err
 	}
 	return result, nil
 }
@@ -310,7 +310,7 @@ func (c *baseRPCClient) BlockchainInfo(minHeight, maxHeight int64) (*ctypes.Resu
 		map[string]interface{}{"minHeight": minHeight, "maxHeight": maxHeight},
 		result)
 	if err != nil {
-		return nil, errors.Wrap(err, "BlockchainInfo")
+		return nil, err
 	}
 	return result, nil
 }
@@ -319,7 +319,7 @@ func (c *baseRPCClient) Genesis() (*ctypes.ResultGenesis, error) {
 	result := new(ctypes.ResultGenesis)
 	_, err := c.caller.Call("genesis", map[string]interface{}{}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "Genesis")
+		return nil, err
 	}
 	return result, nil
 }
@@ -328,7 +328,7 @@ func (c *baseRPCClient) Block(height *int64) (*ctypes.ResultBlock, error) {
 	result := new(ctypes.ResultBlock)
 	_, err := c.caller.Call("block", map[string]interface{}{"height": height}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "Block")
+		return nil, err
 	}
 	return result, nil
 }
@@ -337,7 +337,7 @@ func (c *baseRPCClient) BlockResults(height *int64) (*ctypes.ResultBlockResults,
 	result := new(ctypes.ResultBlockResults)
 	_, err := c.caller.Call("block_results", map[string]interface{}{"height": height}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "Block Result")
+		return nil, err
 	}
 	return result, nil
 }
@@ -346,7 +346,7 @@ func (c *baseRPCClient) Commit(height *int64) (*ctypes.ResultCommit, error) {
 	result := new(ctypes.ResultCommit)
 	_, err := c.caller.Call("commit", map[string]interface{}{"height": height}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "Commit")
+		return nil, err
 	}
 	return result, nil
 }
@@ -376,7 +376,7 @@ func (c *baseRPCClient) TxSearch(query string, prove bool, page, perPage int, or
 	}
 	_, err := c.caller.Call("tx_search", params, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "TxSearch")
+		return nil, err
 	}
 	return result, nil
 }
@@ -389,7 +389,7 @@ func (c *baseRPCClient) Validators(height *int64, page, perPage int) (*ctypes.Re
 		"per_page": perPage,
 	}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "Validators")
+		return nil, err
 	}
 	return result, nil
 }
@@ -398,7 +398,7 @@ func (c *baseRPCClient) BroadcastEvidence(ev types.Evidence) (*ctypes.ResultBroa
 	result := new(ctypes.ResultBroadcastEvidence)
 	_, err := c.caller.Call("broadcast_evidence", map[string]interface{}{"evidence": ev}, result)
 	if err != nil {
-		return nil, errors.Wrap(err, "BroadcastEvidence")
+		return nil, err
 	}
 	return result, nil
 }
