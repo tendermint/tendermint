72f78cc937e2cce07afe4ebd88324b43ee9d661e
Erik Grinaker <erik@interchain.berlin>
2020-03-27 10:14:45 +0100

    Improved error message
    Co-Authored-By: Anton Kaliaev <anton.kalyaev@gmail.com>

--- consensus/state.go
+++ consensus/state.go
@@ -883,7 +883,7 @@ func (cs *State) needProofBlock(height int64) bool {
 
 	lastBlockMeta := cs.blockStore.LoadBlockMeta(height - 1)
 	if lastBlockMeta == nil {
-		panic("needProofBlock: last block meta not found")
+		panic(fmt.Sprintf("needProofBlock: last block meta for height %d not found", height-1))
 	}
 	return !bytes.Equal(cs.state.AppHash, lastBlockMeta.Header.AppHash)
 }
