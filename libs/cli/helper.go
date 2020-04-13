1c71733140cea63b99b2d1cd57656e4cb5399d3c
Alessio Treglia <alessio@tendermint.com>
2020-04-13 13:08:23 +0100

    cli: add command to generate shell completion scripts (#4665)
    How to use it:
    
    ```
    $ . <(tendermint completion)
    ```
    
    Note that the completion command does not show up in the help screen,
    though it comes with its own --help option.
    
    This is a port of the feature provided by cosmos-sdk.

--- libs/cli/helper.go
+++ libs/cli/helper.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+
+	"github.com/spf13/cobra"
 )
 
 // WriteConfigVals writes a toml file with the given values.
@@ -84,4 +86,43 @@ func RunCaptureWithArgs(cmd Executable, args []string, env map[string]string) (s
 	stdout = <-*outC
 	stderr = <-*errC
 	return stdout, stderr, err
+}
+
+// NewCompletionCmd returns a cobra.Command that generates bash and zsh
+// completion scripts for the given root command. If hidden is true, the
+// command will not show up in the root command's list of available commands.
+func NewCompletionCmd(rootCmd *cobra.Command, hidden bool) *cobra.Command {
+	flagZsh := "zsh"
+	cmd := &cobra.Command{
+		Use:   "completion",
+		Short: "Generate shell completion scripts",
+		Long: fmt.Sprintf(`Generate Bash and Zsh completion scripts and print them to STDOUT.
+
+Once saved to file, a completion script can be loaded in the shell's
+current session as shown:
+
+   $ . <(%s completion)
+
+To configure your bash shell to load completions for each session add to
+your $HOME/.bashrc or $HOME/.profile the following instruction:
+
+   . <(%s completion)
+`, rootCmd.Use, rootCmd.Use),
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			zsh, err := cmd.Flags().GetBool(flagZsh)
+			if err != nil {
+				return err
+			}
+			if zsh {
+				return rootCmd.GenZshCompletion(cmd.OutOrStdout())
+			}
+			return rootCmd.GenBashCompletion(cmd.OutOrStdout())
+		},
+		Hidden: hidden,
+		Args:   cobra.NoArgs,
+	}
+
+	cmd.Flags().Bool(flagZsh, false, "Generate Zsh completion script")
+
+	return cmd
 }
