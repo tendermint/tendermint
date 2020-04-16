0cb4cf23ea66371e5e2aba6807dcb9f5d88b40d5
Erik Grinaker <erik@interchain.berlin>
2020-04-16 16:20:17 +0200

    config: add statesync:info logging by default

--- config/config.go
+++ config/config.go
@@ -305,7 +305,7 @@ func DefaultLogLevel() string {
 // DefaultPackageLogLevels returns a default log level setting so all packages
 // log at "error", while the `state` and `main` packages log at "info"
 func DefaultPackageLogLevels() string {
-	return fmt.Sprintf("main:info,state:info,*:%s", DefaultLogLevel())
+	return fmt.Sprintf("main:info,state:info,statesync:info,*:%s", DefaultLogLevel())
 }
 
 //-----------------------------------------------------------------------------
