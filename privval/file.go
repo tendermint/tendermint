f31f4327b5cee09e90fb6034a3970e4df14dddf5
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-04-28 12:51:51 +0400

    privval: remove deprecated `OldFilePV`
    The old format was deprecated in v0.28. It's time we remove it.

--- privval/file.go
+++ privval/file.go
@@ -32,7 +32,7 @@ func voteToStep(vote *types.Vote) int8 {
 	case types.PrecommitType:
 		return stepPrecommit
 	default:
-		panic("Unknown vote type")
+		panic(fmt.Sprintf("Unknown vote type: %v", vote.Type))
 	}
 }
 
