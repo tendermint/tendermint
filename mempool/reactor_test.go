e5c619ae1284f2424da04c060b80081cae5cfaa4
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-05-26 11:07:45 +0400

    add comment

--- mempool/reactor_test.go
+++ mempool/reactor_test.go
@@ -37,6 +37,10 @@ func (ps peerState) GetHeight() int64 {
 // be received in the others.
 func TestReactorBroadcastTxMessage(t *testing.T) {
 	config := cfg.TestConfig()
+	// if there were more than two reactors, the order of transactions cannot be
+	// asserted in waitForTxsOnReactors (due to transactions gossiping). If we
+	// replace Connect2Switches (full mesh) with a func, which connects first
+	// reactor to others and nothing else, this test should also pass.
 	const N = 2
 	reactors := makeAndConnectReactors(config, N)
 	defer func() {
