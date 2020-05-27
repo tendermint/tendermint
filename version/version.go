e22acc0f451805e9ceef588561b5877be0fb1614
Tess Rinearson <tess.rinearson@gmail.com>
2020-05-27 17:42:57 +0200

    prepare v0.33.5 RC2

--- version/version.go
+++ version/version.go
@@ -21,7 +21,7 @@ const (
 	// XXX: Don't change the name of this variable or you will break
 	// automation :)
 
-	TMCoreSemVer = "0.33.4"
+	TMCoreSemVer = "0.33.5"
 
 	// ABCISemVer is the semantic version of the ABCI library
 	ABCISemVer  = "0.16.2"
