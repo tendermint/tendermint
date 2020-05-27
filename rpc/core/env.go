celestia-org/celestia-core

rpc/core: do not lock ConsensusState mutex
in /validators, /consensus_params and /status

Closes #3161

rpc/core/env.go
@@ -129,3 +129,32 @@ func validateSkipCount(page, perPage int) int {
 
 	return skipCount
 }
+
+// latestHeight can be either latest committed or uncommitted (+1) height.
+func getHeight(latestHeight int64, heightPtr *int64) (int64, error) {
+	if heightPtr != nil {
+		height := *heightPtr
+		if height <= 0 {
+			return 0, fmt.Errorf("height must be greater than 0, but got %d", height)
+		}
+		if height > latestHeight {
+			return 0, fmt.Errorf("height %d must be less than or equal to the current blockchain height %d",
+				height, latestHeight)
+		}
+		base := env.BlockStore.Base()
+		if height < base {
+			return 0, fmt.Errorf("height %v is not available, blocks pruned at height %v",
+				height, base)
+		}
+		return height, nil
+	}
+	return latestHeight, nil
+}
+
+func latestUncommittedHeight() int64 {
+	nodeIsSyncing := env.ConsensusReactor.WaitSync()
+	if nodeIsSyncing {
+		return env.BlockStore.Height()
+	}
+	return env.BlockStore.Height() + 1
+}

tendermint/tendermint

rpc/core/env.go
@@ -129,3 +129,32 @@ func validateSkipCount(page, perPage int) int {
 
 	return skipCount
 }
+
+// latestHeight can be either latest committed or uncommitted (+1) height.
+func getHeight(latestHeight int64, heightPtr *int64) (int64, error) {
+	if heightPtr != nil {
+		height := *heightPtr
+		if height <= 0 {
+			return 0, fmt.Errorf("height must be greater than 0, but got %d", height)
+		}
+		if height > latestHeight {
+			return 0, fmt.Errorf("height %d must be less than or equal to the current blockchain height %d",
+				height, latestHeight)
+		}
+		base := env.BlockStore.Base()
+		if height < base {
+			return 0, fmt.Errorf("height %v is not available, blocks pruned at height %v",
+				height, base)
+		}
+		return height, nil
+	}
+	return latestHeight, nil
+}
+
+func latestUncommittedHeight() int64 {
+	nodeIsSyncing := env.ConsensusReactor.FastSync()
+	if nodeIsSyncing {
+		return env.BlockStore.Height()
+	}
+	return env.BlockStore.Height() + 1
+}