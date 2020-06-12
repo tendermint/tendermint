071b703f7831cd8debcbb24f7da078a8e21d1483
Marko Baricevic <marbar3778@yahoo.com>
2020-06-12 17:00:19 +0200

    remove codec

--- state/main_test.go
+++ state/main_test.go
@@ -3,11 +3,8 @@ package state_test
 import (
 	"os"
 	"testing"
-
-	"github.com/tendermint/tendermint/types"
 )
 
 func TestMain(m *testing.M) {
-	types.RegisterMockEvidencesGlobal()
 	os.Exit(m.Run())
 }
