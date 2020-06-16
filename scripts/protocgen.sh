884b4925b01153397f54509a68d92e7259c1804c
Marko Baricevic <marbar3778@yahoo.com>
2020-06-16 12:28:03 +0200

    move all proto types into proto dir

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,10 +2,19 @@
 
 set -eo pipefail
 
-proto_dirs=$(find . -path ./third_party -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   protoc \
   -I. \
-  --gogofaster_out=Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/golang/protobuf/ptypes/duration,plugins=grpc,paths=source_relative:. \
+  --gogofaster_out=\
+Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/duration.proto=github.com/golang/protobuf/ptypes/duration,\
+plugins=grpc,paths=source_relative:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
 done
+
+cp -r proto/abci/types.pb.go ./abci/types
+rm -rf proto/abci/types.pb.go
+
+cp -r proto/rpc/types.pb.go ./rpc/grpc
+rm -rf proto/rpc/types.pb.go
