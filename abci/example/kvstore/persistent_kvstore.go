3b83e787429d053e705b736fb65dd2fb48aaf7ff
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-06-26 10:51:43 +0400

    format code

--- abci/example/kvstore/persistent_kvstore.go
+++ abci/example/kvstore/persistent_kvstore.go
@@ -125,7 +125,7 @@ func (app *PersistentKVStoreApplication) BeginBlock(req types.RequestBeginBlock)
 	// reset valset changes
 	app.ValUpdates = make([]types.ValidatorUpdate, 0)
 
-  // Remove validators who committed equivocation from the set.
+	// Remove validators who committed equivocation from the set.
 	for _, ev := range req.ByzantineValidators {
 		if ev.Type == tmtypes.ABCIEvidenceTypeDuplicateVote {
 			app.updateValidator(types.ValidatorUpdate{
