f351ff1abc9587e13eeb9a87f2372ca289c632bf
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-07-03 16:56:10 +0400

    version: bump p2p and block protocol versions
    "It looks like we neglected to bump both protocol versions with v0.33.0.
    They were bumped to Block Protocol version 10 and P2P Protocol version 7
    in the v0.30.0 release, and no breaking changes were made until v0.33.0.
    We should have bumped them to 11 and 8 respectively in v0.33.0. In the
    upcoming v0.34.0, we should probably bump them to 12 and 9 then to make
    up for it."
    
    Refs #5057

--- version/version.go
+++ version/version.go
@@ -31,9 +31,9 @@ const (
 var (
 	// P2PProtocol versions all p2p behaviour and msgs.
 	// This includes proposer selection.
-	P2PProtocol uint64 = 8
+	P2PProtocol uint64 = 9
 
 	// BlockProtocol versions all block data structures and processing.
 	// This includes validity of blocks and state updates.
-	BlockProtocol uint64 = 11
+	BlockProtocol uint64 = 12
 )
