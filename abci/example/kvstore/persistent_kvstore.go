008389931c2cd6fff29dea1bd530674c03ae4988
Erik Grinaker <erik@interchain.berlin>
2020-07-08 16:21:20 +0200

    proto: move crypto/keys and crypto/merkle into crypto/

--- abci/example/kvstore/persistent_kvstore.go
+++ abci/example/kvstore/persistent_kvstore.go
@@ -13,7 +13,7 @@ import (
 	"github.com/tendermint/tendermint/abci/types"
 	cryptoenc "github.com/tendermint/tendermint/crypto/encoding"
 	"github.com/tendermint/tendermint/libs/log"
-	pc "github.com/tendermint/tendermint/proto/tendermint/crypto/keys"
+	pc "github.com/tendermint/tendermint/proto/tendermint/crypto"
 	tmtypes "github.com/tendermint/tendermint/types"
 )
 
