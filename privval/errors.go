340a7e0d428db051b310f911be80e034c3fb8b6b
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-07-21 13:28:04 +0400

    privval: change ErrUnexpectedResponse description

--- privval/errors.go
+++ privval/errors.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 )
 
+// EndpointTimeoutError occurs when endpoint times out.
 type EndpointTimeoutError struct{}
 
 // Implement the net.Error interface.
@@ -14,12 +15,11 @@ func (e EndpointTimeoutError) Temporary() bool { return true }
 
 // Socket errors.
 var (
-	ErrUnexpectedResponse = errors.New("received unexpected response")
-	ErrNoConnection       = errors.New("endpoint is not connected")
 	ErrConnectionTimeout  = EndpointTimeoutError{}
-
-	ErrReadTimeout  = errors.New("endpoint read timed out")
-	ErrWriteTimeout = errors.New("endpoint write timed out")
+	ErrNoConnection       = errors.New("endpoint is not connected")
+	ErrReadTimeout        = errors.New("endpoint read timed out")
+	ErrUnexpectedResponse = errors.New("empty response")
+	ErrWriteTimeout       = errors.New("endpoint write timed out")
 )
 
 // RemoteSignerError allows (remote) validators to include meaningful error
