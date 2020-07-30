d4acdefaff8ca3bbe350544c1908a5d0c1053d79
Erik Grinaker <erik@interchain.berlin>
2020-07-30 14:06:50 +0200

    version: bump version numbers

--- version/version.go
+++ version/version.go
@@ -20,7 +20,7 @@ const (
 	// Must be a string because scripts like dist.sh read this file.
 	// XXX: Don't change the name of this variable or you will break
 	// automation :)
-	TMCoreSemVer = "0.33.6"
+	TMCoreSemVer = "0.34.0"
 
 	// ABCISemVer is the semantic version of the ABCI library
 	ABCISemVer = "0.17.0"
@@ -31,9 +31,9 @@ const (
 var (
 	// P2PProtocol versions all p2p behaviour and msgs.
 	// This includes proposer selection.
-	P2PProtocol uint64 = 9
+	P2PProtocol uint64 = 8
 
 	// BlockProtocol versions all block data structures and processing.
 	// This includes validity of blocks and state updates.
-	BlockProtocol uint64 = 12
+	BlockProtocol uint64 = 11
 )
