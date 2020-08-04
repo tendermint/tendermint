987d1996fecb258044599e703caf316edc38fa85
hanyunx <hanyunx2@gmail.com>
2020-08-03 17:24:19 -0700

    Minor

--- proto/tendermint/state/utils.go
+++ proto/tendermint/state/utils.go
@@ -5,15 +5,15 @@ import "github.com/tendermint/tendermint/abci/types"
 func (m *ABCIResponses) ValidatorUpdates() types.ValidatorUpdates {
 	if m.EndBlock.ValidatorUpdates != nil {
 		return m.EndBlock.ValidatorUpdates
-	} else {
-		return m.DeliverBlock.ValidatorUpdates
 	}
+	return m.DeliverBlock.ValidatorUpdates
+
 }
 
 func (m *ABCIResponses) ConsensusParamUpdates() *types.ConsensusParams {
 	if m.EndBlock.ConsensusParamUpdates != nil {
 		return m.EndBlock.ConsensusParamUpdates
-	} else {
-		return m.DeliverBlock.ConsensusParamUpdates
 	}
+	return m.DeliverBlock.ConsensusParamUpdates
+
 }
