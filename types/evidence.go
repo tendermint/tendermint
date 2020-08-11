351b1f016a6e196a43e18b18fc44cbd7ffcf18a7
Callum Michael Waters <cmwaters19@gmail.com>
2020-08-11 12:52:27 +0200

    remove validator index verification

--- types/evidence.go
+++ types/evidence.go
@@ -270,15 +270,6 @@ func (dve *DuplicateVoteEvidence) Verify(chainID string, pubKey crypto.PubKey) e
 		)
 	}
 
-	// Index must be the same
-	if dve.VoteA.ValidatorIndex != dve.VoteB.ValidatorIndex {
-		return fmt.Errorf(
-			"validator indices do not match: %d and %d",
-			dve.VoteA.ValidatorIndex,
-			dve.VoteB.ValidatorIndex,
-		)
-	}
-
 	// BlockIDs must be different
 	if dve.VoteA.BlockID.Equals(dve.VoteB.BlockID) {
 		return fmt.Errorf(
@@ -1002,16 +993,6 @@ func (e *PotentialAmnesiaEvidence) ValidateBasic() error {
 		return fmt.Errorf("validator addresses do not match: %X vs %X",
 			e.VoteA.ValidatorAddress,
 			e.VoteB.ValidatorAddress,
-		)
-	}
-
-	// Index must be the same
-	// https://github.com/tendermint/tendermint/issues/4619
-	if e.VoteA.ValidatorIndex != e.VoteB.ValidatorIndex {
-		return fmt.Errorf(
-			"duplicateVoteEvidence Error: Validator indices do not match. Got %d and %d",
-			e.VoteA.ValidatorIndex,
-			e.VoteB.ValidatorIndex,
 		)
 	}
 
