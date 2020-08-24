7a2a398cb5807d41714c08658d216a0a1b097f39
Marko Baricevic <marbar3778@yahoo.com>
2020-08-24 17:12:43 +0200

    remove secp256k1 import

--- p2p/conn/secret_connection_test.go
+++ p2p/conn/secret_connection_test.go
@@ -19,7 +19,7 @@ import (
 
 	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/crypto/ed25519"
-	"github.com/tendermint/tendermint/crypto/secp256k1"
+	"github.com/tendermint/tendermint/crypto/sr25519"
 	"github.com/tendermint/tendermint/libs/async"
 	tmos "github.com/tendermint/tendermint/libs/os"
 	tmrand "github.com/tendermint/tendermint/libs/rand"
@@ -276,7 +276,7 @@ func TestNonEd25519Pubkey(t *testing.T) {
 	defer fooConn.Close()
 	defer barConn.Close()
 	var fooPrvKey = ed25519.GenPrivKey()
-	var barPrvKey = secp256k1.GenPrivKey()
+	var barPrvKey = sr25519.GenPrivKey()
 
 	go MakeSecretConnection(fooConn, fooPrvKey) //nolint:errcheck // ignore for tests
 
