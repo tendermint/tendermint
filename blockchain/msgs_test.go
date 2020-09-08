05d057f77835d225096b72cb4c734dfd3b333d25
Marko Baricevic <marbar3778@yahoo.com>
2020-09-08 16:21:50 +0200

    overwrite updated version

--- blockchain/msgs_test.go
+++ blockchain/msgs_test.go
@@ -81,6 +81,7 @@ func TestBcStatusResponseMessageValidateBasic(t *testing.T) {
 // nolint:lll // ignore line length in tests
 func TestBlockchainMessageVectors(t *testing.T) {
 	block := types.MakeBlock(int64(3), []types.Tx{types.Tx("Hello World")}, nil, nil)
+	block.Version.Block = 11 // overwrite updated protocol version
 
 	bpb, err := block.ToProto()
 	require.NoError(t, err)
