dbb4c0a52d53b9899b69d16bd10101fae52dbcd3
Erik Grinaker <erik@interchain.berlin>
2020-09-22 15:58:31 +0200

    comment on block store mutex semantics

--- store/store.go
+++ store/store.go
@@ -33,6 +33,11 @@ The store can be assumed to contain all contiguous blocks between base and heigh
 type BlockStore struct {
 	db dbm.DB
 
+	// mtx guards access to the struct fields listed below it. We rely on the database to enforce
+	// fine-grained concurrency control for its data, and thus this mutex does not apply to
+	// database contents. The only reason for keeping these fields in the struct is that the data
+	// can't efficiently be queried from the database since the key encoding we use is not
+	// lexicographically ordered (see https://github.com/tendermint/tendermint/issues/4567).
 	mtx    tmsync.RWMutex
 	base   int64
 	height int64
