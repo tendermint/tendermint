250c3aa92e89b745f9d423a6ea3f21041eddba39
Erik Grinaker <erik@interchain.berlin>
2020-10-05 11:35:01 +0200

    test: add end-to-end testing framework (#5435)
    Partial fix for #5291. For details, see [README.md](https://github.com/tendermint/tendermint/blob/erik/e2e-tests/test/e2e/README.md) and [RFC-001](https://github.com/tendermint/tendermint/blob/master/docs/rfc/rfc-001-end-to-end-testing.md).
    
    This only includes a single test case under `test/e2e/tests/`, as a proof of concept - additional test cases will be submitted separately. A randomized testnet generator will also be submitted separately, there a currently just a handful of static testnets under `test/e2e/networks/`. This will eventually replace the current P2P tests and run in CI.

---
+++ test/e2e/app/state.go
@@ -0,0 +1,155 @@
+//nolint: gosec
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"sync"
+)
+
+// State is the application state.
+type State struct {
+	sync.RWMutex
+	Height uint64
+	Values map[string]string
+	Hash   []byte
+
+	// private fields aren't marshalled to disk.
+	file            string
+	persistInterval uint64
+	initialHeight   uint64
+}
+
+// NewState creates a new state.
+func NewState(file string, persistInterval uint64) (*State, error) {
+	state := &State{
+		Values:          make(map[string]string),
+		file:            file,
+		persistInterval: persistInterval,
+	}
+	state.Hash = hashItems(state.Values)
+	err := state.load()
+	switch {
+	case errors.Is(err, os.ErrNotExist):
+	case err != nil:
+		return nil, err
+	}
+	return state, nil
+}
+
+// load loads state from disk. It does not take out a lock, since it is called
+// during construction.
+func (s *State) load() error {
+	bz, err := ioutil.ReadFile(s.file)
+	if err != nil {
+		return fmt.Errorf("failed to read state from %q: %w", s.file, err)
+	}
+	err = json.Unmarshal(bz, s)
+	if err != nil {
+		return fmt.Errorf("invalid state data in %q: %w", s.file, err)
+	}
+	return nil
+}
+
+// save saves the state to disk. It does not take out a lock since it is called
+// internally by Commit which does lock.
+func (s *State) save() error {
+	bz, err := json.Marshal(s)
+	if err != nil {
+		return fmt.Errorf("failed to marshal state: %w", err)
+	}
+	// We write the state to a separate file and move it to the destination, to
+	// make it atomic.
+	newFile := fmt.Sprintf("%v.new", s.file)
+	err = ioutil.WriteFile(newFile, bz, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write state to %q: %w", s.file, err)
+	}
+	return os.Rename(newFile, s.file)
+}
+
+// Export exports key/value pairs as JSON, used for state sync snapshots.
+func (s *State) Export() ([]byte, error) {
+	s.RLock()
+	defer s.RUnlock()
+	return json.Marshal(s.Values)
+}
+
+// Import imports key/value pairs from JSON bytes, used for InitChain.AppStateBytes and
+// state sync snapshots. It also saves the state once imported.
+func (s *State) Import(height uint64, jsonBytes []byte) error {
+	s.Lock()
+	defer s.Unlock()
+	values := map[string]string{}
+	err := json.Unmarshal(jsonBytes, &values)
+	if err != nil {
+		return fmt.Errorf("failed to decode imported JSON data: %w", err)
+	}
+	s.Height = height
+	s.Values = values
+	s.Hash = hashItems(values)
+	return s.save()
+}
+
+// Get fetches a value. A missing value is returned as an empty string.
+func (s *State) Get(key string) string {
+	s.RLock()
+	defer s.RUnlock()
+	return s.Values[key]
+}
+
+// Set sets a value. Setting an empty value is equivalent to deleting it.
+func (s *State) Set(key, value string) {
+	s.Lock()
+	defer s.Unlock()
+	if value == "" {
+		delete(s.Values, key)
+	} else {
+		s.Values[key] = value
+	}
+}
+
+// Commit commits the current state.
+func (s *State) Commit() (uint64, []byte, error) {
+	s.Lock()
+	defer s.Unlock()
+	s.Hash = hashItems(s.Values)
+	switch {
+	case s.Height > 0:
+		s.Height++
+	case s.initialHeight > 0:
+		s.Height = s.initialHeight
+	default:
+		s.Height = 1
+	}
+	if s.persistInterval > 0 && s.Height%s.persistInterval == 0 {
+		err := s.save()
+		if err != nil {
+			return 0, nil, err
+		}
+	}
+	return s.Height, s.Hash, nil
+}
+
+// hashItems hashes a set of key/value items.
+func hashItems(items map[string]string) []byte {
+	keys := make([]string, 0, len(items))
+	for key := range items {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+
+	hasher := sha256.New()
+	for _, key := range keys {
+		_, _ = hasher.Write([]byte(key))
+		_, _ = hasher.Write([]byte{0})
+		_, _ = hasher.Write([]byte(items[key]))
+		_, _ = hasher.Write([]byte{0})
+	}
+	return hasher.Sum(nil)
+}
---
+++ test/e2e/runner/setup.go
@@ -0,0 +1,360 @@
+// nolint: gosec
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	"github.com/tendermint/tendermint/config"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+	"github.com/tendermint/tendermint/p2p"
+	"github.com/tendermint/tendermint/privval"
+	e2e "github.com/tendermint/tendermint/test/e2e/pkg"
+	"github.com/tendermint/tendermint/types"
+)
+
+const (
+	AppAddressTCP  = "tcp://127.0.0.1:30000"
+	AppAddressUNIX = "unix:///var/run/app.sock"
+
+	PrivvalAddressTCP     = "tcp://0.0.0.0:27559"
+	PrivvalAddressUNIX    = "unix:///var/run/privval.sock"
+	PrivvalKeyFile        = "config/priv_validator_key.json"
+	PrivvalStateFile      = "data/priv_validator_state.json"
+	PrivvalDummyKeyFile   = "config/dummy_validator_key.json"
+	PrivvalDummyStateFile = "data/dummy_validator_state.json"
+)
+
+// Setup sets up the testnet configuration.
+func Setup(testnet *e2e.Testnet) error {
+	logger.Info(fmt.Sprintf("Generating testnet files in %q", testnet.Dir))
+
+	err := os.MkdirAll(testnet.Dir, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	compose, err := MakeDockerCompose(testnet)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(filepath.Join(testnet.Dir, "docker-compose.yml"), compose, 0644)
+	if err != nil {
+		return err
+	}
+
+	genesis, err := MakeGenesis(testnet)
+	if err != nil {
+		return err
+	}
+
+	for _, node := range testnet.Nodes {
+		nodeDir := filepath.Join(testnet.Dir, node.Name)
+		dirs := []string{
+			filepath.Join(nodeDir, "config"),
+			filepath.Join(nodeDir, "data"),
+			filepath.Join(nodeDir, "data", "app"),
+		}
+		for _, dir := range dirs {
+			err := os.MkdirAll(dir, 0755)
+			if err != nil {
+				return err
+			}
+		}
+
+		err = genesis.SaveAs(filepath.Join(nodeDir, "config", "genesis.json"))
+		if err != nil {
+			return err
+		}
+
+		cfg, err := MakeConfig(node)
+		if err != nil {
+			return err
+		}
+		config.WriteConfigFile(filepath.Join(nodeDir, "config", "config.toml"), cfg) // panics
+
+		appCfg, err := MakeAppConfig(node)
+		if err != nil {
+			return err
+		}
+		err = ioutil.WriteFile(filepath.Join(nodeDir, "config", "app.toml"), appCfg, 0644)
+		if err != nil {
+			return err
+		}
+
+		err = (&p2p.NodeKey{PrivKey: node.Key}).SaveAs(filepath.Join(nodeDir, "config", "node_key.json"))
+		if err != nil {
+			return err
+		}
+
+		(privval.NewFilePV(node.Key,
+			filepath.Join(nodeDir, PrivvalKeyFile),
+			filepath.Join(nodeDir, PrivvalStateFile),
+		)).Save()
+
+		// Set up a dummy validator. Tendermint requires a file PV even when not used, so we
+		// give it a dummy such that it will fail if it actually tries to use it.
+		(privval.NewFilePV(ed25519.GenPrivKey(),
+			filepath.Join(nodeDir, PrivvalDummyKeyFile),
+			filepath.Join(nodeDir, PrivvalDummyStateFile),
+		)).Save()
+	}
+
+	return nil
+}
+
+// MakeDockerCompose generates a Docker Compose config for a testnet.
+func MakeDockerCompose(testnet *e2e.Testnet) ([]byte, error) {
+	// Must use version 2 Docker Compose format, to support IPv6.
+	tmpl, err := template.New("docker-compose").Parse(`version: '2.4'
+
+networks:
+  {{ .Name }}:
+    driver: bridge
+{{- if .IPv6 }}
+    enable_ipv6: true
+{{- end }}
+    ipam:
+      driver: default
+      config:
+      - subnet: {{ .IP }}
+
+services:
+{{- range .Nodes }}
+  {{ .Name }}:
+    container_name: {{ .Name }}
+    image: tendermint/e2e-node
+{{- if eq .ABCIProtocol "builtin" }}
+    entrypoint: /usr/bin/entrypoint-builtin
+{{- end }}
+    init: true
+    ports:
+    - 26656
+    - {{ if .ProxyPort }}{{ .ProxyPort }}:{{ end }}26657
+    volumes:
+    - ./{{ .Name }}:/tendermint
+    networks:
+      {{ $.Name }}:
+        ipv{{ if $.IPv6 }}6{{ else }}4{{ end}}_address: {{ .IP }}
+
+{{end}}`)
+	if err != nil {
+		return nil, err
+	}
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, testnet)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// MakeGenesis generates a genesis document.
+func MakeGenesis(testnet *e2e.Testnet) (types.GenesisDoc, error) {
+	genesis := types.GenesisDoc{
+		GenesisTime:     time.Now(),
+		ChainID:         testnet.Name,
+		ConsensusParams: types.DefaultConsensusParams(),
+		InitialHeight:   testnet.InitialHeight,
+	}
+	for validator, power := range testnet.Validators {
+		genesis.Validators = append(genesis.Validators, types.GenesisValidator{
+			Name:    validator.Name,
+			Address: validator.Key.PubKey().Address(),
+			PubKey:  validator.Key.PubKey(),
+			Power:   power,
+		})
+	}
+	// The validator set will be sorted internally by Tendermint ranked by power,
+	// but we sort it here as well so that all genesis files are identical.
+	sort.Slice(genesis.Validators, func(i, j int) bool {
+		return strings.Compare(genesis.Validators[i].Name, genesis.Validators[j].Name) == -1
+	})
+	if len(testnet.InitialState) > 0 {
+		appState, err := json.Marshal(testnet.InitialState)
+		if err != nil {
+			return genesis, err
+		}
+		genesis.AppState = appState
+	}
+	return genesis, genesis.ValidateAndComplete()
+}
+
+// MakeConfig generates a Tendermint config for a node.
+func MakeConfig(node *e2e.Node) (*config.Config, error) {
+	cfg := config.DefaultConfig()
+	cfg.Moniker = node.Name
+	cfg.ProxyApp = AppAddressTCP
+	cfg.RPC.ListenAddress = "tcp://0.0.0.0:26657"
+	cfg.P2P.ExternalAddress = fmt.Sprintf("tcp://%v", node.AddressP2P(false))
+	cfg.P2P.AddrBookStrict = false
+	cfg.DBBackend = node.Database
+	cfg.StateSync.DiscoveryTime = 5 * time.Second
+
+	switch node.ABCIProtocol {
+	case e2e.ProtocolUNIX:
+		cfg.ProxyApp = AppAddressUNIX
+	case e2e.ProtocolTCP:
+		cfg.ProxyApp = AppAddressTCP
+	case e2e.ProtocolGRPC:
+		cfg.ProxyApp = AppAddressTCP
+		cfg.ABCI = "grpc"
+	case e2e.ProtocolBuiltin:
+		cfg.ProxyApp = ""
+		cfg.ABCI = ""
+	default:
+		return nil, fmt.Errorf("unexpected ABCI protocol setting %q", node.ABCIProtocol)
+	}
+
+	// Tendermint errors if it does not have a privval key set up, regardless of whether
+	// it's actually needed (e.g. for remote KMS or non-validators). We set up a dummy
+	// key here by default, and use the real key for actual validators that should use
+	// the file privval.
+	cfg.PrivValidatorListenAddr = ""
+	cfg.PrivValidatorKey = PrivvalDummyKeyFile
+	cfg.PrivValidatorState = PrivvalDummyStateFile
+
+	switch node.Mode {
+	case e2e.ModeValidator:
+		switch node.PrivvalProtocol {
+		case e2e.ProtocolFile:
+			cfg.PrivValidatorKey = PrivvalKeyFile
+			cfg.PrivValidatorState = PrivvalStateFile
+		case e2e.ProtocolUNIX:
+			cfg.PrivValidatorListenAddr = PrivvalAddressUNIX
+		case e2e.ProtocolTCP:
+			cfg.PrivValidatorListenAddr = PrivvalAddressTCP
+		default:
+			return nil, fmt.Errorf("invalid privval protocol setting %q", node.PrivvalProtocol)
+		}
+	case e2e.ModeSeed:
+		cfg.P2P.SeedMode = true
+		cfg.P2P.PexReactor = true
+	case e2e.ModeFull:
+		// Don't need to do anything, since we're using a dummy privval key by default.
+	default:
+		return nil, fmt.Errorf("unexpected mode %q", node.Mode)
+	}
+
+	if node.FastSync == "" {
+		cfg.FastSyncMode = false
+	} else {
+		cfg.FastSync.Version = node.FastSync
+	}
+
+	if node.StateSync {
+		cfg.StateSync.Enable = true
+		cfg.StateSync.RPCServers = []string{}
+		for _, peer := range node.Testnet.ArchiveNodes() {
+			if peer.Name == node.Name {
+				continue
+			}
+			cfg.StateSync.RPCServers = append(cfg.StateSync.RPCServers, peer.AddressRPC())
+		}
+		if len(cfg.StateSync.RPCServers) < 2 {
+			return nil, errors.New("unable to find 2 suitable state sync RPC servers")
+		}
+	}
+
+	cfg.P2P.Seeds = ""
+	for _, seed := range node.Seeds {
+		if len(cfg.P2P.Seeds) > 0 {
+			cfg.P2P.Seeds += ","
+		}
+		cfg.P2P.Seeds += seed.AddressP2P(true)
+	}
+	cfg.P2P.PersistentPeers = ""
+	for _, peer := range node.PersistentPeers {
+		if len(cfg.P2P.PersistentPeers) > 0 {
+			cfg.P2P.PersistentPeers += ","
+		}
+		cfg.P2P.PersistentPeers += peer.AddressP2P(true)
+	}
+	return cfg, nil
+}
+
+// MakeAppConfig generates an ABCI application config for a node.
+func MakeAppConfig(node *e2e.Node) ([]byte, error) {
+	cfg := map[string]interface{}{
+		"chain_id":          node.Testnet.Name,
+		"dir":               "data/app",
+		"listen":            AppAddressUNIX,
+		"protocol":          "socket",
+		"persist_interval":  node.PersistInterval,
+		"snapshot_interval": node.SnapshotInterval,
+		"retain_blocks":     node.RetainBlocks,
+	}
+	switch node.ABCIProtocol {
+	case e2e.ProtocolUNIX:
+		cfg["listen"] = AppAddressUNIX
+	case e2e.ProtocolTCP:
+		cfg["listen"] = AppAddressTCP
+	case e2e.ProtocolGRPC:
+		cfg["listen"] = AppAddressTCP
+		cfg["protocol"] = "grpc"
+	case e2e.ProtocolBuiltin:
+		delete(cfg, "listen")
+		cfg["protocol"] = "builtin"
+	default:
+		return nil, fmt.Errorf("unexpected ABCI protocol setting %q", node.ABCIProtocol)
+	}
+	switch node.PrivvalProtocol {
+	case e2e.ProtocolFile:
+	case e2e.ProtocolTCP:
+		cfg["privval_server"] = PrivvalAddressTCP
+		cfg["privval_key"] = PrivvalKeyFile
+		cfg["privval_state"] = PrivvalStateFile
+	case e2e.ProtocolUNIX:
+		cfg["privval_server"] = PrivvalAddressUNIX
+		cfg["privval_key"] = PrivvalKeyFile
+		cfg["privval_state"] = PrivvalStateFile
+	default:
+		return nil, fmt.Errorf("unexpected privval protocol setting %q", node.PrivvalProtocol)
+	}
+
+	if len(node.Testnet.ValidatorUpdates) > 0 {
+		validatorUpdates := map[string]map[string]int64{}
+		for height, validators := range node.Testnet.ValidatorUpdates {
+			updateVals := map[string]int64{}
+			for node, power := range validators {
+				updateVals[base64.StdEncoding.EncodeToString(node.Key.PubKey().Bytes())] = power
+			}
+			validatorUpdates[fmt.Sprintf("%v", height)] = updateVals
+		}
+		cfg["validator_update"] = validatorUpdates
+	}
+
+	var buf bytes.Buffer
+	err := toml.NewEncoder(&buf).Encode(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate app config: %w", err)
+	}
+	return buf.Bytes(), nil
+}
+
+// UpdateConfigStateSync updates the state sync config for a node.
+func UpdateConfigStateSync(node *e2e.Node, height int64, hash []byte) error {
+	cfgPath := filepath.Join(node.Testnet.Dir, node.Name, "config", "config.toml")
+
+	// FIXME Apparently there's no function to simply load a config file without
+	// involving the entire Viper apparatus, so we'll just resort to regexps.
+	bz, err := ioutil.ReadFile(cfgPath)
+	if err != nil {
+		return err
+	}
+	bz = regexp.MustCompile(`(?m)^trust_height =.*`).ReplaceAll(bz, []byte(fmt.Sprintf(`trust_height = %v`, height)))
+	bz = regexp.MustCompile(`(?m)^trust_hash =.*`).ReplaceAll(bz, []byte(fmt.Sprintf(`trust_hash = "%X"`, hash)))
+	return ioutil.WriteFile(cfgPath, bz, 0644)
+}
