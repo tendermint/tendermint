9daa374de3a2cff897f50f6bee421f6bb4c5e846
Marko Baricevic <marbar3778@yahoo.com>
2020-10-12 17:56:34 +0200

    ignore some linting

--- abci/tests/test_app/main.go
+++ abci/tests/test_app/main.go
@@ -68,7 +68,7 @@ func testCounter() {
 	}()
 
 	if err := ensureABCIIsUp(abciType, maxABCIConnectTries); err != nil {
-		log.Fatalf("echo failed: %v", err)
+		log.Fatalf("echo failed: %v", err) //nolint:gocritic
 	}
 
 	client := startClient(abciType)
