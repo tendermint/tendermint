13474c9d1b2142a4e69789c6b365175e2411c837
Erik Grinaker <erik@interchain.berlin>
2020-10-15 20:40:19 +0200

    use single container to clean up

--- test/e2e/runner/cleanup.go
+++ test/e2e/runner/cleanup.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	e2e "github.com/tendermint/tendermint/test/e2e/pkg"
 )
@@ -29,11 +30,14 @@ func Cleanup(testnet *e2e.Testnet) error {
 	// On Linux, some local files in the volume will be owned by root since Tendermint
 	// runs as root inside the container, so we need to clean them up from within a
 	// container running as root too.
-	for _, node := range testnet.Nodes {
-		err = execCompose(testnet.Dir, "run", "--entrypoint", "", node.Name, "sh", "-c", "rm -rf /tendermint/*")
-		if err != nil {
-			return err
-		}
+	absDir, err := filepath.Abs(testnet.Dir)
+	if err != nil {
+		return err
+	}
+	err = execDocker("run", "--entrypoint", "", "-v", fmt.Sprintf("%v:/network", absDir),
+		"tendermint/e2e-node", "sh", "-c", "rm -rf /network/*/")
+	if err != nil {
+		return err
 	}
 
 	err = execCompose(testnet.Dir, "down")
