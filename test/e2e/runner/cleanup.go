7b8e375ee570f618c8f5c45283c363919fbc0413
Erik Grinaker <erik@interchain.berlin>
2020-10-15 15:31:54 +0200

    test: clean up e2e container volumes using a container

--- test/e2e/runner/cleanup.go
+++ test/e2e/runner/cleanup.go
@@ -21,6 +21,21 @@ func Cleanup(testnet *e2e.Testnet) error {
 	}
 
 	logger.Info("Removing Docker containers and networks")
+	err = execCompose(testnet.Dir, "stop")
+	if err != nil {
+		return err
+	}
+
+	// On Linux, some local files in the volume will be owned by root since Tendermint
+	// runs as root inside the container, so we need to clean them up from within a
+	// container running as root too.
+	for _, node := range testnet.Nodes {
+		err = execCompose(testnet.Dir, "run", "--entrypoint", "", node.Name, "rm", "-rf", "/tendermint/*")
+		if err != nil {
+			return err
+		}
+	}
+
 	err = execCompose(testnet.Dir, "down")
 	if err != nil {
 		return err
