10dda219a1b2e7ef17d2a47f5b3279f7245bdd26
Erik Grinaker <erik@interchain.berlin>
2020-10-26 11:09:46 +0100

    test: fix handling of start height in generated E2E testnets (#5563)
    In #5488 the E2E testnet generator changed to setting explicit `StartAt` heights for initial nodes. This broke the runner, which expected all initial nodes to have `StartAt: 0`, as well as validator set scheduling in the generator. Testnet loading now normalizes initial nodes to have `StartAt: 0`.
    
    This also tweaks waiting for misbehavior heights to only use an additional wait if there actually is any misbehavior in the testnet, and to output information when waiting.

--- test/e2e/runner/wait.go
+++ test/e2e/runner/wait.go
@@ -7,8 +7,6 @@ import (
 	e2e "github.com/tendermint/tendermint/test/e2e/pkg"
 )
 
-const interphaseWaitPeriod = 5
-
 // Wait waits for a number of blocks to be produced, and for all nodes to catch
 // up with it.
 func Wait(testnet *e2e.Testnet, blocks int64) error {
@@ -16,30 +14,15 @@ func Wait(testnet *e2e.Testnet, blocks int64) error {
 	if err != nil {
 		return err
 	}
-	waitFor := block.Height + blocks
-	logger.Info(fmt.Sprintf("Waiting for all nodes to reach height %v...", waitFor))
-	_, err = waitForAllNodes(testnet, waitFor, 20*time.Second)
+	return WaitUntil(testnet, block.Height+blocks)
+}
+
+// WaitUntil waits until a given height has been reached.
+func WaitUntil(testnet *e2e.Testnet, height int64) error {
+	logger.Info(fmt.Sprintf("Waiting for all nodes to reach height %v...", height))
+	_, err := waitForAllNodes(testnet, height, 20*time.Second)
 	if err != nil {
 		return err
 	}
 	return nil
-}
-
-// WaitForAllMisbehaviors calculates the height of the last misbehavior and ensures the entire
-// testnet has surpassed this height before moving on to the next phase
-func waitForAllMisbehaviors(testnet *e2e.Testnet) error {
-	_, _, err := waitForHeight(testnet, lastMisbehaviorHeight(testnet))
-	return err
-}
-
-func lastMisbehaviorHeight(testnet *e2e.Testnet) int64 {
-	lastHeight := testnet.InitialHeight
-	for _, n := range testnet.Nodes {
-		for height := range n.Misbehaviors {
-			if height > lastHeight {
-				lastHeight = height
-			}
-		}
-	}
-	return lastHeight + interphaseWaitPeriod
 }
