8da685b5fde459e696928da8f0e90ebb7bb0b71e
Marko Baricevic <marbar3778@yahoo.com>
2020-10-27 15:05:55 +0100

    remove upload count

--- version/version.go
+++ version/version.go
@@ -17,7 +17,6 @@ var (
 	// P2PProtocol versions all p2p behaviour and msgs.
 	// This includes proposer selection.
 	P2PProtocol uint64 = 8
-
 	// BlockProtocol versions all block data structures and processing.
 	// This includes validity of blocks and state updates.
 	BlockProtocol uint64 = 11
