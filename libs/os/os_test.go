6a497f802da840d02c562f6a4f0d2613ccb3f81d
Alessio Treglia <alessio@tendermint.com>
2020-11-10 18:59:42 +0000

    test TrapSignal
    little bonus: testcase for Kill()

--- libs/os/os_test.go
+++ libs/os/os_test.go
@@ -1,11 +1,16 @@
-package os
+package os_test
 
 import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"syscall"
 	"testing"
+	"time"
+
+	tmos "github.com/tendermint/tendermint/libs/os"
 )
 
 func TestCopyFile(t *testing.T) {
@@ -20,7 +25,7 @@ func TestCopyFile(t *testing.T) {
 	}
 
 	copyfile := fmt.Sprintf("%s.copy", tmpfile.Name())
-	if err := CopyFile(tmpfile.Name(), copyfile); err != nil {
+	if err := tmos.CopyFile(tmpfile.Name(), copyfile); err != nil {
 		t.Fatal(err)
 	}
 	if _, err := os.Stat(copyfile); os.IsNotExist(err) {
@@ -34,4 +39,48 @@ func TestCopyFile(t *testing.T) {
 		t.Fatalf("copy file content differs: expected %v, got %v", content, data)
 	}
 	os.Remove(copyfile)
+}
+
+func TestTrapSignal(t *testing.T) {
+	if os.Getenv("TRAP_SIGNAL_TEST") == "1" {
+		t.Log("inside test process")
+		killer()
+		return
+	}
+
+	cmd := exec.Command(os.Args[0], "-test.run="+t.Name())
+	mockStderr := bytes.NewBufferString("")
+	cmd.Env = append(os.Environ(), "TRAP_SIGNAL_TEST=1")
+	cmd.Stderr = mockStderr
+
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		want := int(syscall.SIGTERM) + 128
+		if e.ExitCode() != int(syscall.SIGTERM)+128 {
+			t.Fatalf("wrong exit code, want %d, got %d", want, e.ExitCode())
+		}
+
+		return
+	}
+
+	t.Fatal("this error should not be triggered")
+
+}
+
+type mockLogger struct{}
+
+func (ml mockLogger) Info(msg string, keyvals ...interface{}) {}
+
+func killer() {
+	logger := mockLogger{}
+
+	tmos.TrapSignal(logger, nil)
+	time.Sleep(1 * time.Second)
+
+	// use Kill() to test SIGTERM
+	if err := tmos.Kill(); err != nil {
+		panic(err)
+	}
+
+	time.Sleep(1 * time.Second)
 }
