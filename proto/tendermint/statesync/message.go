07f7970134c04cc4d605754c2d9c630986cac906
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-11-16 12:38:27 -0500

    p2p: wrapper msg logic for state sync

---
+++ proto/tendermint/statesync/message.go
@@ -0,0 +1,50 @@
+package statesync
+
+import (
+	fmt "fmt"
+
+	proto "github.com/gogo/protobuf/proto"
+)
+
+// Wrap implements the p2p Wrapper interface and wraps a state sync messages.
+func (m *Message) Wrap(msg proto.Message) error {
+	switch msg := msg.(type) {
+	case *ChunkRequest:
+		m.Sum = &Message_ChunkRequest{ChunkRequest: msg}
+
+	case *ChunkResponse:
+		m.Sum = &Message_ChunkResponse{ChunkResponse: msg}
+
+	case *SnapshotsRequest:
+		m.Sum = &Message_SnapshotsRequest{SnapshotsRequest: msg}
+
+	case *SnapshotsResponse:
+		m.Sum = &Message_SnapshotsResponse{SnapshotsResponse: msg}
+
+	default:
+		return fmt.Errorf("unknown message: %T", msg)
+	}
+
+	return nil
+}
+
+// Unwrap implements the p2p Wrapper interface and unwraps a wrapped state sync
+// message.
+func (m *Message) Unwrap() (proto.Message, error) {
+	switch msg := m.Sum.(type) {
+	case *Message_ChunkRequest:
+		return m.GetChunkRequest(), nil
+
+	case *Message_ChunkResponse:
+		return m.GetChunkResponse(), nil
+
+	case *Message_SnapshotsRequest:
+		return m.GetSnapshotsRequest(), nil
+
+	case *Message_SnapshotsResponse:
+		return m.GetSnapshotsResponse(), nil
+
+	default:
+		return nil, fmt.Errorf("unknown message: %T", msg)
+	}
+}
--- statesync/messages.go
+++ statesync/messages.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 
+	"github.com/tendermint/tendermint/p2p"
 	ssproto "github.com/tendermint/tendermint/proto/tendermint/statesync"
 )
 
@@ -15,6 +16,9 @@ const (
 	// chunkMsgSize is the maximum size of a chunkResponseMessage
 	chunkMsgSize = int(16e6)
 )
+
+// assert Wrapper interface implementation of the state sync proto message type.
+var _ p2p.Wrapper = (*ssproto.Message)(nil)
 
 // mustEncodeMsg encodes a Protobuf message, panicing on error.
 func mustEncodeMsg(pb proto.Message) []byte {
