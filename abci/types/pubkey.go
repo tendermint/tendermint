00f4830fdacc0a89784b9f1aba66303e9a3eb4cd
Marko Baricevic <marbar3778@yahoo.com>
2020-11-16 12:17:29 +0100

    dont break the api

--- abci/types/pubkey.go
+++ abci/types/pubkey.go
@@ -1,36 +1,44 @@
 package types
 
 import (
+	fmt "fmt"
+
 	"github.com/tendermint/tendermint/crypto/ed25519"
 	cryptoenc "github.com/tendermint/tendermint/crypto/encoding"
 	"github.com/tendermint/tendermint/crypto/secp256k1"
-	tmcrypto "github.com/tendermint/tendermint/proto/tendermint/crypto"
 )
 
-func UpdateValidator(pk []byte, power int64, keyType string) ValidatorUpdate {
-	var (
-		pkp tmcrypto.PublicKey
-		err error
-	)
+func Ed25519ValidatorUpdate(pk []byte, power int64) ValidatorUpdate {
+	pke := ed25519.PubKey(pk)
 
-	switch keyType {
-	case "", ed25519.KeyType:
-		pke := ed25519.PubKey(pk)
-		pkp, err = cryptoenc.PubKeyToProto(pke)
-		if err != nil {
-			panic(err)
-		}
-	case secp256k1.KeyType:
-		pke := secp256k1.PubKey(pk)
-		pkp, err = cryptoenc.PubKeyToProto(pke)
-		if err != nil {
-			panic(err)
-		}
+	pkp, err := cryptoenc.PubKeyToProto(pke)
+	if err != nil {
+		panic(err)
 	}
 
 	return ValidatorUpdate{
 		// Address:
 		PubKey: pkp,
 		Power:  power,
+	}
+}
+
+func UpdateValidator(pk []byte, power int64, keyType string) ValidatorUpdate {
+	switch keyType {
+	case "", ed25519.KeyType:
+		return Ed25519ValidatorUpdate(pk, power)
+	case secp256k1.KeyType:
+		pke := secp256k1.PubKey(pk)
+		pkp, err := cryptoenc.PubKeyToProto(pke)
+		if err != nil {
+			panic(err)
+		}
+		return ValidatorUpdate{
+			// Address:
+			PubKey: pkp,
+			Power:  power,
+		}
+	default:
+		panic(fmt.Sprintf("key type %s not supported", keyType))
 	}
 }
