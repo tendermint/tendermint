e0ebeb3d55dbc28f54667b2c57fd9020ecccc820
Marko <marbar3778@yahoo.com>
2020-11-17 14:55:23 +0100

    e2e: use ed25519 for secretConn (remote signer) (#5678)
    ## Description
    
    Hardcode ed25519 to dialTCPFn in e2e tests. 
    
    I will backport `DefaultRequestHandler` fixes
    
    This will be replaced when grpc is implemented.

--- test/e2e/app/main.go
+++ test/e2e/app/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/tendermint/tendermint/abci/server"
 	"github.com/tendermint/tendermint/config"
+	"github.com/tendermint/tendermint/crypto/ed25519"
 	tmflags "github.com/tendermint/tendermint/libs/cli/flags"
 	"github.com/tendermint/tendermint/libs/log"
 	tmnet "github.com/tendermint/tendermint/libs/net"
@@ -174,7 +175,7 @@ func startSigner(cfg *Config) error {
 	var dialFn privval.SocketDialer
 	switch protocol {
 	case "tcp":
-		dialFn = privval.DialTCPFn(address, 3*time.Second, filePV.Key.PrivKey)
+		dialFn = privval.DialTCPFn(address, 3*time.Second, ed25519.GenPrivKey())
 	case "unix":
 		dialFn = privval.DialUnixFn(address)
 	default:
