9ea136d50bbc7741bad0b5f4547d40b46798aa5e
Cyrus Goh <hello@lovincyrus.com>
2020-12-04 13:46:48 -0800

    docs: remove sidebar nav versions

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -57,17 +57,6 @@ module.exports = {
               path: 'https://docs.tendermint.com/master/rpc/',
               static: true
             },
-            // TODO: remove once https://github.com/cosmos/vuepress-theme-cosmos/issues/91 is closed
-            {
-              title: 'Version 0.32',
-              path: '/v0.32',
-              static: true
-            },
-            {
-              title: 'Version 0.33',
-              path: '/v0.33',
-              static: true
-            },
           ]
         }
       ]
