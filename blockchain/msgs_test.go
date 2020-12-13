dashpay/tenderdash

rename variables

blockchain/msgs_test.go
@@ -80,9 +80,9 @@ func TestBcStatusResponseMessageValidateBasic(t *testing.T) {
 
 // nolint:lll // ignore line length in tests
 func TestBlockchainMessageVectors(t *testing.T) {
-	chainLock := types.NewMockChainLock(1)
+	coreChainLock := types.NewMockChainLock(1)
 
-	block := types.MakeBlock(int64(3),chainLock.BlockHeight,&chainLock,[]types.Tx{types.Tx("Hello World")}, nil, nil)
+	block := types.MakeBlock(int64(3), coreChainLock.BlockHeight, &coreChainLock, []types.Tx{ types.Tx("Hello World") }, nil, nil)
 	block.Version.Block = 11 // overwrite updated protocol version
 
 

blockchain/v1/peer_test.go
@@ -276,6 +276,7 @@ func checkByStoppingPeerTimer(t *testing.T, peer *BpPeer, running bool) {
 }
 
 func makeSmallBlock(height int) *types.Block {
-	chainLock := types.NewMockChainLock(1)
-	return types.MakeBlock(int64(height),chainLock.BlockHeight, &chainLock, []types.Tx{types.Tx("foo")}, nil, nil)
+	coreChainLock := types.NewMockChainLock(1)
+
+	return types.MakeBlock(int64(height), coreChainLock.BlockHeight, &coreChainLock, []types.Tx{types.Tx("foo")}, nil, nil)
 }

blockchain/v1/pool_test.go
@@ -75,14 +75,16 @@ func makeBlockPool(bcr *testBcR, height int64, peers []BpPeer, blocks map[int64]
 		bPool.peers[p.ID].SetLogger(bcr.logger)
 
 	}
-	chainLock := types.NewMockChainLock(1)
+
+	coreChainLock := types.NewMockChainLock(1)
+
 	bPool.MaxPeerHeight = maxH
 	for h, p := range blocks {
 		bPool.blocks[h] = p.id
 		bPool.peers[p.id].RequestSent(h)
 		if p.create {
 			// simulate that a block at height h has been received
-			_ = bPool.peers[p.id].AddBlock(types.MakeBlock(h,chainLock.BlockHeight, &chainLock, txs, nil, nil), 100)
+			_ = bPool.peers[p.id].AddBlock(types.MakeBlock(h, coreChainLock.BlockHeight, &coreChainLock, txs, nil, nil), 100)
 		}
 	}
 	return bPool
@@ -376,7 +378,8 @@ func TestBlockPoolSendRequestBatch(t *testing.T) {
 func TestBlockPoolAddBlock(t *testing.T) {
 	testBcR := newTestBcR()
 	txs := []types.Tx{types.Tx("foo"), types.Tx("bar")}
-	chainLock := types.NewMockChainLock(1)
+
+	coreChainLock := types.NewMockChainLock(1)
 
 	type args struct {
 		peerID    p2p.ID
@@ -394,7 +397,7 @@ func TestBlockPoolAddBlock(t *testing.T) {
 			pool: makeBlockPool(testBcR, 10, []BpPeer{{ID: "P1", Height: 100}}, map[int64]tPBlocks{}),
 			args: args{
 				peerID:    "P2",
-				block:     types.MakeBlock(int64(10),chainLock.BlockHeight, &chainLock, txs, nil, nil),
+				block:     types.MakeBlock(int64(10), coreChainLock.BlockHeight, &coreChainLock, txs, nil, nil),
 				blockSize: 100,
 			},
 			poolWanted: makeBlockPool(testBcR, 10, []BpPeer{{ID: "P1", Height: 100}}, map[int64]tPBlocks{}),
@@ -406,7 +409,7 @@ func TestBlockPoolAddBlock(t *testing.T) {
 				map[int64]tPBlocks{10: {"P1", false}}),
 			args: args{
 				peerID:    "P1",
-				block:     types.MakeBlock(int64(11),chainLock.BlockHeight, &chainLock, txs, nil, nil),
+				block:     types.MakeBlock(int64(11), coreChainLock.BlockHeight, &coreChainLock, txs, nil, nil),
 				blockSize: 100,
 			},
 			poolWanted: makeBlockPool(testBcR, 10,
@@ -420,7 +423,7 @@ func TestBlockPoolAddBlock(t *testing.T) {
 				map[int64]tPBlocks{10: {"P1", true}, 11: {"P1", false}}),
 			args: args{
 				peerID:    "P1",
-				block:     types.MakeBlock(int64(10),chainLock.BlockHeight, &chainLock, txs, nil, nil),
+				block:     types.MakeBlock(int64(10), coreChainLock.BlockHeight, &coreChainLock, txs, nil, nil),
 				blockSize: 100,
 			},
 			poolWanted: makeBlockPool(testBcR, 10,
@@ -434,7 +437,7 @@ func TestBlockPoolAddBlock(t *testing.T) {
 				map[int64]tPBlocks{10: {"P1", false}}),
 			args: args{
 				peerID:    "P2",
-				block:     types.MakeBlock(int64(10),chainLock.BlockHeight, &chainLock, txs, nil, nil),
+				block:     types.MakeBlock(int64(10), coreChainLock.BlockHeight, &coreChainLock, txs, nil, nil),
 				blockSize: 100,
 			},
 			poolWanted: makeBlockPool(testBcR, 10,
@@ -448,7 +451,7 @@ func TestBlockPoolAddBlock(t *testing.T) {
 				map[int64]tPBlocks{10: {"P1", false}}),
 			args: args{
 				peerID:    "P1",
-				block:     types.MakeBlock(int64(10),chainLock.BlockHeight, &chainLock, txs, nil, nil),
+				block:     types.MakeBlock(int64(10), coreChainLock.BlockHeight, &coreChainLock, txs, nil, nil),
 				blockSize: 100,
 			},
 			poolWanted: makeBlockPool(testBcR, 10,

evidence/pool_test.go
@@ -172,18 +172,20 @@ func TestEvidencePoolUpdate(t *testing.T) {
 	require.NoError(t, err)
 	ev := types.NewMockDuplicateVoteEvidenceWithValidator(height, defaultEvidenceTime, val, evidenceChainID)
 	lastCommit := makeCommit(height, val.PrivKey.PubKey().Address())
-	var chainLock *types.CoreChainLock = nil
+
+	var coreChainLock *types.CoreChainLock = nil
 	if state.NextCoreChainLock.BlockHeight > state.LastCoreChainLock.BlockHeight {
-		chainLock = &state.NextCoreChainLock
+		coreChainLock = &state.NextCoreChainLock
 	}
 
-	var chainLockHeight uint32
-	if chainLock == nil {
-		chainLockHeight = state.LastCoreChainLock.BlockHeight
+	var coreChainLockHeight uint32
+	if coreChainLock == nil {
+		coreChainLockHeight = state.LastCoreChainLock.BlockHeight
 	} else {
-		chainLockHeight = chainLock.BlockHeight
+		coreChainLockHeight = coreChainLock.BlockHeight
 	}
-	block := types.MakeBlock(height+1, chainLockHeight, chainLock, []types.Tx{}, lastCommit, []types.Evidence{ev})
+
+	block := types.MakeBlock(height + 1, coreChainLockHeight, coreChainLock, []types.Tx{}, lastCommit, []types.Evidence{ev})
 	// update state (partially)
 	state.LastBlockHeight = height + 1
 	state.LastBlockTime = defaultEvidenceTime.Add(22 * time.Minute)

state/execution.go
@@ -159,10 +159,12 @@ func (blockExec *BlockExecutor) ApplyBlock(
 
 	// validate the validator updates and convert to tendermint types
 	abciValUpdates := abciResponses.EndBlock.ValidatorUpdates
-	nextChainLock, err := types.CoreChainLockFromProto(abciResponses.EndBlock.CoreChainLockUpdate)
+
+	nextCoreChainLock, err := types.CoreChainLockFromProto(abciResponses.EndBlock.CoreChainLockUpdate)
 	if err != nil {
 		return state, 0, fmt.Errorf("error in chain lock from proto: %v", err)
 	}
+
 	err = validateValidatorUpdates(abciValUpdates, state.ConsensusParams.Validator)
 	if err != nil {
 		return state, 0, fmt.Errorf("error in validator updates: %v", err)
@@ -176,7 +178,7 @@ func (blockExec *BlockExecutor) ApplyBlock(
 	}
 
 	// Update the state with the block and responses.
-	state, err = updateState(state, blockID, &block.Header, block.CoreChainLock, nextChainLock, abciResponses, validatorUpdates)
+	state, err = updateState(state, blockID, &block.Header, block.CoreChainLock, nextCoreChainLock, abciResponses, validatorUpdates)
 	if err != nil {
 		return state, 0, fmt.Errorf("commit failed for application: %v", err)
 	}
@@ -405,8 +407,8 @@ func updateState(
 	state State,
 	blockID types.BlockID,
 	header *types.Header,
-	lastChainLock *types.CoreChainLock,
-	nextChainLock *types.CoreChainLock,
+	lastCoreChainLock *types.CoreChainLock,
+	nextCoreChainLock *types.CoreChainLock,
 	abciResponses *tmstate.ABCIResponses,
 	validatorUpdates []*types.Validator,
 ) (State, error) {
@@ -448,16 +450,16 @@ func updateState(
 
 	nextVersion := state.Version
 
-	if lastChainLock == nil {
-		lastChainLock = &state.LastCoreChainLock
+	if lastCoreChainLock == nil {
+		lastCoreChainLock = &state.LastCoreChainLock
 	}
 
-	if nextChainLock == nil {
-		nextChainLock = &state.NextCoreChainLock
+	if nextCoreChainLock == nil {
+		nextCoreChainLock = &state.NextCoreChainLock
 	}
 
-	if nextChainLock.BlockHeight < lastChainLock.BlockHeight {
-		nextChainLock = lastChainLock
+	if nextCoreChainLock.BlockHeight < lastCoreChainLock.BlockHeight {
+		nextCoreChainLock = lastCoreChainLock
 	}
 
 	// NOTE: the AppHash has not been populated.
@@ -469,8 +471,8 @@ func updateState(
 		LastBlockHeight:                  header.Height,
 		LastBlockID:                      blockID,
 		LastBlockTime:                    header.Time,
-		LastCoreChainLock:                *lastChainLock,
-		NextCoreChainLock:                *nextChainLock,
+		LastCoreChainLock:                *lastCoreChainLock,
+		NextCoreChainLock:                *nextCoreChainLock,
 		NextValidators:                   nValSet,
 		Validators:                       state.NextValidators.Copy(),
 		LastValidators:                   state.Validators.Copy(),

state/state.go
@@ -254,19 +254,20 @@ func (state State) MakeBlock(
 	proposerAddress []byte,
 ) (*types.Block, *types.PartSet) {
 
-	var chainLock *types.CoreChainLock = nil
+	var coreChainLock *types.CoreChainLock = nil
 	if state.NextCoreChainLock.BlockHeight > state.LastCoreChainLock.BlockHeight {
-		chainLock = &state.NextCoreChainLock
+		coreChainLock = &state.NextCoreChainLock
 	}
 
-	var chainLockHeight uint32
-	if chainLock == nil {
-		chainLockHeight = state.LastCoreChainLock.BlockHeight
+	var coreChainLockHeight uint32
+	if coreChainLock == nil {
+		coreChainLockHeight = state.LastCoreChainLock.BlockHeight
 	} else {
-		chainLockHeight = chainLock.BlockHeight
+		coreChainLockHeight = coreChainLock.BlockHeight
 	}
+
 	// Build base block with block data.
-	block := types.MakeBlock(height, chainLockHeight, chainLock, txs, commit, evidence)
+	block := types.MakeBlock(height, coreChainLockHeight, coreChainLock, txs, commit, evidence)
 
 	// Set time.
 	var timestamp time.Time

types/block_test.go
@@ -36,7 +36,8 @@ func TestBlockAddEvidence(t *testing.T) {
 	txs := []Tx{Tx("foo"), Tx("bar")}
 	lastID := makeBlockIDRandom()
 	h := int64(3)
-	chainLock := NewMockChainLock(1)
+
+	coreChainLock := NewMockChainLock(1)
 
 	voteSet, _, vals := randVoteSet(h-1, 1, tmproto.PrecommitType, 10, 1)
 	commit, err := MakeCommit(lastID, h-1, 1, voteSet, vals, time.Now())
@@ -45,7 +46,7 @@ func TestBlockAddEvidence(t *testing.T) {
 	ev := NewMockDuplicateVoteEvidenceWithValidator(h, time.Now(), vals[0], "block-test-chain")
 	evList := []Evidence{ev}
 
-	block := MakeBlock(h, chainLock.BlockHeight, &chainLock, txs, commit, evList)
+	block := MakeBlock(h, coreChainLock.BlockHeight, &coreChainLock, txs, commit, evList)
 	require.NotNil(t, block)
 	require.Equal(t, 1, len(block.Evidence.Evidence))
 	require.NotNil(t, block.EvidenceHash)
@@ -882,34 +883,3 @@ func TestBlockIDEquals(t *testing.T) {
 	assert.True(t, blockIDEmpty.Equals(blockIDEmpty))
 	assert.False(t, blockIDEmpty.Equals(blockIDDifferent))
 }
-
-// TODO: Remove later
-func TestProtoIndexGapCompatibility(t *testing.T) {
-	// Marshal the first message and unmarshal it with the second proto
-
-	protoOne := tmproto.BlockTestOne{FirstField: "hello", SecondField: "world!"}
-	protoOneBytes, err := protoOne.Marshal()
-	if err != nil {
-		panic("die")
-	}
-
-	protoTwo := tmproto.BlockTestTwo{}
-	protoTwo.Unmarshal(protoOneBytes)
-
-	assert.Equal(t, protoOne.FirstField, protoTwo.FirstField)
-	assert.Empty(t, protoTwo.ThirdField)
-
-	// Marshal the second message and unmarshal it with first proto
-
-	protoTwo.ThirdField = "from third"
-
-	protoTwoBytes, err := protoTwo.Marshal()
-
-	protoOneAgain := new(tmproto.BlockTestOne)
-
-	protoOneAgain.Unmarshal(protoTwoBytes)
-
-	assert.Equal(t, protoOneAgain.FirstField, protoTwo.FirstField)
-	assert.Empty(t, protoOneAgain.SecondField)
-}
-

tendermint/tendermint

blockchain/msgs_test.go
@@ -80,9 +80,9 @@ func TestBcStatusResponseMessageValidateBasic(t *testing.T) {
 
 // nolint:lll // ignore line length in tests
 func TestBlockchainMessageVectors(t *testing.T) {
-	chainLock := types.NewMockChainLock(1)
+	coreChainLock := types.NewMockChainLock(1)
 
-	block := types.MakeBlock(int64(3),chainLock.BlockHeight,&chainLock,[]types.Tx{types.Tx("Hello World")}, nil, nil)
+	block := types.MakeBlock(int64(3), coreChainLock.CoreBlockHeight, &coreChainLock, []types.Tx{types.Tx("Hello World") }, nil, nil)
 	block.Version.Block = 11 // overwrite updated protocol version
 
 

blockchain/v1/peer_test.go
@@ -276,6 +276,7 @@ func checkByStoppingPeerTimer(t *testing.T, peer *BpPeer, running bool) {
 }
 
 func makeSmallBlock(height int) *types.Block {
-	chainLock := types.NewMockChainLock(1)
-	return types.MakeBlock(int64(height),chainLock.BlockHeight, &chainLock, []types.Tx{types.Tx("foo")}, nil, nil)
+	coreChainLock := types.NewMockChainLock(1)
+
+	return types.MakeBlock(int64(height), coreChainLock.CoreBlockHeight, &coreChainLock, []types.Tx{types.Tx("foo")}, nil, nil)
 }

blockchain/v1/pool_test.go
@@ -75,14 +75,16 @@ func makeBlockPool(bcr *testBcR, height int64, peers []BpPeer, blocks map[int64]
 		bPool.peers[p.ID].SetLogger(bcr.logger)
 
 	}
-	chainLock := types.NewMockChainLock(1)
+
+	coreChainLock := types.NewMockChainLock(1)
+
 	bPool.MaxPeerHeight = maxH
 	for h, p := range blocks {
 		bPool.blocks[h] = p.id
 		bPool.peers[p.id].RequestSent(h)
 		if p.create {
 			// simulate that a block at height h has been received
-			_ = bPool.peers[p.id].AddBlock(types.MakeBlock(h,chainLock.BlockHeight, &chainLock, txs, nil, nil), 100)
+			_ = bPool.peers[p.id].AddBlock(types.MakeBlock(h, coreChainLock.CoreBlockHeight, &coreChainLock, txs, nil, nil), 100)
 		}
 	}
 	return bPool
@@ -376,7 +378,8 @@ func TestBlockPoolSendRequestBatch(t *testing.T) {
 func TestBlockPoolAddBlock(t *testing.T) {
 	testBcR := newTestBcR()
 	txs := []types.Tx{types.Tx("foo"), types.Tx("bar")}
-	chainLock := types.NewMockChainLock(1)
+
+	coreChainLock := types.NewMockChainLock(1)
 
 	type args struct {
 		peerID    p2p.ID
@@ -394,7 +397,7 @@ func TestBlockPoolAddBlock(t *testing.T) {
 			pool: makeBlockPool(testBcR, 10, []BpPeer{{ID: "P1", Height: 100}}, map[int64]tPBlocks{}),
 			args: args{
 				peerID:    "P2",
-				block:     types.MakeBlock(int64(10),chainLock.BlockHeight, &chainLock, txs, nil, nil),
+				block:     types.MakeBlock(int64(10), coreChainLock.CoreBlockHeight, &coreChainLock, txs, nil, nil),
 				blockSize: 100,
 			},
 			poolWanted: makeBlockPool(testBcR, 10, []BpPeer{{ID: "P1", Height: 100}}, map[int64]tPBlocks{}),
@@ -406,7 +409,7 @@ func TestBlockPoolAddBlock(t *testing.T) {
 				map[int64]tPBlocks{10: {"P1", false}}),
 			args: args{
 				peerID:    "P1",
-				block:     types.MakeBlock(int64(11),chainLock.BlockHeight, &chainLock, txs, nil, nil),
+				block:     types.MakeBlock(int64(11), coreChainLock.CoreBlockHeight, &coreChainLock, txs, nil, nil),
 				blockSize: 100,
 			},
 			poolWanted: makeBlockPool(testBcR, 10,
@@ -420,7 +423,7 @@ func TestBlockPoolAddBlock(t *testing.T) {
 				map[int64]tPBlocks{10: {"P1", true}, 11: {"P1", false}}),
 			args: args{
 				peerID:    "P1",
-				block:     types.MakeBlock(int64(10),chainLock.BlockHeight, &chainLock, txs, nil, nil),
+				block:     types.MakeBlock(int64(10), coreChainLock.CoreBlockHeight, &coreChainLock, txs, nil, nil),
 				blockSize: 100,
 			},
 			poolWanted: makeBlockPool(testBcR, 10,
@@ -434,7 +437,7 @@ func TestBlockPoolAddBlock(t *testing.T) {
 				map[int64]tPBlocks{10: {"P1", false}}),
 			args: args{
 				peerID:    "P2",
-				block:     types.MakeBlock(int64(10),chainLock.BlockHeight, &chainLock, txs, nil, nil),
+				block:     types.MakeBlock(int64(10), coreChainLock.CoreBlockHeight, &coreChainLock, txs, nil, nil),
 				blockSize: 100,
 			},
 			poolWanted: makeBlockPool(testBcR, 10,
@@ -448,7 +451,7 @@ func TestBlockPoolAddBlock(t *testing.T) {
 				map[int64]tPBlocks{10: {"P1", false}}),
 			args: args{
 				peerID:    "P1",
-				block:     types.MakeBlock(int64(10),chainLock.BlockHeight, &chainLock, txs, nil, nil),
+				block:     types.MakeBlock(int64(10), coreChainLock.CoreBlockHeight, &coreChainLock, txs, nil, nil),
 				blockSize: 100,
 			},
 			poolWanted: makeBlockPool(testBcR, 10,

evidence/pool_test.go
@@ -173,18 +173,20 @@ func TestEvidencePoolUpdate(t *testing.T) {
 	ev := types.NewMockDuplicateVoteEvidenceWithValidator(height, defaultEvidenceTime.Add(21*time.Minute),
 		val, evidenceChainID)
 	lastCommit := makeCommit(height, val.PrivKey.PubKey().Address())
-	var chainLock *types.CoreChainLock = nil
-	if state.NextCoreChainLock.BlockHeight > state.LastCoreChainLock.BlockHeight {
-		chainLock = &state.NextCoreChainLock
+
+	var coreChainLock *types.CoreChainLock = nil
+	if state.NextCoreChainLock.CoreBlockHeight > state.LastCoreChainLock.CoreBlockHeight {
+		coreChainLock = &state.NextCoreChainLock
 	}
 
-	var chainLockHeight uint32
-	if chainLock == nil {
-		chainLockHeight = state.LastCoreChainLock.BlockHeight
+	var coreChainLockHeight uint32
+	if coreChainLock == nil {
+		coreChainLockHeight = state.LastCoreChainLock.CoreBlockHeight
 	} else {
-		chainLockHeight = chainLock.BlockHeight
+		coreChainLockHeight = coreChainLock.CoreBlockHeight
 	}
-	block := types.MakeBlock(height+1, chainLockHeight, chainLock, []types.Tx{}, lastCommit, []types.Evidence{ev})
+
+	block := types.MakeBlock(height + 1, coreChainLockHeight, coreChainLock, []types.Tx{}, lastCommit, []types.Evidence{ev})
 	// update state (partially)
 	state.LastBlockHeight = height + 1
 	state.LastBlockTime = defaultEvidenceTime.Add(22 * time.Minute)

state/execution.go
@@ -161,10 +161,12 @@ func (blockExec *BlockExecutor) ApplyBlock(
 
 	// validate the validator updates and convert to tendermint types
 	abciValUpdates := abciResponses.EndBlock.ValidatorUpdates
-	nextChainLock, err := types.CoreChainLockFromProto(abciResponses.EndBlock.CoreChainLockUpdate)
+
+	nextCoreChainLock, err := types.CoreChainLockFromProto(abciResponses.EndBlock.CoreChainLockUpdate)
 	if err != nil {
 		return state, 0, fmt.Errorf("error in chain lock from proto: %v", err)
 	}
+
 	err = validateValidatorUpdates(abciValUpdates, state.ConsensusParams.Validator)
 	if err != nil {
 		return state, 0, fmt.Errorf("error in validator updates: %v", err)
@@ -178,7 +180,7 @@ func (blockExec *BlockExecutor) ApplyBlock(
 	}
 
 	// Update the state with the block and responses.
-	state, err = updateState(state, blockID, &block.Header, block.CoreChainLock, nextChainLock, abciResponses, validatorUpdates)
+	state, err = updateState(state, blockID, &block.Header, block.CoreChainLock, nextCoreChainLock, abciResponses, validatorUpdates)
 	if err != nil {
 		return state, 0, fmt.Errorf("commit failed for application: %v", err)
 	}
@@ -411,8 +413,8 @@ func updateState(
 	state State,
 	blockID types.BlockID,
 	header *types.Header,
-	lastChainLock *types.CoreChainLock,
-	nextChainLock *types.CoreChainLock,
+	lastCoreChainLock *types.CoreChainLock,
+	nextCoreChainLock *types.CoreChainLock,
 	abciResponses *tmstate.ABCIResponses,
 	validatorUpdates []*types.Validator,
 ) (State, error) {
@@ -454,16 +456,16 @@ func updateState(
 
 	nextVersion := state.Version
 
-	if lastChainLock == nil {
-		lastChainLock = &state.LastCoreChainLock
+	if lastCoreChainLock == nil {
+		lastCoreChainLock = &state.LastCoreChainLock
 	}
 
-	if nextChainLock == nil {
-		nextChainLock = &state.NextCoreChainLock
+	if nextCoreChainLock == nil {
+		nextCoreChainLock = &state.NextCoreChainLock
 	}
 
-	if nextChainLock.BlockHeight < lastChainLock.BlockHeight {
-		nextChainLock = lastChainLock
+	if nextCoreChainLock.CoreBlockHeight < lastCoreChainLock.CoreBlockHeight {
+		nextCoreChainLock = lastCoreChainLock
 	}
 
 	// NOTE: the AppHash has not been populated.
@@ -475,8 +477,8 @@ func updateState(
 		LastBlockHeight:                  header.Height,
 		LastBlockID:                      blockID,
 		LastBlockTime:                    header.Time,
-		LastCoreChainLock:                *lastChainLock,
-		NextCoreChainLock:                *nextChainLock,
+		LastCoreChainLock:                *lastCoreChainLock,
+		NextCoreChainLock:                *nextCoreChainLock,
 		NextValidators:                   nValSet,
 		Validators:                       state.NextValidators.Copy(),
 		LastValidators:                   state.Validators.Copy(),

state/state.go
@@ -254,19 +254,20 @@ func (state State) MakeBlock(
 	proposerAddress []byte,
 ) (*types.Block, *types.PartSet) {
 
-	var chainLock *types.CoreChainLock = nil
-	if state.NextCoreChainLock.BlockHeight > state.LastCoreChainLock.BlockHeight {
-		chainLock = &state.NextCoreChainLock
+	var coreChainLock *types.CoreChainLock = nil
+	if state.NextCoreChainLock.CoreBlockHeight > state.LastCoreChainLock.CoreBlockHeight {
+		coreChainLock = &state.NextCoreChainLock
 	}
 
-	var chainLockHeight uint32
-	if chainLock == nil {
-		chainLockHeight = state.LastCoreChainLock.BlockHeight
+	var coreChainLockHeight uint32
+	if coreChainLock == nil {
+		coreChainLockHeight = state.LastCoreChainLock.CoreBlockHeight
 	} else {
-		chainLockHeight = chainLock.BlockHeight
+		coreChainLockHeight = coreChainLock.CoreBlockHeight
 	}
+
 	// Build base block with block data.
-	block := types.MakeBlock(height, chainLockHeight, chainLock, txs, commit, evidence)
+	block := types.MakeBlock(height, coreChainLockHeight, coreChainLock, txs, commit, evidence)
 
 	// Set time.
 	var timestamp time.Time

types/block_test.go
@@ -36,7 +36,8 @@ func TestBlockAddEvidence(t *testing.T) {
 	txs := []Tx{Tx("foo"), Tx("bar")}
 	lastID := makeBlockIDRandom()
 	h := int64(3)
-	chainLock := NewMockChainLock(1)
+
+	coreChainLock := NewMockChainLock(1)
 
 	voteSet, _, vals := randVoteSet(h-1, 1, tmproto.PrecommitType, 10, 1)
 	commit, err := MakeCommit(lastID, h-1, 1, voteSet, vals, time.Now())
@@ -45,7 +46,7 @@ func TestBlockAddEvidence(t *testing.T) {
 	ev := NewMockDuplicateVoteEvidenceWithValidator(h, time.Now(), vals[0], "block-test-chain")
 	evList := []Evidence{ev}
 
-	block := MakeBlock(h, chainLock.BlockHeight, &chainLock, txs, commit, evList)
+	block := MakeBlock(h, coreChainLock.CoreBlockHeight, &coreChainLock, txs, commit, evList)
 	require.NotNil(t, block)
 	require.Equal(t, 1, len(block.Evidence.Evidence))
 	require.NotNil(t, block.EvidenceHash)
@@ -877,34 +878,3 @@ func TestBlockIDEquals(t *testing.T) {
 	assert.True(t, blockIDEmpty.Equals(blockIDEmpty))
 	assert.False(t, blockIDEmpty.Equals(blockIDDifferent))
 }
-
-// TODO: Remove later
-func TestProtoIndexGapCompatibility(t *testing.T) {
-	// Marshal the first message and unmarshal it with the second proto
-
-	protoOne := tmproto.BlockTestOne{FirstField: "hello", SecondField: "world!"}
-	protoOneBytes, err := protoOne.Marshal()
-	if err != nil {
-		panic("die")
-	}
-
-	protoTwo := tmproto.BlockTestTwo{}
-	protoTwo.Unmarshal(protoOneBytes)
-
-	assert.Equal(t, protoOne.FirstField, protoTwo.FirstField)
-	assert.Empty(t, protoTwo.ThirdField)
-
-	// Marshal the second message and unmarshal it with first proto
-
-	protoTwo.ThirdField = "from third"
-
-	protoTwoBytes, err := protoTwo.Marshal()
-
-	protoOneAgain := new(tmproto.BlockTestOne)
-
-	protoOneAgain.Unmarshal(protoTwoBytes)
-
-	assert.Equal(t, protoOneAgain.FirstField, protoTwo.FirstField)
-	assert.Empty(t, protoOneAgain.SecondField)
-}
-