aef1ac7ba5d5f2d73cba706d2d23fafeeb73d8ef
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-12-23 16:31:00 +0400

    modify Reactor priorities (#5826)
    blockchain/vX reactor priority was decreased because during the normal operation
    (i.e. when the node is not fast syncing) blockchain priority can't be
    the same as consensus reactor priority. Otherwise, it's theoretically possible to
    slow down consensus by constantly requesting blocks from the node.
    
    NOTE: ideally blockchain/vX reactor priority would be dynamic. e.g. when
    the node is fast syncing, the priority is 10 (max), but when it's done
    fast syncing - the priority gets decreased to 5 (only to serve blocks
    for other nodes). But it's not possible now, therefore I decided to
    focus on the normal operation (priority = 5).
    
    evidence and consensus critical messages are more important than
    the mempool ones, hence priorities are bumped by 1 (from 5 to 6).
    
    statesync reactor priority was changed from 1 to 5 to be the same as
    blockchain/vX priority.
    
    Refs https://github.com/tendermint/tendermint/issues/5816

--- consensus/reactor.go
+++ consensus/reactor.go
@@ -142,7 +142,7 @@ func (conR *Reactor) GetChannels() []*p2p.ChannelDescriptor {
 	return []*p2p.ChannelDescriptor{
 		{
 			ID:                  StateChannel,
-			Priority:            5,
+			Priority:            6,
 			SendQueueCapacity:   100,
 			RecvMessageCapacity: maxMsgSize,
 		},
@@ -156,7 +156,7 @@ func (conR *Reactor) GetChannels() []*p2p.ChannelDescriptor {
 		},
 		{
 			ID:                  VoteChannel,
-			Priority:            5,
+			Priority:            7,
 			SendQueueCapacity:   100,
 			RecvBufferCapacity:  100 * 100,
 			RecvMessageCapacity: maxMsgSize,
