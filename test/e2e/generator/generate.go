af1ac64d8ceb2c0328eda91cdeb65d1a8503301a
Erik Grinaker <erik@interchain.berlin>
2021-01-04 16:19:07 +0100

    test/e2e: disable abci/grpc and blockchain/v2 due to flake (#5854)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- test/e2e/generator/generate.go
+++ test/e2e/generator/generate.go
@@ -25,14 +25,12 @@ var (
 	}
 
 	// The following specify randomly chosen values for testnet nodes.
-	nodeDatabases        = uniformChoice{"goleveldb", "cleveldb", "rocksdb", "boltdb", "badgerdb"}
-	nodeABCIProtocols    = uniformChoice{"unix", "tcp", "grpc", "builtin"}
+	nodeDatabases = uniformChoice{"goleveldb", "cleveldb", "rocksdb", "boltdb", "badgerdb"}
+	// FIXME: grpc disabled due to https://github.com/tendermint/tendermint/issues/5439
+	nodeABCIProtocols    = uniformChoice{"unix", "tcp", "builtin"} // "grpc"
 	nodePrivvalProtocols = uniformChoice{"file", "unix", "tcp"}
-	// FIXME v1 disabled due to https://github.com/tendermint/tendermint/issues/5444
-	// FIXME v2 disabled due to:
-	// https://github.com/tendermint/tendermint/issues/5513
-	// https://github.com/tendermint/tendermint/issues/5541
-	nodeFastSyncs         = uniformChoice{"", "v0"} // "v1", "v2"
+	// FIXME: v2 disabled due to flake
+	nodeFastSyncs         = uniformChoice{"", "v0"} // "v2"
 	nodeStateSyncs        = uniformChoice{false, true}
 	nodePersistIntervals  = uniformChoice{0, 1, 5}
 	nodeSnapshotIntervals = uniformChoice{0, 3}
@@ -44,7 +42,7 @@ var (
 		"restart":    0.1,
 	}
 	nodeMisbehaviors = weightedChoice{
-		// FIXME evidence disabled due to node panicing when not
+		// FIXME: evidence disabled due to node panicing when not
 		// having sufficient block history to process evidence.
 		// https://github.com/tendermint/tendermint/issues/5617
 		// misbehaviorOption{"double-prevote"}: 1,
