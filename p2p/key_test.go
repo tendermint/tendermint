9274845e9dad8cfd01432718b2451f2082017df8
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-01-04 12:02:39 -0500

    p2p: add TestNodeID_Equal

--- p2p/key_test.go
+++ p2p/key_test.go
@@ -5,7 +5,6 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	tmrand "github.com/tendermint/tendermint/libs/rand"
@@ -15,35 +14,51 @@ func TestLoadOrGenNodeKey(t *testing.T) {
 	filePath := filepath.Join(os.TempDir(), tmrand.Str(12)+"_peer_id.json")
 
 	nodeKey, err := LoadOrGenNodeKey(filePath)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 
 	nodeKey2, err := LoadOrGenNodeKey(filePath)
-	assert.Nil(t, err)
-
-	assert.Equal(t, nodeKey, nodeKey2)
+	require.Nil(t, err)
+	require.Equal(t, nodeKey, nodeKey2)
 }
 
 func TestLoadNodeKey(t *testing.T) {
 	filePath := filepath.Join(os.TempDir(), tmrand.Str(12)+"_peer_id.json")
 
 	_, err := LoadNodeKey(filePath)
-	assert.True(t, os.IsNotExist(err))
+	require.True(t, os.IsNotExist(err))
 
 	_, err = LoadOrGenNodeKey(filePath)
 	require.NoError(t, err)
 
 	nodeKey, err := LoadNodeKey(filePath)
-	assert.NoError(t, err)
-	assert.NotNil(t, nodeKey)
+	require.NoError(t, err)
+	require.NotNil(t, nodeKey)
 }
 
 func TestNodeKeySaveAs(t *testing.T) {
 	filePath := filepath.Join(os.TempDir(), tmrand.Str(12)+"_peer_id.json")
-
-	assert.NoFileExists(t, filePath)
+	require.NoFileExists(t, filePath)
 
 	nodeKey := GenNodeKey()
-	err := nodeKey.SaveAs(filePath)
-	assert.NoError(t, err)
-	assert.FileExists(t, filePath)
+	require.NoError(t, nodeKey.SaveAs(filePath))
+	require.FileExists(t, filePath)
+}
+
+func TestNodeID_Equal(t *testing.T) {
+	testCases := map[string]struct {
+		inputA NodeID
+		inputB NodeID
+		result bool
+	}{
+		"empty node IDs":                         {"", "", true},
+		"single empty node ID":                   {"", "869152802e11af56b774", false},
+		"non-equal and non-empty empty node IDs": {"f84a7b9f4ae1e61a760f", "869152802e11af56b774", false},
+		"equal node IDs":                         {"f84a7b9f4ae1e61a760f", "f84a7b9f4ae1e61a760f", true},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			require.Equal(t, tc.result, tc.inputA.Equal(tc.inputB))
+		})
+	}
 }
