80c90b84ec81de0cce1259714c8ba9d46a14a8bd
Erik Grinaker <erik@interchain.berlin>
2021-01-04 14:38:42 +0100

    tools/tm-signer-harness: fix listener leak in newTestHarnessListener() (#5850)
    Fixes #5837.

--- tools/tm-signer-harness/internal/test_harness.go
+++ tools/tm-signer-harness/internal/test_harness.go
@@ -374,6 +374,7 @@ func newTestHarnessListener(logger log.Logger, cfg TestHarnessConfig) (*privval.
 		logger.Info("Resolved TCP address for listener", "addr", tcpLn.Addr())
 		svln = tcpLn
 	default:
+		_ = ln.Close()
 		logger.Error("Unsupported protocol (must be unix:// or tcp://)", "proto", proto)
 		return nil, newTestHarnessError(ErrInvalidParameters, nil, fmt.Sprintf("Unsupported protocol: %s", proto))
 	}
