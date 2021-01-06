deafda30e8da629244bcfeb8403560348578e5ec
Marko Baricevic <marbar3778@yahoo.com>
2021-01-06 15:17:42 -0800

    query key

--- cmd/tendermint/commands/show_validator.go
+++ cmd/tendermint/commands/show_validator.go
@@ -5,31 +5,55 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/tendermint/tendermint/crypto"
 	tmjson "github.com/tendermint/tendermint/libs/json"
+	tmnet "github.com/tendermint/tendermint/libs/net"
 	tmos "github.com/tendermint/tendermint/libs/os"
 	"github.com/tendermint/tendermint/privval"
+	tmgrpc "github.com/tendermint/tendermint/privval/grpc"
 )
 
 // ShowValidatorCmd adds capabilities for showing the validator info.
 var ShowValidatorCmd = &cobra.Command{
-	Use:     "show-validator",
+	Use:     "show-validator [chainID]",
 	Aliases: []string{"show_validator"},
 	Short:   "Show this node's validator info",
+	Args:    cobra.ExactArgs(1),
 	RunE:    showValidator,
 	PreRun:  deprecateSnakeCase,
 }
 
 func showValidator(cmd *cobra.Command, args []string) error {
-	keyFilePath := config.PrivValidatorKeyFile()
-	if !tmos.FileExists(keyFilePath) {
-		return fmt.Errorf("private validator file %s does not exist", keyFilePath)
-	}
+	var (
+		pubKey crypto.PubKey
+		err    error
+	)
 
-	pv := privval.LoadFilePV(keyFilePath, config.PrivValidatorStateFile())
+	//todo: remove once gRPC is the only supported protocol
+	protocol, _ := tmnet.ProtocolAndAddress(config.PrivValidatorListenAddr)
+	switch protocol {
+	case "grpc":
+		pvsc, err := tmgrpc.DialRemoteSigner(config, args[0], logger)
+		if err != nil {
+			return fmt.Errorf("can't instansiate connection: %w", err)
+		}
+		pubKey, err = pvsc.GetPubKey()
+		if err != nil {
+			return fmt.Errorf("can't get pubkey: %w", err)
+		}
+	default:
 
-	pubKey, err := pv.GetPubKey()
-	if err != nil {
-		return fmt.Errorf("can't get pubkey: %w", err)
+		keyFilePath := config.PrivValidatorKeyFile()
+		if !tmos.FileExists(keyFilePath) {
+			return fmt.Errorf("private validator file %s does not exist", keyFilePath)
+		}
+
+		pv := privval.LoadFilePV(keyFilePath, config.PrivValidatorStateFile())
+
+		pubKey, err = pv.GetPubKey()
+		if err != nil {
+			return fmt.Errorf("can't get pubkey: %w", err)
+		}
 	}
 
 	bz, err := tmjson.Marshal(pubKey)
