22536ba3cf0dd65d9bbc4f388fa987a5dc106d56
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-01-11 14:37:04 -0500

    blockchain v0: decrease max block height

--- blockchain/v0/reactor_test.go
+++ blockchain/v0/reactor_test.go
@@ -311,7 +311,7 @@ func TestReactor_BadBlockStopsPeer(t *testing.T) {
 	config := cfg.ResetTestRoot("blockchain_reactor_test")
 	defer os.RemoveAll(config.RootDir)
 
-	maxBlockHeight := int64(64)
+	maxBlockHeight := int64(48)
 	genDoc, privVals := randGenesisDoc(config, 1, false, 30)
 
 	testSuites := []*reactorTestSuite{
