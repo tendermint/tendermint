c1579471abae8fa164342b3269ff7482e1c49635
Erik Grinaker <erik@interchain.berlin>
2021-01-31 19:40:26 +0100

    revert removed comment

--- p2p/peer.go
+++ p2p/peer.go
@@ -1163,6 +1163,13 @@ func keyPeerInfoRange() ([]byte, []byte) {
 	return start, end
 }
 
+// ============================================================================
+// Types and business logic below may be deprecated.
+//
+// TODO: Rename once legacy p2p types are removed.
+// ref: https://github.com/tendermint/tendermint/issues/5670
+// ============================================================================
+
 //go:generate mockery --case underscore --name Peer
 
 const metricsTickerDuration = 10 * time.Second
