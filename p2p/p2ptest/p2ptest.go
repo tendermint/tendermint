d9e47a7c5c0a30c564f1e05aefb1668af025c292
Erik Grinaker <erik@interchain.berlin>
2021-02-02 23:08:30 +0100

    add p2ptest package with network setup tooling

--- p2p/p2ptest/p2ptest.go
+++ p2p/p2ptest/p2ptest.go
@@ -1,10 +1,196 @@
 package p2ptest
 
-import "github.com/tendermint/tendermint/p2p"
+import (
+	"math/rand"
+	"testing"
+	"time"
 
-// TestNetwork sets up an in-memory network that can be used for high-level P2P
-// testing.
-type TestNetwork struct {
+	"github.com/gogo/protobuf/proto"
+	gogotypes "github.com/gogo/protobuf/types"
+	"github.com/stretchr/testify/require"
+	dbm "github.com/tendermint/tm-db"
+
+	"github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+	"github.com/tendermint/tendermint/libs/log"
+	"github.com/tendermint/tendermint/p2p"
+)
+
+// StringMessage is a simple message containing a string-typed Value field.
+type StringMessage = gogotypes.StringValue
+
+// Network sets up an in-memory network that can be used for high-level P2P
+// testing. It creates an arbitrary number of nodes that are connected to each
+// other, and can open channels across all nodes with custom reactors.
+type Network struct {
+	Nodes         map[p2p.NodeID]*Node
+	memoryNetwork *p2p.MemoryNetwork
+}
+
+// MakeNetwork creates a test network with the given number of nodes and
+// connects them to each other.
+func MakeNetwork(t *testing.T, nodes int) *Network {
+	network := &Network{
+		Nodes:         map[p2p.NodeID]*Node{},
+		memoryNetwork: p2p.NewMemoryNetwork(log.TestingLogger()),
+	}
+	for i := 0; i < nodes; i++ {
+		node := MakeNode(t, network)
+		network.Nodes[node.NodeID] = node
+	}
+
+	// Set up a list of node addresses to dial, and a peer update subscription
+	// for each node.
+	dialQueue := []p2p.NodeAddress{}
+	subs := map[p2p.NodeID]*p2p.PeerUpdates{}
+	for _, node := range network.Nodes {
+		dialQueue = append(dialQueue, node.NodeAddress)
+		subs[node.NodeID] = node.PeerManager.Subscribe()
+		defer subs[node.NodeID].Close()
+	}
+
+	// For each node, dial the nodes that it still doesn't have a connection to
+	// (either inbound or outbound), and wait for both sides to confirm the
+	// connection via the subscriptions.
+	for i, sourceAddress := range dialQueue {
+		sourceNode := network.Nodes[sourceAddress.NodeID]
+		sourceSub := subs[sourceAddress.NodeID]
+		for _, targetAddress := range dialQueue[i+1:] { // nodes <i already connected
+			targetNode := network.Nodes[targetAddress.NodeID]
+			targetSub := subs[targetAddress.NodeID]
+			require.NoError(t, sourceNode.PeerManager.Add(targetAddress))
+
+			select {
+			case peerUpdate := <-sourceSub.Updates():
+				require.Equal(t, p2p.PeerUpdate{
+					NodeID: targetNode.NodeID,
+					Status: p2p.PeerStatusUp,
+				}, peerUpdate)
+			case <-time.After(time.Second):
+				require.Fail(t, "timed out waiting for %v to dial %v", sourceNode.NodeID, targetNode.NodeID)
+			}
+
+			select {
+			case peerUpdate := <-targetSub.Updates():
+				require.Equal(t, p2p.PeerUpdate{
+					NodeID: sourceNode.NodeID,
+					Status: p2p.PeerStatusUp,
+				}, peerUpdate)
+			case <-time.After(time.Second):
+				require.Fail(t, "timed out waiting for %v to accept %v", targetNode.NodeID, sourceNode.NodeID)
+			}
+
+			// Add the address to the target as well, so it's able to dial the
+			// source back if that's even necessary.
+			require.NoError(t, targetNode.PeerManager.Add(sourceAddress))
+		}
+	}
+
+	return network
+}
+
+// MakeChannelAtNode opens a channel across all nodes in the network, calling
+// the given onPeers function for each node except the given node ID, and returns
+// the channel at the given node ID.
+func (n *Network) MakeChannelAtNode(
+	t *testing.T,
+	nodeID p2p.NodeID,
+	chID p2p.ChannelID,
+	messageType proto.Message,
+	onPeers func(*testing.T, p2p.NodeID, *p2p.Channel),
+) *p2p.Channel {
+
+	// First create the channel on the "local" (specified) node.
+	require.Contains(t, n.Nodes, nodeID)
+	node := n.Nodes[nodeID]
+	channel, err := node.Router.OpenChannel(chID, messageType)
+	require.NoError(t, err)
+	t.Cleanup(channel.Close)
+
+	// Then create the channel on all "remote" (unspecified) peers,
+	// calling onPeers with the channel.
+	for _, peer := range n.Nodes {
+		if peer.NodeID == nodeID {
+			continue
+		}
+
+		channel, err := peer.Router.OpenChannel(chID, messageType)
+		require.NoError(t, err)
+		t.Cleanup(channel.Close)
+
+		onPeers(t, peer.NodeID, channel)
+	}
+
+	return channel
+}
+
+// RandomNode returns a random node.
+func (n *Network) RandomNode() *Node {
+	nodes := make([]*Node, 0, len(n.Nodes))
+	for _, node := range n.Nodes {
+		nodes = append(nodes, node)
+	}
+	return nodes[rand.Intn(len(nodes))] // nolint:gosec
+}
+
+// Peers returns a node's peers (i.e. everyone except itself).
+func (n *Network) Peers(id p2p.NodeID) []*Node {
+	peers := make([]*Node, 0, len(n.Nodes)-1)
+	for _, peer := range n.Nodes {
+		if peer.NodeID != id {
+			peers = append(peers, peer)
+		}
+	}
+	return peers
+}
+
+// Node is a node in a TestNetwork, with a Router and a PeerManager.
+type Node struct {
+	NodeID      p2p.NodeID
+	NodeInfo    p2p.NodeInfo
+	NodeAddress p2p.NodeAddress
+	PrivKey     crypto.PrivKey
 	Router      *p2p.Router
 	PeerManager *p2p.PeerManager
+}
+
+// NewNode creates a new TestNode.
+func MakeNode(t *testing.T, network *Network) *Node {
+	logger := log.TestingLogger()
+	privKey := ed25519.GenPrivKey()
+	nodeID := p2p.NodeIDFromPubKey(privKey.PubKey())
+	nodeInfo := p2p.NodeInfo{
+		NodeID:     nodeID,
+		ListenAddr: "0.0.0.0:0", // FIXME: We have to fake this for now.
+		Moniker:    string(nodeID),
+	}
+
+	transport := network.memoryNetwork.CreateTransport(nodeID)
+	endpoints := transport.Endpoints()
+	require.Len(t, endpoints, 1)
+
+	peerManager, err := p2p.NewPeerManager(nodeID, dbm.NewMemDB(), p2p.PeerManagerOptions{})
+	require.NoError(t, err)
+
+	router, err := p2p.NewRouter(logger, nodeInfo, privKey, peerManager, []p2p.Transport{transport}, p2p.RouterOptions{})
+	require.NoError(t, err)
+	err = router.Start()
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		peerManager.Close()
+		require.NoError(t, transport.Close())
+		if router.IsRunning() {
+			require.NoError(t, router.Stop())
+		}
+	})
+
+	return &Node{
+		NodeID:      nodeID,
+		NodeInfo:    nodeInfo,
+		NodeAddress: endpoints[0].NodeAddress(nodeID),
+		PrivKey:     privKey,
+		Router:      router,
+		PeerManager: peerManager,
+	}
 }
--- p2p/router_test.go
+++ p2p/router_test.go
@@ -5,38 +5,20 @@ import (
 	"testing"
 
 	"github.com/fortytw2/leaktest"
-	gogotypes "github.com/gogo/protobuf/types"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	dbm "github.com/tendermint/tm-db"
 
-	"github.com/tendermint/tendermint/crypto"
-	"github.com/tendermint/tendermint/crypto/ed25519"
-	"github.com/tendermint/tendermint/libs/log"
 	"github.com/tendermint/tendermint/p2p"
+	"github.com/tendermint/tendermint/p2p/p2ptest"
 )
-
-type TestMessage = gogotypes.StringValue
-
-func generateNode() (p2p.NodeInfo, crypto.PrivKey) {
-	privKey := ed25519.GenPrivKey()
-	nodeID := p2p.NodeIDFromPubKey(privKey.PubKey())
-	nodeInfo := p2p.NodeInfo{
-		NodeID: nodeID,
-		// FIXME: We have to fake a ListenAddr for now.
-		ListenAddr: "127.0.0.1:1234",
-		Moniker:    "foo",
-	}
-	return nodeInfo, privKey
-}
 
 func echoReactor(channel *p2p.Channel) {
 	for {
 		select {
 		case envelope := <-channel.In:
+			value := envelope.Message.(*p2ptest.StringMessage).Value
 			channel.Out <- p2p.Envelope{
 				To:      envelope.From,
-				Message: &TestMessage{Value: envelope.Message.(*TestMessage).Value},
+				Message: &p2ptest.StringMessage{Value: value},
 			}
 		case <-channel.Done():
 			return
@@ -44,114 +26,64 @@ func echoReactor(channel *p2p.Channel) {
 	}
 }
 
-func TestRouter(t *testing.T) {
-	defer leaktest.Check(t)()
+func TestNetwork(t *testing.T) {
+	t.Cleanup(leaktest.Check(t))
 
-	logger := log.TestingLogger()
-	network := p2p.NewMemoryNetwork(logger)
-	nodeInfo, privKey := generateNode()
-	transport := network.CreateTransport(nodeInfo.NodeID)
-	defer transport.Close()
-	chID := p2p.ChannelID(1)
+	// Create a test network and open a channel where all peers run echoReactor.
+	network := p2ptest.MakeNetwork(t, 4)
+	local := network.RandomNode()
+	peers := network.Peers(local.NodeID)
+	channel := network.MakeChannelAtNode(t, local.NodeID, 1, &p2ptest.StringMessage{},
+		func(t *testing.T, peerID p2p.NodeID, peerChannel *p2p.Channel) {
+			go echoReactor(peerChannel)
+		},
+	)
 
-	// Start some other in-memory network nodes to communicate with, running
-	// a simple echo reactor that returns received messages.
-	peers := []p2p.NodeAddress{}
-	for i := 0; i < 3; i++ {
-		peerInfo, peerKey := generateNode()
-		peerManager, err := p2p.NewPeerManager(peerInfo.NodeID, dbm.NewMemDB(), p2p.PeerManagerOptions{})
-		require.NoError(t, err)
-		peerTransport := network.CreateTransport(peerInfo.NodeID)
-		defer peerTransport.Close()
-		peerRouter, err := p2p.NewRouter(
-			logger.With("peerID", i),
-			peerInfo,
-			peerKey,
-			peerManager,
-			[]p2p.Transport{peerTransport},
-			p2p.RouterOptions{},
-		)
-		require.NoError(t, err)
-		peers = append(peers, peerTransport.Endpoints()[0].NodeAddress(peerInfo.NodeID))
-
-		channel, err := peerRouter.OpenChannel(chID, &TestMessage{})
-		require.NoError(t, err)
-		defer channel.Close()
-		go echoReactor(channel)
-
-		err = peerRouter.Start()
-		require.NoError(t, err)
-		defer func() { require.NoError(t, peerRouter.Stop()) }()
+	// Sending a message to each peer should return a message.
+	for _, peer := range peers {
+		channel.Out <- p2p.Envelope{
+			To:      peer.NodeID,
+			Message: &p2ptest.StringMessage{Value: "foo"},
+		}
+		e := <-channel.In
+		require.Equal(t, p2p.Envelope{
+			From:    peer.NodeID,
+			Message: &p2ptest.StringMessage{Value: "foo"},
+		}, e)
 	}
 
-	// Start the main router and connect it to the peers above.
-	peerManager, err := p2p.NewPeerManager(nodeInfo.NodeID, dbm.NewMemDB(), p2p.PeerManagerOptions{})
-	require.NoError(t, err)
-	defer peerManager.Close()
-	for _, address := range peers {
-		err := peerManager.Add(address)
-		require.NoError(t, err)
-	}
-	peerUpdates := peerManager.Subscribe()
-	defer peerUpdates.Close()
-
-	router, err := p2p.NewRouter(logger, nodeInfo, privKey, peerManager, []p2p.Transport{transport}, p2p.RouterOptions{})
-	require.NoError(t, err)
-	channel, err := router.OpenChannel(chID, &TestMessage{})
-	require.NoError(t, err)
-	defer channel.Close()
-
-	err = router.Start()
-	require.NoError(t, err)
-	defer func() {
-		// Since earlier defers are closed after this, and we have to make sure
-		// we close channels and subscriptions before the router, we explicitly
-		// close them here to.
-		peerUpdates.Close()
-		channel.Close()
-		require.NoError(t, router.Stop())
-	}()
-
-	// Wait for peers to come online, and ping them as they do.
-	for i := 0; i < len(peers); i++ {
-		peerUpdate := <-peerUpdates.Updates()
-		peerID := peerUpdate.NodeID
-		require.Equal(t, p2p.PeerUpdate{
-			NodeID: peerID,
-			Status: p2p.PeerStatusUp,
-		}, peerUpdate)
-
-		channel.Out <- p2p.Envelope{To: peerID, Message: &TestMessage{Value: "hi!"}}
-		assert.Equal(t, p2p.Envelope{
-			From:    peerID,
-			Message: &TestMessage{Value: "hi!"},
-		}, <-channel.In)
-	}
-
-	// We now send a broadcast, which we should return back from all peers.
+	// Sending a broadcast should return back a message from all peers.
 	channel.Out <- p2p.Envelope{
 		Broadcast: true,
-		Message:   &TestMessage{Value: "broadcast"},
+		Message:   &p2ptest.StringMessage{Value: "foo"},
 	}
-	for i := 0; i < len(peers); i++ {
-		envelope := <-channel.In
-		require.Equal(t, &TestMessage{Value: "broadcast"}, envelope.Message)
+	seen := map[p2p.NodeID]bool{}
+	for i := 0; i < len(network.Nodes)-1; i++ {
+		e := <-channel.In
+		require.NotEmpty(t, e.From, "received message without sender")
+		require.NotEqual(t, local.NodeID, e.From)
+		require.NotContains(t, seen, e.From, "received duplicate message from %v", e.From)
+		require.Equal(t, &p2ptest.StringMessage{Value: "foo"}, e.Message)
+		seen[e.From] = true
 	}
 
 	// We then submit an error for a peer, and watch it get disconnected.
+	sub := local.PeerManager.Subscribe()
+	defer sub.Close()
+
 	channel.Error <- p2p.PeerError{
 		NodeID: peers[0].NodeID,
 		Err:    errors.New("test error"),
 	}
-	peerUpdate := <-peerUpdates.Updates()
+	peerUpdate := <-sub.Updates()
 	require.Equal(t, p2p.PeerUpdate{
 		NodeID: peers[0].NodeID,
 		Status: p2p.PeerStatusDown,
 	}, peerUpdate)
 
-	// The peer manager will automatically reconnect the peer, so we wait
-	// for that to happen.
-	peerUpdate = <-peerUpdates.Updates()
+	// The peer will automatically get reconnected by the router and peer
+	// manager, so we wait for that to happen.
+	peerUpdate = <-sub.Updates()
 	require.Equal(t, p2p.PeerUpdate{
 		NodeID: peers[0].NodeID,
 		Status: p2p.PeerStatusUp,
