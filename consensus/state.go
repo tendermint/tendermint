f35c6bc13689690adb38461e6ef5b741d13f1ac3
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-02-18 13:25:32 -0500

    remove key/attr from log

--- consensus/state.go
+++ consensus/state.go
@@ -1095,7 +1095,6 @@ func (cs *State) enterPropose(height int64, round int32) {
 		logger.Debug(
 			"propose step; our turn to propose",
 			"proposer", address,
-			"priv_validator", address,
 		)
 
 		cs.decideProposal(height, round)
@@ -1103,7 +1102,6 @@ func (cs *State) enterPropose(height int64, round int32) {
 		logger.Debug(
 			"propose step; not our turn to propose",
 			"proposer", cs.Validators.GetProposer().Address,
-			"priv_validator", address,
 		)
 	}
 }
