90fe178b5290f35ce1ebdfa1466527d7c716f256
Callum Waters <cmwaters19@gmail.com>
2021-03-03 21:02:45 +0100

    mempool: don't return an error on checktx with the same tx (#6199)

--- mempool/clist_mempool.go
+++ mempool/clist_mempool.go
@@ -277,13 +277,17 @@ func (mem *CListMempool) CheckTx(tx types.Tx, cb func(*abci.Response), txInfo Tx
 		// so we only record the sender for txs still in the mempool.
 		if e, ok := mem.txsMap.Load(TxKey(tx)); ok {
 			memTx := e.(*clist.CElement).Value.(*mempoolTx)
-			memTx.senders.LoadOrStore(txInfo.SenderID, true)
+			_, loaded := memTx.senders.LoadOrStore(txInfo.SenderID, true)
 			// TODO: consider punishing peer for dups,
 			// its non-trivial since invalid txs can become valid,
 			// but they can spam the same tx with little cost to them atm.
+			if loaded {
+				return ErrTxInCache
+			}
 		}
 
-		return ErrTxInCache
+		mem.logger.Debug("tx exists already in cache", "tx_hash", tx.Hash())
+		return nil
 	}
 
 	ctx := context.Background()
