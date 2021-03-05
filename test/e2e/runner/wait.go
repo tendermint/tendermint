d3ce1a6b5cf7cb4febdd9f9ba3b1f88062c40a61
Callum Waters <cmwaters19@gmail.com>
2021-03-05 14:19:39 +0100

    resolve conflicts

--- test/e2e/runner/wait.go
+++ test/e2e/runner/wait.go
@@ -20,18 +20,15 @@ func Wait(testnet *e2e.Testnet, blocks int64) error {
 // WaitUntil waits until a given height has been reached.
 func WaitUntil(testnet *e2e.Testnet, height int64) error {
 	logger.Info(fmt.Sprintf("Waiting for all nodes to reach height %v...", height))
-	_, err := waitForAllNodes(testnet, height, 20*time.Second)
+	_, err := waitForAllNodes(testnet, height, waitingTime(len(testnet.Nodes)))
 	if err != nil {
 		return err
 	}
 	return nil
 }
-<<<<<<< HEAD
-=======
 
 // waitingTime estimates how long it should take for a node to reach the height.
 // More nodes in a network implies we may expect a slower network and may have to wait longer.
 func waitingTime(nodes int) time.Duration {
-	return time.Duration(20+(nodes*4)) * time.Second
+	return time.Duration(20+(nodes*2)) * time.Second
 }
->>>>>>> 0566bea8b... e2e: add benchmarking functionality (#6210)
