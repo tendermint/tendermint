1e27bf4274d01e98de66b788c6b5b051e2fc0861
Callum Waters <cmwaters19@gmail.com>
2021-03-18 12:15:02 +0100

    fix waitingTime

--- test/e2e/runner/wait.go
+++ test/e2e/runner/wait.go
@@ -20,7 +20,7 @@ func Wait(testnet *e2e.Testnet, blocks int64) error {
 // WaitUntil waits until a given height has been reached.
 func WaitUntil(testnet *e2e.Testnet, height int64) error {
 	logger.Info(fmt.Sprintf("Waiting for all nodes to reach height %v...", height))
-	_, err := waitForAllNodes(testnet, height, 20*time.Second+(time.Duration(2*len(testnet.Nodes))*time.Second))
+	_, err := waitForAllNodes(testnet, height, waitingTime(len(testnet.Nodes)))
 	if err != nil {
 		return err
 	}
