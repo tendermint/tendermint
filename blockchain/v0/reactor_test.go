0fd7d75a55c2898dcf46cbe95aed1c191bcdd59f
tycho garen <garen@tychoish.com>
2021-03-18 15:43:23 -0400

    test refactors

--- blockchain/v0/reactor_test.go
+++ blockchain/v0/reactor_test.go
@@ -35,6 +35,8 @@ type reactorTestSuite struct {
 	blockchainChannels map[p2p.NodeID]*p2p.Channel
 	peerChans          map[p2p.NodeID]chan p2p.PeerUpdate
 	peerUpdates        map[p2p.NodeID]*p2p.PeerUpdates
+
+	fastSync bool
 }
 
 func setup(
@@ -59,90 +61,14 @@ func setup(
 		blockchainChannels: make(map[p2p.NodeID]*p2p.Channel, numNodes),
 		peerChans:          make(map[p2p.NodeID]chan p2p.PeerUpdate, numNodes),
 		peerUpdates:        make(map[p2p.NodeID]*p2p.PeerUpdates, numNodes),
+		fastSync:           true,
 	}
 
-	rts.network.MakeChannelsNoCleanup(t, BlockchainChannel, new(bcproto.Message), int(chBuf))
-
-	const fastSync = true
+	rts.blockchainChannels = rts.network.MakeChannelsNoCleanup(t, BlockchainChannel, new(bcproto.Message), int(chBuf))
 
 	i := 0
 	for nodeID := range rts.network.Nodes {
-		rts.nodes = append(rts.nodes, nodeID)
-		app := proxy.NewAppConns(proxy.NewLocalClientCreator(&abci.BaseApplication{}))
-		require.NoError(t, app.Start())
-
-		blockDB := dbm.NewMemDB()
-		stateDB := dbm.NewMemDB()
-		stateStore := sm.NewStore(stateDB)
-		blockStore := store.NewBlockStore(blockDB)
-
-		state, err := stateStore.LoadFromDBOrGenesisDoc(genDoc)
-		require.NoError(t, err)
-
-		db := dbm.NewMemDB()
-		stateStore = sm.NewStore(db)
-
-		blockExec := sm.NewBlockExecutor(
-			stateStore,
-			log.TestingLogger(),
-			app.Consensus(),
-			mock.Mempool{},
-			sm.EmptyEvidencePool{},
-		)
-		require.NoError(t, stateStore.Save(state))
-
-		for blockHeight := int64(1); blockHeight <= maxBlockHeights[i]; blockHeight++ {
-			lastCommit := types.NewCommit(blockHeight-1, 0, types.BlockID{}, nil)
-
-			if blockHeight > 1 {
-				lastBlockMeta := blockStore.LoadBlockMeta(blockHeight - 1)
-				lastBlock := blockStore.LoadBlock(blockHeight - 1)
-
-				vote, err := types.MakeVote(
-					lastBlock.Header.Height,
-					lastBlockMeta.BlockID,
-					state.Validators,
-					privVal,
-					lastBlock.Header.ChainID,
-					time.Now(),
-				)
-				require.NoError(t, err)
-
-				lastCommit = types.NewCommit(
-					vote.Height,
-					vote.Round,
-					lastBlockMeta.BlockID,
-					[]types.CommitSig{vote.CommitSig()},
-				)
-			}
-
-			thisBlock := makeBlock(blockHeight, state, lastCommit)
-			thisParts := thisBlock.MakePartSet(types.BlockPartSizeBytes)
-			blockID := types.BlockID{Hash: thisBlock.Hash(), PartSetHeader: thisParts.Header()}
-
-			state, _, err = blockExec.ApplyBlock(state, blockID, thisBlock)
-			require.NoError(t, err)
-
-			blockStore.SaveBlock(thisBlock, thisParts, lastCommit)
-		}
-
-		rts.peerChans[nodeID] = make(chan p2p.PeerUpdate)
-		rts.peerUpdates[nodeID] = p2p.NewPeerUpdates(rts.peerChans[nodeID])
-		rts.network.Nodes[nodeID].PeerManager.Register(rts.peerUpdates[nodeID])
-		rts.reactors[nodeID], err = NewReactor(
-			rts.logger.With("nodeID", nodeID),
-			state.Copy(),
-			blockExec,
-			blockStore,
-			nil,
-			rts.blockchainChannels[nodeID],
-			rts.peerUpdates[nodeID],
-			fastSync)
-		require.NoError(t, err)
-
-		require.NoError(t, rts.reactors[nodeID].Start())
-		require.True(t, rts.reactors[nodeID].IsRunning())
-
+		rts.addNode(t, nodeID, genDoc, privVal, maxBlockHeights[i])
 		i++
 	}
 
@@ -155,11 +81,95 @@ func setup(
 				require.NoError(t, rts.app[nodeID].Stop())
 				require.False(t, rts.reactors[nodeID].IsRunning())
 			}
-
 		}
 	})
 
 	return rts
+}
+
+func (rts *reactorTestSuite) addNode(t *testing.T,
+	nodeID p2p.NodeID,
+	genDoc *types.GenesisDoc,
+	privVal types.PrivValidator,
+	maxBlockHeight int64,
+) {
+	t.Helper()
+
+	rts.nodes = append(rts.nodes, nodeID)
+	rts.app[nodeID] = proxy.NewAppConns(proxy.NewLocalClientCreator(&abci.BaseApplication{}))
+	require.NoError(t, rts.app[nodeID].Start())
+
+	blockDB := dbm.NewMemDB()
+	stateDB := dbm.NewMemDB()
+	stateStore := sm.NewStore(stateDB)
+	blockStore := store.NewBlockStore(blockDB)
+
+	state, err := stateStore.LoadFromDBOrGenesisDoc(genDoc)
+	require.NoError(t, err)
+
+	db := dbm.NewMemDB()
+	stateStore = sm.NewStore(db)
+
+	blockExec := sm.NewBlockExecutor(
+		stateStore,
+		log.TestingLogger(),
+		rts.app[nodeID].Consensus(),
+		mock.Mempool{},
+		sm.EmptyEvidencePool{},
+	)
+	require.NoError(t, stateStore.Save(state))
+
+	for blockHeight := int64(1); blockHeight <= maxBlockHeight; blockHeight++ {
+		lastCommit := types.NewCommit(blockHeight-1, 0, types.BlockID{}, nil)
+
+		if blockHeight > 1 {
+			lastBlockMeta := blockStore.LoadBlockMeta(blockHeight - 1)
+			lastBlock := blockStore.LoadBlock(blockHeight - 1)
+
+			vote, err := types.MakeVote(
+				lastBlock.Header.Height,
+				lastBlockMeta.BlockID,
+				state.Validators,
+				privVal,
+				lastBlock.Header.ChainID,
+				time.Now(),
+			)
+			require.NoError(t, err)
+
+			lastCommit = types.NewCommit(
+				vote.Height,
+				vote.Round,
+				lastBlockMeta.BlockID,
+				[]types.CommitSig{vote.CommitSig()},
+			)
+		}
+
+		thisBlock := makeBlock(blockHeight, state, lastCommit)
+		thisParts := thisBlock.MakePartSet(types.BlockPartSizeBytes)
+		blockID := types.BlockID{Hash: thisBlock.Hash(), PartSetHeader: thisParts.Header()}
+
+		state, _, err = blockExec.ApplyBlock(state, blockID, thisBlock)
+		require.NoError(t, err)
+
+		blockStore.SaveBlock(thisBlock, thisParts, lastCommit)
+	}
+
+	rts.peerChans[nodeID] = make(chan p2p.PeerUpdate)
+	rts.peerUpdates[nodeID] = p2p.NewPeerUpdates(rts.peerChans[nodeID])
+	rts.network.Nodes[nodeID].PeerManager.Register(rts.peerUpdates[nodeID])
+	rts.reactors[nodeID], err = NewReactor(
+		rts.logger.With("nodeID", nodeID),
+		state.Copy(),
+		blockExec,
+		blockStore,
+		nil,
+		rts.blockchainChannels[nodeID],
+		rts.peerUpdates[nodeID],
+		rts.fastSync)
+	require.NoError(t, err)
+
+	require.NoError(t, rts.reactors[nodeID].Start())
+	require.True(t, rts.reactors[nodeID].IsRunning())
 }
 
 func (rts *reactorTestSuite) start(t *testing.T) {
@@ -177,28 +187,15 @@ func TestReactor_AbruptDisconnect(t *testing.T) {
 
 	genDoc, privVals := randGenesisDoc(config, 1, false, 30)
 	maxBlockHeight := int64(64)
-	testSuites := []*reactorTestSuite{
-		setup(t, genDoc, privVals, maxBlockHeight, 0),
-		setup(t, genDoc, privVals, 0, 0),
-	}
 
-	require.Equal(t, maxBlockHeight, testSuites[0].reactor.store.Height())
+	rts := setup(t, genDoc, privVals[0], []int64{maxBlockHeight, 0}, 0)
 
-	for _, s := range testSuites {
-		simulateRouter(s, testSuites, true)
+	require.Equal(t, maxBlockHeight, rts.reactors[rts.nodes[0]].store.Height())
 
-		// connect reactor to every other reactor
-		for _, ss := range testSuites {
-			if s.peerID != ss.peerID {
-				s.peerUpdatesCh <- p2p.PeerUpdate{
-					Status: p2p.PeerStatusUp,
-					NodeID: ss.peerID,
-				}
-			}
-		}
-	}
+	rts.start(t)
 
-	secondaryPool := testSuites[1].reactor.pool
+	secondaryPool := rts.reactors[rts.nodes[1]].pool
+
 	require.Eventually(
 		t,
 		func() bool {
@@ -212,10 +209,11 @@ func TestReactor_AbruptDisconnect(t *testing.T) {
 
 	// Remove synced node from the syncing node which should not result in any
 	// deadlocks or race conditions within the context of poolRoutine.
-	testSuites[1].peerUpdatesCh <- p2p.PeerUpdate{
+	rts.peerChans[rts.nodes[1]] <- p2p.PeerUpdate{
 		Status: p2p.PeerStatusDown,
-		NodeID: testSuites[0].peerID,
+		NodeID: rts.nodes[0],
 	}
+	require.NoError(t, rts.network.Nodes[rts.nodes[1]].PeerManager.Disconnected(rts.nodes[0]))
 }
 
 func TestReactor_NoBlockResponse(t *testing.T) {
@@ -224,26 +222,12 @@ func TestReactor_NoBlockResponse(t *testing.T) {
 
 	genDoc, privVals := randGenesisDoc(config, 1, false, 30)
 	maxBlockHeight := int64(65)
-	testSuites := []*reactorTestSuite{
-		setup(t, genDoc, privVals, maxBlockHeight, 0),
-		setup(t, genDoc, privVals, 0, 0),
-	}
 
-	require.Equal(t, maxBlockHeight, testSuites[0].reactor.store.Height())
+	rts := setup(t, genDoc, privVals[0], []int64{maxBlockHeight, 0}, 0)
 
-	for _, s := range testSuites {
-		simulateRouter(s, testSuites, true)
+	require.Equal(t, maxBlockHeight, rts.reactors[rts.nodes[0]].store.Height())
 
-		// connect reactor to every other reactor
-		for _, ss := range testSuites {
-			if s.peerID != ss.peerID {
-				s.peerUpdatesCh <- p2p.PeerUpdate{
-					Status: p2p.PeerStatusUp,
-					NodeID: ss.peerID,
-				}
-			}
-		}
-	}
+	rts.start(t)
 
 	testCases := []struct {
 		height   int64
@@ -255,7 +239,7 @@ func TestReactor_NoBlockResponse(t *testing.T) {
 		{100, false},
 	}
 
-	secondaryPool := testSuites[1].reactor.pool
+	secondaryPool := rts.reactors[rts.nodes[1]].pool
 	require.Eventually(
 		t,
 		func() bool { return secondaryPool.MaxPeerHeight() > 0 && secondaryPool.IsCaughtUp() },
@@ -265,7 +249,7 @@ func TestReactor_NoBlockResponse(t *testing.T) {
 	)
 
 	for _, tc := range testCases {
-		block := testSuites[1].reactor.store.LoadBlock(tc.height)
+		block := rts.reactors[rts.nodes[1]].store.LoadBlock(tc.height)
 		if tc.existent {
 			require.True(t, block != nil)
 		} else {
@@ -286,36 +270,18 @@ func TestReactor_BadBlockStopsPeer(t *testing.T) {
 	maxBlockHeight := int64(48)
 	genDoc, privVals := randGenesisDoc(config, 1, false, 30)
 
-	testSuites := []*reactorTestSuite{
-		setup(t, genDoc, privVals, maxBlockHeight, 1000), // fully synced node
-		setup(t, genDoc, privVals, 0, 1000),
-		setup(t, genDoc, privVals, 0, 1000),
-		setup(t, genDoc, privVals, 0, 1000),
-		setup(t, genDoc, privVals, 0, 1000), // new node
-	}
+	rts := setup(t, genDoc, privVals[0], []int64{maxBlockHeight, 0, 0, 0, 0}, 1000)
 
-	require.Equal(t, maxBlockHeight, testSuites[0].reactor.store.Height())
+	require.Equal(t, maxBlockHeight, rts.reactors[rts.nodes[0]].store.Height())
 
-	for _, s := range testSuites[:len(testSuites)-1] {
-		simulateRouter(s, testSuites, true)
-
-		// connect reactor to every other reactor except the new node
-		for _, ss := range testSuites[:len(testSuites)-1] {
-			if s.peerID != ss.peerID {
-				s.peerUpdatesCh <- p2p.PeerUpdate{
-					Status: p2p.PeerStatusUp,
-					NodeID: ss.peerID,
-				}
-			}
-		}
-	}
+	rts.start(t)
 
 	require.Eventually(
 		t,
 		func() bool {
 			caughtUp := true
-			for _, s := range testSuites[1 : len(testSuites)-1] {
-				if s.reactor.pool.MaxPeerHeight() == 0 || !s.reactor.pool.IsCaughtUp() {
+			for _, id := range rts.nodes[1 : len(rts.nodes)-1] {
+				if rts.reactors[id].pool.MaxPeerHeight() == 0 || !rts.reactors[id].pool.IsCaughtUp() {
 					caughtUp = false
 				}
 			}
@@ -327,8 +293,8 @@ func TestReactor_BadBlockStopsPeer(t *testing.T) {
 		"expected all nodes to be fully synced",
 	)
 
-	for _, s := range testSuites[:len(testSuites)-1] {
-		require.Len(t, s.reactor.pool.peers, 3)
+	for _, id := range rts.nodes[:len(rts.nodes)-1] {
+		require.Len(t, rts.reactors[id].pool.peers, 3)
 	}
 
 	// Mark testSuites[3] as an invalid peer which will cause newSuite to disconnect
@@ -337,28 +303,18 @@ func TestReactor_BadBlockStopsPeer(t *testing.T) {
 	// XXX: This causes a potential race condition.
 	// See: https://github.com/tendermint/tendermint/issues/6005
 	otherGenDoc, otherPrivVals := randGenesisDoc(config, 1, false, 30)
-	otherSuite := setup(t, otherGenDoc, otherPrivVals, maxBlockHeight, 0)
-	testSuites[3].reactor.store = otherSuite.reactor.store
+	newNode := rts.network.AddNode(t)
+	rts.addNode(t, newNode.NodeID, otherGenDoc, otherPrivVals[0], maxBlockHeight)
 
 	// add a fake peer just so we do not wait for the consensus ticker to timeout
-	otherSuite.reactor.pool.SetPeerRange("00ff", 10, 10)
-
-	// start the new peer's faux router
-	newSuite := testSuites[len(testSuites)-1]
-	simulateRouter(newSuite, testSuites, false)
-
-	// connect all nodes to the new peer
-	for _, s := range testSuites[:len(testSuites)-1] {
-		newSuite.peerUpdatesCh <- p2p.PeerUpdate{
-			Status: p2p.PeerStatusUp,
-			NodeID: s.peerID,
-		}
-	}
+	rts.reactors[newNode.NodeID].pool.SetPeerRange("00ff", 10, 10)
 
 	// wait for the new peer to catch up and become fully synced
 	require.Eventually(
 		t,
-		func() bool { return newSuite.reactor.pool.MaxPeerHeight() > 0 && newSuite.reactor.pool.IsCaughtUp() },
+		func() bool {
+			return rts.reactors[newNode.NodeID].pool.MaxPeerHeight() > 0 && rts.reactors[newNode.NodeID].pool.IsCaughtUp()
+		},
 		10*time.Minute,
 		10*time.Millisecond,
 		"expected new node to be fully synced",
@@ -366,11 +322,11 @@ func TestReactor_BadBlockStopsPeer(t *testing.T) {
 
 	require.Eventuallyf(
 		t,
-		func() bool { return len(newSuite.reactor.pool.peers) < len(testSuites)-1 },
+		func() bool { return len(rts.reactors[newNode.NodeID].pool.peers) < len(rts.nodes)-1 },
 		10*time.Minute,
 		10*time.Millisecond,
 		"invalid number of peers; expected < %d, got: %d",
-		len(testSuites)-1,
-		len(newSuite.reactor.pool.peers),
+		len(rts.nodes)-1,
+		len(rts.reactors[newNode.NodeID].pool.peers),
 	)
 }
