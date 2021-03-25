027ee8afe89d8f8513a4ce49d899938cf6c991d0
Sam Kleinman <garen@tychoish.com>
2021-03-25 12:26:56 -0400

    logging: shorten precommit log message (#6270)
    This is an attempt to clean up the logging message as requested in #6269.
    
    (cherry picked from commit 3f9066b290ba2de266b42aa6ad2b076c7be4f5df)

--- types/block.go
+++ types/block.go
@@ -1180,7 +1180,7 @@ func (blockID BlockID) Key() string {
 		panic(err)
 	}
 
-	return string(blockID.Hash) + string(bz)
+	return fmt.Sprint(string(blockID.Hash), string(bz))
 }
 
 // ValidateBasic performs basic validation.
