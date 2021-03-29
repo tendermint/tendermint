67aeeed4d24bae79de6505582e6aa44b3bbe624f
Frojdi Dymylja <33157909+fdymylja@users.noreply.github.com>
2021-03-29 11:24:52 +0200

    fix: jsonrpc url parsing and dial function (#6264)
    This PR fixes how the jsonrpc parses the URL, and how the dial function connects to the RPC.
    
    Closes: https://github.com/tendermint/tendermint/issues/6260
    
    (cherry picked from commit 9ecfcc93a6364bbecc9e1d7740b53602eda667eb)

--- rpc/jsonrpc/client/http_json_client.go
+++ rpc/jsonrpc/client/http_json_client.go
@@ -21,6 +21,7 @@ const (
 	protoWSS   = "wss"
 	protoWS    = "ws"
 	protoTCP   = "tcp"
+	protoUNIX  = "unix"
 )
 
 //-------------------------------------------------------------
@@ -28,6 +29,8 @@ const (
 // Parsed URL structure
 type parsedURL struct {
 	url.URL
+
+	isUnixSocket bool
 }
 
 // Parse URL and set defaults
@@ -42,7 +45,16 @@ func newParsedURL(remoteAddr string) (*parsedURL, error) {
 		u.Scheme = protoTCP
 	}
 
-	return &parsedURL{*u}, nil
+	pu := &parsedURL{
+		URL:          *u,
+		isUnixSocket: false,
+	}
+
+	if u.Scheme == protoUNIX {
+		pu.isUnixSocket = true
+	}
+
+	return pu, nil
 }
 
 // Change protocol to HTTP for unknown protocols and TCP protocol - useful for RPC connections
@@ -65,8 +77,24 @@ func (u parsedURL) GetHostWithPath() string {
 
 // Get a trimmed address - useful for WS connections
 func (u parsedURL) GetTrimmedHostWithPath() string {
-	// replace / with . for http requests (kvstore domain)
+	// if it's not an unix socket we return the normal URL
+	if !u.isUnixSocket {
+		return u.GetHostWithPath()
+	}
+	// if it's a unix socket we replace the host slashes with a period
+	// this is because otherwise the http.Client would think that the
+	// domain is invalid.
 	return strings.ReplaceAll(u.GetHostWithPath(), "/", ".")
+}
+
+// GetDialAddress returns the endpoint to dial for the parsed URL
+func (u parsedURL) GetDialAddress() string {
+	// if it's not a unix socket we return the host, example: localhost:443
+	if !u.isUnixSocket {
+		return u.Host
+	}
+	// otherwise we return the path of the unix socket, ex /tmp/socket
+	return u.GetHostWithPath()
 }
 
 // Get a trimmed address with protocol - useful as address in RPC connections
@@ -350,7 +378,7 @@ func makeHTTPDialer(remoteAddr string) (func(string, string) (net.Conn, error), 
 	}
 
 	dialFn := func(proto, addr string) (net.Conn, error) {
-		return net.Dial(protocol, u.GetHostWithPath())
+		return net.Dial(protocol, u.GetDialAddress())
 	}
 
 	return dialFn, nil
