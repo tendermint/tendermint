d9916c9af9d04d5eab5ab120afe20affef432aea
Callum Waters <cmwaters19@gmail.com>
2021-04-13 17:59:15 +0200

    e2e: tx load to use broadcast sync instead of commit (#6347)
    (cherry picked from commit 2b8aa65e4f5ce818a52dd66fa0252f62e5de4fc4)
    
    # Conflicts:
    #	test/e2e/runner/load.go

--- test/e2e/runner/load.go
+++ test/e2e/runner/load.go
@@ -13,9 +13,16 @@ import (
 	"github.com/tendermint/tendermint/types"
 )
 
+<<<<<<< HEAD
 // Load generates transactions against the network until the given
 // context is cancelled.
 func Load(ctx context.Context, testnet *e2e.Testnet) error {
+=======
+// Load generates transactions against the network until the given context is
+// canceled. A multiplier of greater than one can be supplied if load needs to
+// be generated beyond a minimum amount.
+func Load(ctx context.Context, testnet *e2e.Testnet, multiplier int) error {
+>>>>>>> 2b8aa65e4... e2e: tx load to use broadcast sync instead of commit (#6347)
 	// Since transactions are executed across all nodes in the network, we need
 	// to reduce transaction load for larger networks to avoid using too much
 	// CPU. This gives high-throughput small networks and low-throughput large ones.
@@ -37,7 +44,7 @@ func Load(ctx context.Context, testnet *e2e.Testnet) error {
 	logger.Info(fmt.Sprintf("Starting transaction load (%v workers)...", concurrency))
 	started := time.Now()
 
-	go loadGenerate(ctx, chTx)
+	go loadGenerate(ctx, chTx, multiplier)
 
 	for w := 0; w < concurrency; w++ {
 		go loadProcess(ctx, testnet, chTx, chSuccess)
@@ -64,14 +71,19 @@ func Load(ctx context.Context, testnet *e2e.Testnet) error {
 	}
 }
 
+<<<<<<< HEAD
 // loadGenerate generates jobs until the context is cancelled
 func loadGenerate(ctx context.Context, chTx chan<- types.Tx) {
+=======
+// loadGenerate generates jobs until the context is canceled
+func loadGenerate(ctx context.Context, chTx chan<- types.Tx, multiplier int) {
+>>>>>>> 2b8aa65e4... e2e: tx load to use broadcast sync instead of commit (#6347)
 	for i := 0; i < math.MaxInt64; i++ {
 		// We keep generating the same 1000 keys over and over, with different values.
 		// This gives a reasonable load without putting too much data in the app.
 		id := i % 1000
 
-		bz := make([]byte, 2048) // 4kb hex-encoded
+		bz := make([]byte, 1024) // 1kb hex-encoded
 		_, err := rand.Read(bz)
 		if err != nil {
 			panic(fmt.Sprintf("Failed to read random bytes: %v", err))
@@ -80,7 +92,12 @@ func loadGenerate(ctx context.Context, chTx chan<- types.Tx) {
 
 		select {
 		case chTx <- tx:
+<<<<<<< HEAD
 			time.Sleep(10 * time.Millisecond)
+=======
+			time.Sleep(time.Duration(100/multiplier) * time.Millisecond)
+
+>>>>>>> 2b8aa65e4... e2e: tx load to use broadcast sync instead of commit (#6347)
 		case <-ctx.Done():
 			close(chTx)
 			return
@@ -103,10 +120,24 @@ func loadProcess(ctx context.Context, testnet *e2e.Testnet, chTx <-chan types.Tx
 			if err != nil {
 				continue
 			}
+<<<<<<< HEAD
 			clients[node.Name] = client
 		}
 		_, err = client.BroadcastTxCommit(ctx, tx)
 		if err != nil {
+=======
+
+			// check that the node is up
+			_, err = client.Health(ctx)
+			if err != nil {
+				continue
+			}
+
+			clients[node.Name] = client
+		}
+
+		if _, err = client.BroadcastTxSync(ctx, tx); err != nil {
+>>>>>>> 2b8aa65e4... e2e: tx load to use broadcast sync instead of commit (#6347)
 			continue
 		}
 		chSuccess <- tx
