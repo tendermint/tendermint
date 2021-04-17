2ee7bfe8fbb5d51019191cab9da0e537f91179b7
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2021-04-17 14:05:32 +0200

    actually appease the linter 2.0

--- p2p/pex/addrbook.go
+++ p2p/pex/addrbook.go
@@ -385,7 +385,6 @@ func (a *addrBook) ReinstateBadPeers() {
 // GetSelection implements AddrBook.
 // It randomly selects some addresses (old & new). Suitable for peer-exchange protocols.
 // Must never return a nil address.
-// nolint:gosec // G404: Use of weak random number generator
 func (a *addrBook) GetSelection() []*p2p.NetAddress {
 	a.mtx.Lock()
 	defer a.mtx.Unlock()
@@ -416,6 +415,7 @@ func (a *addrBook) GetSelection() []*p2p.NetAddress {
 	// `numAddresses' since we are throwing the rest.
 	for i := 0; i < numAddresses; i++ {
 		// pick a number between current index and the end
+		// nolint:gosec // G404: Use of weak random number generator
 		j := mrand.Intn(len(allAddr)-i) + i
 		allAddr[i], allAddr[j] = allAddr[j], allAddr[i]
 	}
@@ -680,6 +680,7 @@ func (a *addrBook) addAddress(addr, src *p2p.NetAddress) error {
 		}
 		// The more entries we have, the less likely we are to add more.
 		factor := int32(2 * len(ka.Buckets))
+		// nolint:gosec // G404: Use of weak random number generator
 		if mrand.Int31n(factor) != 0 {
 			return nil
 		}
