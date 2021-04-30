8448d1847163b41eea7de97702da34ab388ea1d7
Quantum Explorer <quantum@dash.org>
2021-04-30 20:51:09 +0700

    fixed TestBroadcastEvidence_DuplicateVoteEvidence

--- evidence/verify.go
+++ evidence/verify.go
@@ -238,7 +238,8 @@ func VerifyDuplicateVote(e *types.DuplicateVoteEvidence, chainID string, valSet 
 	va := e.VoteA.ToProto()
 	vb := e.VoteB.ToProto()
 	// Signatures must be valid
-	if !pubKey.VerifySignatureDigest(types.VoteBlockSignId(chainID, va, valSet.QuorumType, valSet.QuorumHash), e.VoteA.BlockSignature) {
+	blockSignId := types.VoteBlockSignId(chainID, va, valSet.QuorumType, valSet.QuorumHash)
+	if !pubKey.VerifySignatureDigest(blockSignId, e.VoteA.BlockSignature) {
 		return fmt.Errorf("verifying VoteA: %s", types.ErrVoteInvalidBlockSignature.Error())
 	}
 	if !pubKey.VerifySignatureDigest(types.VoteBlockSignId(chainID, vb, valSet.QuorumType, valSet.QuorumHash), e.VoteB.BlockSignature) {
