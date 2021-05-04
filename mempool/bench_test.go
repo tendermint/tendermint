f366ae3c875a4f4f61f37f4b39383558ac5a58cc
Sam Kleinman <garen@tychoish.com>
2021-05-04 10:45:32 -0400

    mempool: benchmark improvements (#6418)

--- mempool/bench_test.go
+++ mempool/bench_test.go
@@ -13,13 +13,14 @@ func BenchmarkReap(b *testing.B) {
 	cc := proxy.NewLocalClientCreator(app)
 	mempool, cleanup := newMempoolWithApp(cc)
 	defer cleanup()
+	mempool.config.Size = 100000
 
 	size := 10000
 	for i := 0; i < size; i++ {
 		tx := make([]byte, 8)
 		binary.BigEndian.PutUint64(tx, uint64(i))
 		if err := mempool.CheckTx(tx, nil, TxInfo{}); err != nil {
-			b.Error(err)
+			b.Fatal(err)
 		}
 	}
 	b.ResetTimer()
@@ -34,11 +35,13 @@ func BenchmarkCheckTx(b *testing.B) {
 	mempool, cleanup := newMempoolWithApp(cc)
 	defer cleanup()
 
+	mempool.config.Size = 1000000
+
 	for i := 0; i < b.N; i++ {
 		tx := make([]byte, 8)
 		binary.BigEndian.PutUint64(tx, uint64(i))
 		if err := mempool.CheckTx(tx, nil, TxInfo{}); err != nil {
-			b.Error(err)
+			b.Fatal(err)
 		}
 	}
 }
