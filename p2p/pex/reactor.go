9339645b53498d17f57c3a4a6b055faf97947c49
Callum Waters <cmwaters19@gmail.com>
2021-05-10 10:07:52 +0200

    add channel descriptors to open channel

--- p2p/pex/reactor.go
+++ p2p/pex/reactor.go
@@ -45,16 +45,14 @@ const (
 // within each reactor (as they are now) or, considering that the reactor doesn't
 // really need to care about the channel descriptors, if they should be housed
 // in the node module.
-func ChannelDescriptors() []*conn.ChannelDescriptor {
-	return []*conn.ChannelDescriptor{
-		{
-			ID:                  PexChannel,
-			Priority:            1,
-			SendQueueCapacity:   10,
-			RecvMessageCapacity: maxMsgSize,
+func ChannelDescriptor() conn.ChannelDescriptor {
+	return conn.ChannelDescriptor{
+		ID:                  PexChannel,
+		Priority:            1,
+		SendQueueCapacity:   10,
+		RecvMessageCapacity: maxMsgSize,
 
-			MaxSendBytes: 200,
-		},
+		MaxSendBytes: 200,
 	}
 }
 
