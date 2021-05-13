010af8e5b304fdac5e55e6ede0eab48084aab479
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-05-13 08:36:14 -0400

    config updates

--- config/toml.go
+++ config/toml.go
@@ -355,6 +355,11 @@ dial-timeout = "{{ .P2P.DialTimeout }}"
 #######################################################
 [mempool]
 
+# Mempool version to use:
+#   1) "v0" - The legacy non-prioritized mempool reactor.
+#   2) "v1" (default) - The prioritized mempool reactor.
+version = "{{ .Mempool.Version }}"
+
 recheck = {{ .Mempool.Recheck }}
 broadcast = {{ .Mempool.Broadcast }}
 
