f30d421af8b25a859087105a07f5fc744d0e9938
marbar3778 <marbar3778@yahoo.com>
2021-05-14 13:12:26 +0200

    fix linting error

--- proto/tendermint/consensus/message_test.go
+++ proto/tendermint/consensus/message_test.go
@@ -12,7 +12,6 @@ import (
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
-
 func TestHasVoteVector(t *testing.T) {
 	testcases := []struct{
 		hasVote tmcons.HasVote
@@ -21,7 +20,8 @@ func TestHasVoteVector(t *testing.T) {
 	}{
 		{tmcons.HasVote{1,3,tmproto.PrevoteType,1},"3a080801100318012001", false},
 		{tmcons.HasVote{2,2,tmproto.PrecommitType,2},"3a080802100218022002", false},
-		{tmcons.HasVote{math.MaxInt64,math.MaxInt32,tmproto.ProposalType,math.MaxInt32},"3a1808ffffffffffffffff7f10ffffffff07182020ffffffff07", false},
+		{tmcons.HasVote{math.MaxInt64,math.MaxInt32,tmproto.ProposalType,math.MaxInt32},
+		"3a1808ffffffffffffffff7f10ffffffff07182020ffffffff07", false},
 	}
 
 	for i, tc :=range  testcases {
