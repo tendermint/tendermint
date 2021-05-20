cb373f4638a3fd6cae0e2e29152b85d5c6999ff0
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-05-20 11:02:00 -0400

    mempool: tests

--- mempool/v1/mempool_test.go
+++ mempool/v1/mempool_test.go
@@ -1,14 +1,19 @@
 package v1
 
 import (
+	"math/rand"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/abci/example/kvstore"
+	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/libs/log"
+	"github.com/tendermint/tendermint/mempool"
 	"github.com/tendermint/tendermint/proxy"
+	"github.com/tendermint/tendermint/types"
 )
 
 func setup(t *testing.T) *TxMempool {
@@ -27,4 +32,71 @@ func setup(t *testing.T) *TxMempool {
 	})
 
 	return NewTxMempool(log.TestingLogger().With("test", t.Name()), cfg.Mempool, appConnMem, 0)
+}
+
+func checkTxs(t *testing.T, txmp *TxMempool, numTxs int, peerID uint16) types.Txs {
+	txs := make(types.Txs, numTxs)
+	txInfo := mempool.TxInfo{SenderID: peerID}
+
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for i := 0; i < numTxs; i++ {
+		txBytes := make([]byte, 20)
+		_, err := rng.Read(txBytes)
+		require.NoError(t, err)
+
+		txs[i] = txBytes
+		require.NoError(t, txmp.CheckTx(txBytes, nil, txInfo))
+	}
+
+	return txs
+}
+
+func TestTxMempool_TxsAvailable(t *testing.T) {
+	txmp := setup(t)
+	txmp.EnableTxsAvailable()
+
+	ensureNoTxFire := func() {
+		timer := time.NewTimer(500 * time.Millisecond)
+		select {
+		case <-txmp.TxsAvailable():
+			require.Fail(t, "unexpected transactions event")
+		case <-timer.C:
+		}
+	}
+
+	ensureTxFire := func() {
+		timer := time.NewTimer(500 * time.Millisecond)
+		select {
+		case <-txmp.TxsAvailable():
+		case <-timer.C:
+			require.Fail(t, "expected transactions event")
+		}
+	}
+
+	// ensure no event as we have not executed any transactions yet
+	ensureNoTxFire()
+
+	// Execute CheckTx for some transactions and ensure TxsAvailable only fires
+	// once.
+	txs := checkTxs(t, txmp, 100, 0)
+	ensureTxFire()
+	ensureNoTxFire()
+
+	responses := make([]*abci.ResponseDeliverTx, len(txs[:50]))
+	for i := 0; i < len(responses); i++ {
+		responses[i] = &abci.ResponseDeliverTx{Code: abci.CodeTypeOK}
+	}
+
+	// commit half the transactions and ensure we fire an event
+	txmp.Lock()
+	require.NoError(t, txmp.Update(1, txs[:50], responses, nil, nil))
+	txmp.Unlock()
+	ensureTxFire()
+	ensureNoTxFire()
+
+	// Execute CheckTx for more transactions and ensure we do not fire another
+	// event as we're still on the same height (1).
+	_ = checkTxs(t, txmp, 100, 0)
+	ensureNoTxFire()
 }
