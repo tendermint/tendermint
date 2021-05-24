e2a26c732cd578aa802cdb11eca051303684a3db
Sam Kleinman <garen@tychoish.com>
2021-05-24 14:10:56 -0400

    e2e: prevent non-viable testnets (#6486)

--- test/e2e/runner/main.go
+++ test/e2e/runner/main.go
@@ -97,7 +97,7 @@ func NewCLI() *CLI {
 			if err := <-chLoadResult; err != nil {
 				return err
 			}
-			if err := Wait(cli.testnet, 5); err != nil { // wait for network to settle before tests
+			if err := Wait(cli.testnet, 8); err != nil { // wait for network to settle before tests
 				return err
 			}
 			if err := Test(cli.testnet); err != nil {
