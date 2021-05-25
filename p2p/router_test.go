4d3e6705308fc2835f3e7fc165da91bd1fdd519a
tycho garen <garen@tychoish.com>
2021-05-25 16:51:06 -0400

    more time

--- p2p/router_test.go
+++ p2p/router_test.go
@@ -677,7 +677,7 @@ func TestRouter_DialPeers_Parallel(t *testing.T) {
 
 	require.Eventually(t, func() bool {
 		return len(dialCh) == 3
-	}, 3*time.Second, 10*time.Millisecond)
+	}, 5*time.Second, 10*time.Millisecond)
 	close(closeCh)
 	time.Sleep(500 * time.Millisecond)
 
