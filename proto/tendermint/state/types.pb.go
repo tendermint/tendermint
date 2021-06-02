99051a05a147c5878cc7d49f91f6d8a12853d170
tycho garen <garen@tychoish.com>
2021-06-02 10:49:32 -0400

    revert changes to proto directory

--- proto/tendermint/state/types.pb.go
+++ proto/tendermint/state/types.pb.go
@@ -5,11 +5,6 @@ package state
 
 import (
 	fmt "fmt"
-	io "io"
-	math "math"
-	math_bits "math/bits"
-	time "time"
-
 	_ "github.com/gogo/protobuf/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
 	_ "github.com/gogo/protobuf/types"
@@ -17,6 +12,10 @@ import (
 	types "github.com/tendermint/tendermint/abci/types"
 	types1 "github.com/tendermint/tendermint/proto/tendermint/types"
 	version "github.com/tendermint/tendermint/proto/tendermint/version"
+	io "io"
+	math "math"
+	math_bits "math/bits"
+	time "time"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
