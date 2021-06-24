c211f013357e067bd179f21ff6fd2e65b1edd62c
Anton Suprunchuk <antouhou@gmail.com>
2021-06-24 10:55:31 +0300

    make rpc mock work

--- dashcore/rpc/mock.go
+++ dashcore/rpc/mock.go
@@ -14,41 +14,78 @@ func NewRpcClientMock() (RpcClient, error) {
 
 // Close closes the underlying connection
 func (rpcClientMock *rpcClientMock) Close() error {
-	rpcClientMock.endpoint.Shutdown()
 	return nil
 }
 
 // Ping sends a ping request to the remote signer
 func (rpcClientMock *rpcClientMock) Ping() error {
-	err := rpcClientMock.endpoint.Ping()
-	if err != nil {
-		return err
-	}
-
-	pb, err := rpcClientMock.endpoint.GetPeerInfo()
-	if pb == nil {
-		return err
-	}
-
 	return nil
 }
 
 func (rpcClientMock *rpcClientMock) QuorumInfo(quorumType btcjson.LLMQType, quorumHash string, includeSkShare bool) (*btcjson.QuorumInfoResult, error) {
-	return rpcClientMock.endpoint.QuorumInfo(quorumType, quorumHash, includeSkShare)
+	return &btcjson.QuorumInfoResult{
+		Height:          0,
+		Type:            "",
+		QuorumHash:      "",
+		MinedBlock:      "",
+		Members:         nil,
+		QuorumPublicKey: "",
+		SecretKeyShare:  "",
+	}, nil
 }
 
 func (rpcClientMock *rpcClientMock) MasternodeStatus() (*btcjson.MasternodeStatusResult, error) {
-	return rpcClientMock.endpoint.MasternodeStatus()
+	return &btcjson.MasternodeStatusResult{
+		Outpoint:        "",
+		Service:         "",
+		ProTxHash:       "",
+		CollateralHash:  "",
+		CollateralIndex: 0,
+		DMNState:        btcjson.DMNState{},
+		State:           "",
+		Status:          "",
+	}, nil
 }
 
 func (rpcClientMock *rpcClientMock) GetNetworkInfo() (*btcjson.GetNetworkInfoResult, error) {
-	return rpcClientMock.endpoint.GetNetworkInfo()
+	return &btcjson.GetNetworkInfoResult{
+		Version:         0,
+		SubVersion:      "",
+		ProtocolVersion: 0,
+		LocalServices:   "",
+		LocalRelay:      false,
+		TimeOffset:      0,
+		Connections:     0,
+		NetworkActive:   false,
+		Networks:        nil,
+		RelayFee:        0,
+		IncrementalFee:  0,
+		LocalAddresses:  nil,
+		Warnings:        "",
+	}, nil
 }
 
 func (rpcClientMock *rpcClientMock) MasternodeListJSON(filter string) (map[string]btcjson.MasternodelistResultJSON, error) {
-	return rpcClientMock.endpoint.MasternodeListJSON(filter)
+	m := make(map[string]btcjson.MasternodelistResultJSON)
+	m[""] = btcjson.MasternodelistResultJSON{
+		Address:           "",
+		Collateraladdress: "",
+		Lastpaidblock:     0,
+		Lastpaidtime:      0,
+		Owneraddress:      "",
+		Payee:             "",
+		ProTxHash:         "",
+		Pubkeyoperator:    "",
+		Status:            "",
+		Votingaddress:     "",
+	}
+
+	return m, nil
 }
 
 func (rpcClientMock *rpcClientMock) QuorumSign(quorumType btcjson.LLMQType, requestID string, messageHash string, quorumHash string, submit bool) (*btcjson.QuorumSignResultWithBool, error) {
-	return rpcClientMock.endpoint.QuorumSign(quorumType, requestID, messageHash, quorumHash, submit)
+	return &btcjson.QuorumSignResultWithBool{
+		QuorumSignResult: btcjson.QuorumSignResult{},
+		Result:           false,
+	}, nil
 }
