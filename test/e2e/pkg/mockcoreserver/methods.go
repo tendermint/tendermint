86b9caee6eda70d8a8df26379c25ae789e528c4e
Quantum Explorer <quantum@dash.org>
2021-07-04 06:24:43 +0700

    improvements and bug fixes

--- test/e2e/pkg/mockcoreserver/methods.go
+++ test/e2e/pkg/mockcoreserver/methods.go
@@ -56,7 +56,7 @@ func WithQuorumVerifyMethod(cs CoreServer, times int) MethodFunc {
 	call := OnMethod(func(req btcjson.Request) (interface{}, error) {
 		cmd := btcjson.QuorumCmd{}
 		fmt.Printf("request is %v\n", req)
-		err := unmarshalCmd(req, &cmd.SubCmd, &cmd.LLMQType, &cmd.RequestID, &cmd.MessageHash, &cmd.Signature, &cmd.QuorumHash)
+		err := unmarshalCmd(req, &cmd.SubCmd, &cmd.LLMQType, &cmd.RequestID, &cmd.MessageHash, &cmd.QuorumHash, &cmd.Signature)
 		fmt.Printf("cmd is %v\n", cmd)
 		if err != nil {
 			return nil, err
