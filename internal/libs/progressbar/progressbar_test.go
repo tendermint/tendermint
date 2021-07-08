38813cebeacb0941c764cbb890901cf02c7e5324
jayt106 <jaytseng106@gmail.com>
2021-07-08 14:45:07 -0400

    revise progressbar_test

--- internal/libs/progressbar/progressbar_test.go
+++ internal/libs/progressbar/progressbar_test.go
@@ -1,19 +1,41 @@
-package progressbar_test
+package progressbar
 
 import (
 	"testing"
 	"time"
 
-	"github.com/tendermint/tendermint/internal/libs/progressbar"
+	"github.com/stretchr/testify/require"
 )
 
 func TestProgressBar(t *testing.T) {
-	var bar progressbar.Bar
-	bar.NewOption(0, 100)
+	zero := int64(0)
+	hundred := int64(100)
 
-	for i := 0; i <= 100; i++ {
-		time.Sleep(5 * time.Millisecond)
-		bar.Play(int64(i))
+	var bar Bar
+	bar.NewOption(zero, hundred)
+
+	require.Equal(t, zero, bar.start)
+	require.Equal(t, zero, bar.cur)
+	require.Equal(t, hundred, bar.total)
+	require.Equal(t, zero, bar.percent)
+	require.Equal(t, "█", bar.graph)
+	require.Equal(t, "", bar.rate)
+
+	defer bar.Finish()
+	for i := zero; i <= hundred; i++ {
+		time.Sleep(1 * time.Millisecond)
+		bar.Play(i)
 	}
-	bar.Finish()
+
+	require.Equal(t, zero, bar.start)
+	require.Equal(t, hundred, bar.cur)
+	require.Equal(t, hundred, bar.total)
+	require.Equal(t, hundred, bar.percent)
+
+	var rate string
+	for i := zero; i < hundred/2; i++ {
+		rate += "█"
+	}
+
+	require.Equal(t, rate, bar.rate)
 }
