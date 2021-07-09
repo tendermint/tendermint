40217c9f76979a97e10ed2ce1725f67ee61e70fd
marbar3778 <marbar3778@yahoo.com>
2021-07-09 16:55:11 +0200

    cleanup docs

--- cmd/tendermint/commands/run_node.go
+++ cmd/tendermint/commands/run_node.go
@@ -48,9 +48,7 @@ func AddNodeFlags(cmd *cobra.Command) {
 		"proxy-app",
 		config.ProxyApp,
 		"proxy app address, or one of: 'kvstore',"+
-			" 'persistent_kvstore',"+
-			" 'counter',"+
-			" 'counter_serial' or 'noop' for local testing.")
+			" 'persistent_kvstore',")
 	cmd.Flags().String("abci", config.ABCI, "specify abci transport (socket | grpc)")
 
 	// rpc flags
