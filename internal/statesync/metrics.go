bc5bd0460340d4cba887f6613412b1062da01b08
jayt106 <jaytseng106@gmail.com>
2021-07-30 14:13:07 -0400

    add statesync metrics struct

---
+++ internal/statesync/metrics.go
@@ -0,0 +1,107 @@
+package statesync
+
+import (
+	"github.com/go-kit/kit/metrics"
+	"github.com/go-kit/kit/metrics/discard"
+	"github.com/go-kit/kit/metrics/prometheus"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+)
+
+const (
+	// MetricsSubsystem is a subsystem shared by all metrics exposed by this package.
+	MetricsSubsystem = "statesync"
+)
+
+// Metrics contains metrics exposed by this package.
+type Metrics struct {
+	TotalSnapshots         metrics.Counter
+	ChunkProcess           metrics.Gauge
+	SnapshotHeight         metrics.Counter
+	SnapshotChunk          metrics.Gauge
+	SnapshotChunkTotal     metrics.Gauge
+	StateSyncTime          metrics.Gauge
+	StateSyncTimeRemaining metrics.Gauge
+	BackFill               metrics.Gauge
+	BackFillTotal          metrics.Gauge
+}
+
+// PrometheusMetrics returns Metrics build using Prometheus client library.
+// Optionally, labels can be provided along with their values ("foo",
+// "fooValue").
+func PrometheusMetrics(namespace string, labelsAndValues ...string) *Metrics {
+	labels := []string{}
+	for i := 0; i < len(labelsAndValues); i += 2 {
+		labels = append(labels, labelsAndValues[i])
+	}
+	return &Metrics{
+		TotalSnapshots: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: MetricsSubsystem,
+			Name:      "statesync_total_snapshots",
+			Help:      "The total number of snapshots discovered.",
+		}, labels).With(labelsAndValues...),
+		ChunkProcess: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: MetricsSubsystem,
+			Name:      "statesync_chunk_process",
+			Help:      " The average processing time per chunk.",
+		}, labels).With(labelsAndValues...),
+		SnapshotHeight: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: MetricsSubsystem,
+			Name:      "statesync_snapshot_height",
+			Help:      "The height of the current snapshot being processed.",
+		}, labels).With(labelsAndValues...),
+		SnapshotChunk: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: MetricsSubsystem,
+			Name:      "statesync_snapshot_chunk",
+			Help:      "The current chunk being processed.",
+		}, labels).With(labelsAndValues...),
+		SnapshotChunkTotal: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: MetricsSubsystem,
+			Name:      "statesync_snapshot_chunks_total",
+			Help:      "The total number of chunks in the current snapshot.",
+		}, labels).With(labelsAndValues...),
+		StateSyncTime: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: MetricsSubsystem,
+			Name:      "statesync_synctime",
+			Help:      "The current sync time from starting.",
+		}, labels).With(labelsAndValues...),
+		StateSyncTimeRemaining: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: MetricsSubsystem,
+			Name:      "statesync_synctime_remaining",
+			Help:      " The estimation time to finish the statesync.",
+		}, labels).With(labelsAndValues...),
+		BackFill: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: MetricsSubsystem,
+			Name:      "statesync_backfill",
+			Help:      "The current blocks has been back-filled.",
+		}, labels).With(labelsAndValues...),
+		BackFillTotal: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: MetricsSubsystem,
+			Name:      "statesync_backfill_total",
+			Help:      "The total blocks need to be back-filled.",
+		}, labels).With(labelsAndValues...),
+	}
+}
+
+// NopMetrics returns no-op Metrics.
+func NopMetrics() *Metrics {
+	return &Metrics{
+		TotalSnapshots:         discard.NewCounter(),
+		ChunkProcess:           discard.NewGauge(),
+		SnapshotHeight:         discard.NewCounter(),
+		SnapshotChunk:          discard.NewGauge(),
+		SnapshotChunkTotal:     discard.NewGauge(),
+		StateSyncTime:          discard.NewGauge(),
+		StateSyncTimeRemaining: discard.NewGauge(),
+		BackFill:               discard.NewGauge(),
+		BackFillTotal:          discard.NewGauge(),
+	}
+}
