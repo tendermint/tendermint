03f3c49bb2d26c5f1bf51c97ebfc4ca860d43411
William Banfield <4561443+williambanfield@users.noreply.github.com>
2021-08-17 15:46:03 -0400

    Update cmd/tendermint/commands/inspect.go
    Co-authored-by: M. J. Fromberger <fromberger@interchain.io>

--- cmd/tendermint/commands/inspect.go
+++ cmd/tendermint/commands/inspect.go
@@ -22,10 +22,10 @@ var InspectCmd = &cobra.Command{
 	Short: "Run an inspect server for investigating Tendermint state.",
 	Long: `
 	inspect runs a subset of Tendermint's RPC endpoints that are useful for debugging
-	issues with Tendermint. When the tendermint consensus engine detects inconsistent
-	state, it will crash the tendermint process. Tendermint will not start up while in this
-	inconsistent state. 
+	issues with Tendermint.
 
+	When the Tendermint consensus engine detects inconsistent state, it will crash the
+	tendermint process. Tendermint will not start up while in this inconsistent state. 
 	The inspect command can be used to query the block and state store using Tendermint
 	RPC calls to debug issues of inconsistent state.
 	`,
