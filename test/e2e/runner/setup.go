e9d3a35120f2daee0c9528f0ee96b54e73c5ed2e
William Banfield <wbanfield@gmail.com>
2021-09-03 15:57:25 -0400

    update e2e tests

--- test/e2e/runner/setup.go
+++ test/e2e/runner/setup.go
@@ -297,7 +297,7 @@ func MakeConfig(node *e2e.Node) (*config.Config, error) {
 	}
 
 	if node.BlockSync == "" {
-		cfg.FastSyncMode = false
+		cfg.BlockSyncMode = false
 	} else {
 		cfg.BlockSync.Version = node.BlockSync
 	}
