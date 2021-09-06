03195459eac54b1c34d92c72bb82d9ac105e8f45
M. J. Fromberger <fromberger@interchain.io>
2021-09-06 09:03:43 -0700

    psql: add bridge types for the v0.34 indexer interfaces
    The BackportTxIndexer and BackportBlockIndexer types implement the
    corresponding v0.34 indexing interfaces delegated to a psql EventSink.  The
    implementations are trivial, but we need to delegate rather than surfacing
    another interface because the signatures overlap.

---
+++ state/indexer/sink/psql/backport.go
@@ -0,0 +1,73 @@
+package psql
+
+// This file adds code to the psql package that is needed for integration with
+// v0.34, but which is not part of the original implementation.
+
+import (
+	"context"
+	"errors"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/pubsub/query"
+	"github.com/tendermint/tendermint/state/txindex"
+	"github.com/tendermint/tendermint/types"
+)
+
+// TxIndexer returns a bridge from es to the Tendermint v0.34 transaction indexer.
+func (es *EventSink) TxIndexer() BackportTxIndexer {
+	return BackportTxIndexer{psql: es}
+}
+
+// BackportTxIndexer implements the txindex.TxIndexer interface by delegating
+// indexing operations to an underlying PostgreSQL event sink.
+type BackportTxIndexer struct{ psql *EventSink }
+
+// AddBatch indexes a batch of transactions in Postgres, as part of TxIndexer.
+func (b BackportTxIndexer) AddBatch(batch *txindex.Batch) error {
+	return b.psql.IndexTxEvents(batch.Ops)
+}
+
+// Index indexes a single transaction result in Postgres, as part of TxIndexer.
+func (b BackportTxIndexer) Index(txr *abci.TxResult) error {
+	return b.psql.IndexTxEvents([]*abci.TxResult{txr})
+}
+
+// Get is implemented to satisfy the TxIndexer interface, but is not supported
+// by the psql event sink and reports an error for all inputs.
+func (BackportTxIndexer) Get([]byte) (*abci.TxResult, error) {
+	return nil, errors.New("the TxIndexer.Get method is not supported")
+}
+
+// Search is implemented to satisfy the TxIndexer interface, but it is not
+// supported by the psql event sink and reports an error for all inputs.
+func (BackportTxIndexer) Search(context.Context, *query.Query) ([]*abci.TxResult, error) {
+	return nil, errors.New("the TxIndexer.Search method is not supported")
+}
+
+// BlockIndexer returns a bridge that implements the Tendermint v0.34 block
+// indexer interface, using the Postgres event sink as a backing store.
+func (es *EventSink) BlockIndexer() BackportBlockIndexer {
+	return BackportBlockIndexer{psql: es}
+}
+
+// BackportBlockIndexer implements the indexer.BlockIndexer interface by
+// delegating indexing operations to an underlying PostgreSQL event sink.
+type BackportBlockIndexer struct{ psql *EventSink }
+
+// Has is implemented to satisfy the BlockIndexer interface, but it is not
+// supported by the psql event sink and reports an error for all inputs.
+func (BackportBlockIndexer) Has(height int64) (bool, error) {
+	return false, errors.New("the BlockIndexer.Has method is not supported")
+}
+
+// Index indexes block begin and end events for the specified block.  It is
+// part of the BlockIndexer interface.
+func (b BackportBlockIndexer) Index(block types.EventDataNewBlockHeader) error {
+	return b.psql.IndexBlockEvents(block)
+}
+
+// Search is implemented to satisfy the BlockIndexer interface, but it is not
+// supported by the psql event sink and reports an error for all inputs.
+func (BackportBlockIndexer) Search(context.Context, *query.Query) ([]int64, error) {
+	return nil, errors.New("the BlockIndexer.Search method is not supported")
+}
