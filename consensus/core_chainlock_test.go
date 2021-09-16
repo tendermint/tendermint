5533a8d3a13a8aade5a7116dbfdcadabdc431bcf
Wisdom Ogwu <iammadab@gmail.com>
2021-09-16 14:35:37 +0100

    Diff

--- consensus/core_chainlock_test.go
+++ consensus/core_chainlock_test.go
@@ -143,6 +143,7 @@ func invalidProposeCoreChainLockFunc(t *testing.T, height int64, round int32, cs
 }
 
 func TestReactorInvalidBlockChainLock(t *testing.T) {
+	// Leads to race, explore
 	N := 4
 	css, cleanup := randConsensusNet(N, "consensus_chainlocks_test",
 		newMockTickerFunc(true), newCounterWithBackwardsCoreChainLocks)
