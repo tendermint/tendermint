60f5e52fb02b1dcb41e6604b33ae165c55e3663d
tycho garen <garen@tychoish.com>
2021-09-20 13:12:06 -0400

    fix import

--- internal/proxy/multi_app_conn_test.go
+++ internal/proxy/multi_app_conn_test.go
@@ -8,9 +8,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
-	"gotest.tools/assert"
 
 	abciclient "github.com/tendermint/tendermint/abci/client"
 	abcimocks "github.com/tendermint/tendermint/abci/client/mocks"
