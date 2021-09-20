d2bbe08e44af6d2f6b27e317555ec96f2eb77ae5
tycho garen <garen@tychoish.com>
2021-09-20 09:15:35 -0400

    crypto: suppress deprecation lint error

--- crypto/armor/armor.go
+++ crypto/armor/armor.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	"golang.org/x/crypto/openpgp/armor"
+	"golang.org/x/crypto/openpgp/armor" // nolint: statccheck
 )
 
 func EncodeArmor(blockType string, headers map[string]string, data []byte) string {
