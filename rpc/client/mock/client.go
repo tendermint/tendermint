2d7ee96fe73f65ee8e480fa100228b64a263720b
M. J. Fromberger <fromberger@interchain.io>
2021-09-21 16:18:12 -0700

    Update the manual mock client interface.

--- rpc/client/mock/client.go
+++ rpc/client/mock/client.go
@@ -19,7 +19,6 @@ import (
 	"reflect"
 
 	"github.com/tendermint/tendermint/libs/bytes"
-	"github.com/tendermint/tendermint/libs/service"
 	"github.com/tendermint/tendermint/rpc/client"
 	"github.com/tendermint/tendermint/rpc/core"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
@@ -29,15 +28,7 @@ import (
 
 // Client wraps arbitrary implementations of the various interfaces.
 type Client struct {
-	client.ABCIClient
-	client.SignClient
-	client.HistoryClient
-	client.StatusClient
-	client.EventsClient
-	client.EvidenceClient
-	client.MempoolClient
-	service.Service
-
+	client.Client
 	env *core.Environment
 }
 
