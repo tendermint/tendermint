45275362ddd7deca7f0a933036dcf0f6811e5515
tycho garen <garen@tychoish.com>
2021-09-27 14:08:05 -0400

    hybrid test cases

--- test/e2e/generator/generate_test.go
+++ test/e2e/generator/generate_test.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	e2e "github.com/tendermint/tendermint/test/e2e/pkg"
 )
@@ -12,7 +13,7 @@ import (
 func TestGenerator(t *testing.T) {
 	manifests, err := Generate(rand.New(rand.NewSource(randomSeed)), Options{P2P: MixedP2PMode})
 	require.NoError(t, err)
-	require.True(t, len(manifests) > 16, "insufficient combinations")
+	require.True(t, len(manifests) >= 64, "insufficient combinations")
 
 	// this just means that the numbers reported by the test
 	// failures map to the test cases that you'd see locally.
@@ -40,4 +41,30 @@ func TestGenerator(t *testing.T) {
 			require.True(t, numStateSyncs <= 2)
 		})
 	}
+
+	t.Run("Hybrid", func(t *testing.T) {
+		manifests, err := Generate(rand.New(rand.NewSource(randomSeed)), Options{P2P: HybridP2PMode})
+		require.NoError(t, err)
+		require.True(t, len(manifests) >= 16, "insufficient combinations: %d", len(manifests))
+
+		for idx, m := range manifests {
+			t.Run(fmt.Sprintf("Case%04d", idx), func(t *testing.T) {
+				require.True(t, len(m.Nodes) > 1)
+
+				var numLegacy, numNew int
+				for _, node := range m.Nodes {
+					if node.UseLegacyP2P {
+						numLegacy++
+					} else {
+						numNew++
+					}
+				}
+
+				assert.True(t, numLegacy >= 1, "not enough legacy nodes [%d/%d]",
+					numLegacy, len(m.Nodes))
+				assert.True(t, numNew >= 1, "not enough new nodes [%d/%d]",
+					numNew, len(m.Nodes))
+			})
+		}
+	})
 }
