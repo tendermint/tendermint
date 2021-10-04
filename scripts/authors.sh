b701bf84a4f21d242a61aeb00714d42ceb87e4b9
William Banfield <wbanfield@gmail.com>
2021-10-04 13:33:58 -0400

    scripts: fix authors script to take a tag

--- scripts/authors.sh
+++ scripts/authors.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 
-# Usage:
-#   `./authors.sh`
-#		Print a list of all authors who have committed to develop since master.
-#
-#   `./authors.sh <email address>`
-#		Lookup the email address on Github and print the associated username
+tag=$1
 
-author=$1
 
-if [[ "$author" == "" ]]; then
-	git log master..develop | grep Author | sort | uniq
+if [[ ! -z "$tag" ]]; then
+	git log master..$tag | grep Author | sort | uniq
 else
-	curl -s "https://api.github.com/search/users?q=$author+in%3Aemail&type=Users&utf8=%E2%9C%93" | jq .items[0].login
+cat << EOF
+Usage:
+	./authors.sh <tag>
+		Print a list of all authors who have committed to develop since the supplied tagged version.
+EOF
 fi
+
