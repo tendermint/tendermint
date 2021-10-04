7dbb1fac855b95baf5066083c59dcf0064a19b8b
William Banfield <wbanfield@gmail.com>
2021-10-04 13:38:04 -0400

    fix language

--- scripts/authors.sh
+++ scripts/authors.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 
-tag=$1
+ref=$1
 
-
-if [[ ! -z "$tag" ]]; then
-	git log master..$tag | grep Author | sort | uniq
+if [[ ! -z "$ref" ]]; then
+	git log master..$ref | grep Author | sort | uniq
 else
 cat << EOF
 Usage:
-	./authors.sh <tag>
-		Print a list of all authors who have committed to develop since the supplied tagged version.
+	./authors.sh <ref>
+		Print a list of all authors who have committed to develop since the supplied commit ref. 
 EOF
 fi
 
