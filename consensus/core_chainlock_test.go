8a78b7a8b9f65bec159b9d1ec516535eec9fe04c
Wisdom Ogwu <iammadab@gmail.com>
2021-10-18 13:51:00 +0100

    fix: safe state access in TestValidProposalChainLocks

--- consensus/core_chainlock_test.go
+++ consensus/core_chainlock_test.go
@@ -48,8 +48,9 @@ func TestValidProposalChainLocks(t *testing.T) {
 			msg := <-blocksSubs[j].Out()
 			block := msg.Data().(types.EventDataNewBlock).Block
 			// this is true just because of this test where each new height has a new chain lock that is incremented by 1
+			state := css[0].GetState()
 			assert.EqualValues(t, i+1, block.Header.CoreChainLockedHeight)                  //nolint:scopelint
-			assert.EqualValues(t, css[0].state.InitialHeight+int64(i), block.Header.Height) //nolint:scopelint
+			assert.EqualValues(t, state.InitialHeight+int64(i), block.Header.Height) //nolint:scopelint
 		}, css)
 	}
 }
