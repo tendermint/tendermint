1f47c95b88dce91bc77c9de98220897c43bc6b04
tycho garen <garen@tychoish.com>
2021-10-26 07:01:23 -0400

    fix lint

--- cmd/tendermint/commands/run_node.go
+++ cmd/tendermint/commands/run_node.go
@@ -35,7 +35,6 @@ func AddNodeFlags(cmd *cobra.Command) {
 		"socket address to listen on for connections from external priv-validator process")
 
 	// node flags
-	cmd.Flags().Bool("blocksync.enable", config.BlockSync.Enable, "enable fast blockchain syncing")
 
 	// TODO (https://github.com/tendermint/tendermint/issues/6908): remove this check after the v0.35 release cycle
 	// This check was added to give users an upgrade prompt to use the new flag for syncing.
