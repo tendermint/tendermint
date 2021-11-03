4066c20901ec826f04f6e76c6ecf4db085b763a8
shotonoff <shotonoff@gmail.com>
2021-11-03 16:16:37 +0100

    feat: manual backport the upstream commit b69ac23fd20bdc00dea00c7c8a69fa66f2e675a9

--- light/provider/mock/deadmock.go
+++ light/provider/mock/deadmock.go
@@ -2,13 +2,10 @@ package mock
 
 import (
 	"context"
-	"errors"
 
 	"github.com/tendermint/tendermint/light/provider"
 	"github.com/tendermint/tendermint/types"
 )
-
-var errNoResp = errors.New("no response from provider")
 
 type deadMock struct {
 	chainID string
@@ -24,9 +21,9 @@ func (p *deadMock) ChainID() string { return p.chainID }
 func (p *deadMock) String() string { return "deadMock" }
 
 func (p *deadMock) LightBlock(_ context.Context, height int64) (*types.LightBlock, error) {
-	return nil, errNoResp
+	return nil, provider.ErrNoResponse
 }
 
 func (p *deadMock) ReportEvidence(_ context.Context, ev types.Evidence) error {
-	return errNoResp
+	return provider.ErrNoResponse
 }
