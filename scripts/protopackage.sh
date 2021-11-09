e0d992dca953f539175a71fd9ab18b7874e274e9
William Banfield <wbanfield@gmail.com>
2021-11-09 18:07:20 -0500

    udpate comment

--- scripts/protopackage.sh
+++ scripts/protopackage.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/sh
 set -eo pipefail
 
-# This script appends the "option go_package" proto option to the file located at $FNAME
-
+# This script appends the "option go_package" proto option to the file located at $FNAME.
+# This option is not included in the proto files to allow the files to more easily
+# be hosted in github.com/tendermint/spec and shared between other repos.
+# This option specifies what the package will be named when imported by other packages.
 
 FNAME=$1
 MODNAME=$(echo $2| sed 's/\//\\\//g')
