490a2d6a976b9a87250596c6add5c72aad41db9a
M. J. Fromberger <fromberger@interchain.io>
2021-11-24 15:32:03 -0800

    bench: add matching benchmarks
    Results:
     BenchmarkParsePEG-12     24410    48992  ns/op  526828  B/op  27  alloc/op
     BenchmarkParseCustom-12  566208   2150   ns/op  4616    B/op  17  alloc/op
     BenchmarkMatchPEG-12     396376   3082   ns/op  1097    B/op  22  alloc/op
     BenchmarkMatchCustom-12  4125183  287.4  ns/op  64      B/op  3   alloc/op

--- libs/pubsub/query/bench_test.go
+++ libs/pubsub/query/bench_test.go
@@ -1,18 +1,41 @@
 package query_test
 
 import (
-	"strings"
 	"testing"
 
+	"github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/pubsub/query"
 	oldquery "github.com/tendermint/tendermint/libs/pubsub/query/oldquery"
-	"github.com/tendermint/tendermint/libs/pubsub/query/syntax"
 )
 
-func BenchmarkParsePEG(b *testing.B) {
-	const input = `tm.events.type='NewBlock' AND abci.account.name='Igor'`
+const testQuery = `tm.events.type='NewBlock' AND abci.account.name='Igor'`
 
+var testEvents = []types.Event{
+	{
+		Type: "tm.events",
+		Attributes: []types.EventAttribute{{
+			Key:   "index",
+			Value: "25",
+		}, {
+			Key:   "type",
+			Value: "NewBlock",
+		}},
+	},
+	{
+		Type: "abci.account",
+		Attributes: []types.EventAttribute{{
+			Key:   "name",
+			Value: "Anya",
+		}, {
+			Key:   "name",
+			Value: "Igor",
+		}},
+	},
+}
+
+func BenchmarkParsePEG(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, err := oldquery.New(input)
+		_, err := oldquery.New(testQuery)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -20,12 +43,42 @@ func BenchmarkParsePEG(b *testing.B) {
 }
 
 func BenchmarkParseCustom(b *testing.B) {
-	const input = `tm.events.type='NewBlock' AND abci.account.name='Igor'`
-
 	for i := 0; i < b.N; i++ {
-		_, err := syntax.NewParser(strings.NewReader(input)).Parse()
+		_, err := query.New(testQuery)
 		if err != nil {
 			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkMatchPEG(b *testing.B) {
+	q, err := oldquery.New(testQuery)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ok, err := q.Matches(testEvents)
+		if err != nil {
+			b.Fatal(err)
+		} else if !ok {
+			b.Error("no match")
+		}
+	}
+}
+
+func BenchmarkMatchCustom(b *testing.B) {
+	q, err := query.New(testQuery)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ok, err := q.Matches(testEvents)
+		if err != nil {
+			b.Fatal(err)
+		} else if !ok {
+			b.Error("no match")
 		}
 	}
 }
