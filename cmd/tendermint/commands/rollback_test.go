93bed792e280514095c596b7b042c151d1811b11
Callum <cmwaters19@gmail.com>
2021-11-30 23:21:31 +0100

    use subtests

--- cmd/tendermint/commands/rollback_test.go
+++ cmd/tendermint/commands/rollback_test.go
@@ -14,50 +14,58 @@ import (
 )
 
 func TestRollbackIntegration(t *testing.T) {
+	var height int64
 	dir := t.TempDir()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	cfg, err := rpctest.CreateConfig(t.Name())
 	require.NoError(t, err)
 	cfg.BaseConfig.DBBackend = "goleveldb"
-
-	subctx, subcancel := context.WithCancel(ctx)
-	defer subcancel()
 	app, err := e2e.NewApplication(e2e.DefaultConfig(dir))
-	require.NoError(t, err)
-	node, _, err := rpctest.StartTendermint(subctx, cfg, app, rpctest.SuppressStdout)
-	require.NoError(t, err)
 
-	time.Sleep(3 * time.Second)
-	subcancel()
-	node.Wait()
-	require.False(t, node.IsRunning())
+	t.Run("First run", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		require.NoError(t, err)
+		node, _, err := rpctest.StartTendermint(ctx, cfg, app, rpctest.SuppressStdout)
+		require.NoError(t, err)
+	
+		time.Sleep(3 * time.Second)
+		cancel()
+		node.Wait()
+		require.False(t, node.IsRunning())
+	})
 
-	require.NoError(t, app.Rollback())
-	height, _, err := commands.RollbackState(cfg)
-	require.NoError(t, err)
+	t.Run("Rollback", func(t *testing.T) {
+		require.NoError(t, app.Rollback())
+		height, _, err = commands.RollbackState(cfg)
+		require.NoError(t, err)
 
-	subctx2, subcancel2 := context.WithTimeout(ctx, 10*time.Second)
-	defer subcancel2()
-	node2, _, err2 := rpctest.StartTendermint(subctx2, cfg, app, rpctest.SuppressStdout)
-	require.NoError(t, err2)
+	})
 
-	client, err := local.New(node2.(local.NodeService))
-	require.NoError(t, err)
-
-	ticker := time.NewTicker(200 * time.Millisecond)
-	for {
-		select {
-		case <-subctx2.Done():
-			t.Fatalf("failed to make progress after 20 seconds. Min height: %d", height)
-		case <-ticker.C:
-			status, err := client.Status(subctx2)
-			require.NoError(t, err)
-
-			if status.SyncInfo.LatestBlockHeight > height {
-				return
+	t.Run("R0estart", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		defer cancel()
+		node2, _, err2 := rpctest.StartTendermint(ctx, cfg, app, rpctest.SuppressStdout)
+		require.NoError(t, err2)
+	
+		client, err := local.New(node2.(local.NodeService))
+		require.NoError(t, err)
+	
+		ticker := time.NewTicker(200 * time.Millisecond)
+		for {
+			select {
+			case <-ctx.Done():
+				t.Fatalf("failed to make progress after 20 seconds. Min height: %d", height)
+			case <-ticker.C:
+				status, err := client.Status(ctx)
+				require.NoError(t, err)
+	
+				if status.SyncInfo.LatestBlockHeight > height {
+					return
+				}
 			}
 		}
-	}
+	})
 
 }
