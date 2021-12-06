3276f865878abaaf13b24c7df60384f670e292e5
William Banfield <wbanfield@gmail.com>
2021-12-06 11:41:44 -0500

    statesync and blocksync: remove tests for serialized bytes

--- proto/tendermint/statesync/message_test.go
+++ proto/tendermint/statesync/message_test.go
@@ -1,7 +1,6 @@
 package statesync_test
 
 import (
-	"encoding/hex"
 	"testing"
 
 	"github.com/gogo/protobuf/proto"
@@ -9,7 +8,6 @@ import (
 
 	ssproto "github.com/tendermint/tendermint/proto/tendermint/statesync"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-	"github.com/tendermint/tendermint/types"
 )
 
 func TestValidateMsg(t *testing.T) {
@@ -118,89 +116,5 @@ func TestValidateMsg(t *testing.T) {
 				require.Error(t, msg.Validate())
 			}
 		})
-	}
-}
-
-func TestStateSyncVectors(t *testing.T) {
-	testCases := []struct {
-		testName string
-		msg      proto.Message
-		expBytes string
-	}{
-		{
-			"SnapshotsRequest",
-			&ssproto.SnapshotsRequest{},
-			"0a00",
-		},
-		{
-			"SnapshotsResponse",
-			&ssproto.SnapshotsResponse{
-				Height:   1,
-				Format:   2,
-				Chunks:   3,
-				Hash:     []byte("chuck hash"),
-				Metadata: []byte("snapshot metadata"),
-			},
-			"1225080110021803220a636875636b20686173682a11736e617073686f74206d65746164617461",
-		},
-		{
-			"ChunkRequest",
-			&ssproto.ChunkRequest{
-				Height: 1,
-				Format: 2,
-				Index:  3,
-			},
-			"1a06080110021803",
-		},
-		{
-			"ChunkResponse",
-			&ssproto.ChunkResponse{
-				Height: 1,
-				Format: 2,
-				Index:  3,
-				Chunk:  []byte("it's a chunk"),
-			},
-			"2214080110021803220c697427732061206368756e6b",
-		},
-		{
-			"LightBlockRequest",
-			&ssproto.LightBlockRequest{
-				Height: 100,
-			},
-			"2a020864",
-		},
-		{
-			"LightBlockResponse",
-			&ssproto.LightBlockResponse{
-				LightBlock: nil,
-			},
-			"3200",
-		},
-		{
-			"ParamsRequest",
-			&ssproto.ParamsRequest{
-				Height: 9001,
-			},
-			"3a0308a946",
-		},
-		{
-			"ParamsResponse",
-			&ssproto.ParamsResponse{
-				Height:          9001,
-				ConsensusParams: types.DefaultConsensusParams().ToProto(),
-			},
-			"423408a946122f0a10088080c00a10ffffffffffffffffff01120e08a08d0612040880c60a188080401a090a07656432353531392200",
-		},
-	}
-
-	for _, tc := range testCases {
-		tc := tc
-
-		msg := new(ssproto.Message)
-		require.NoError(t, msg.Wrap(tc.msg))
-
-		bz, err := msg.Marshal()
-		require.NoError(t, err)
-		require.Equal(t, tc.expBytes, hex.EncodeToString(bz), tc.testName)
 	}
 }
