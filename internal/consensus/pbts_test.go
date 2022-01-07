693bae7a2985e67d7692d2e360be89c7c7489d19
Anca Zamfir <zamfiranca@gmail.com>
2022-01-07 12:52:57 +0100

    Review comments from Daniel

--- internal/consensus/pbts_test.go
+++ internal/consensus/pbts_test.go
@@ -460,7 +460,7 @@ func TestTooFarInThePastProposal(t *testing.T) {
 		timeoutPropose:             50 * time.Millisecond,
 		genesisTime:                initialTime,
 		height2ProposedBlockTime:   initialTime.Add(10 * time.Millisecond),
-		height2ProposalDeliverTime: initialTime.Add(100 * time.Millisecond),
+		height2ProposalDeliverTime: initialTime.Add(21 * time.Millisecond),
 	}
 
 	pbtsTest := newPBTSTestHarness(ctx, t, cfg)
