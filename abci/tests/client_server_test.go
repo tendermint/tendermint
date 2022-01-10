692701a551d1e8f4e9a23e6362e7728ca22d72e2
Sam Kleinman <garen@tychoish.com>
2022-01-10 16:26:40 -0500

    abci: socket server shutdown response handler (#7547)

--- abci/tests/client_server_test.go
+++ abci/tests/client_server_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/fortytw2/leaktest"
 	"github.com/stretchr/testify/assert"
 
 	abciclientent "github.com/tendermint/tendermint/abci/client"
@@ -13,6 +14,8 @@ import (
 )
 
 func TestClientServerNoAddrPrefix(t *testing.T) {
+	t.Cleanup(leaktest.Check(t))
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -27,9 +30,11 @@ func TestClientServerNoAddrPrefix(t *testing.T) {
 	assert.NoError(t, err, "expected no error on NewServer")
 	err = server.Start(ctx)
 	assert.NoError(t, err, "expected no error on server.Start")
+	t.Cleanup(server.Wait)
 
 	client, err := abciclientent.NewClient(logger, addr, transport, true)
 	assert.NoError(t, err, "expected no error on NewClient")
 	err = client.Start(ctx)
 	assert.NoError(t, err, "expected no error on client.Start")
+	t.Cleanup(client.Wait)
 }
