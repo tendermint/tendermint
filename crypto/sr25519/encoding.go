dbe2146d0a01e5986a4fd27e8b2c7461eacaa883
M. J. Fromberger <fromberger@interchain.io>
2022-01-14 18:14:09 -0800

    rpc: simplify the encoding of interface-typed arguments in JSON (#7600)
    Add package jsontypes that implements a subset of the custom libs/json 
    package. Specifically it handles encoding and decoding of interface types
    wrapped in "tagged" JSON objects. It omits the deep reflection on arbitrary
    types, preserving only the handling of type tags wrapper encoding.
    
    - Register interface types (Evidence, PubKey, PrivKey) for tagged encoding.
    - Update the existing implementations to satisfy the type.
    - Register those types with the jsontypes registry.
    - Add string tags to 64-bit integer fields where needed.
    - Add marshalers to structs that export interface-typed fields.

--- crypto/sr25519/encoding.go
+++ crypto/sr25519/encoding.go
@@ -1,6 +1,9 @@
 package sr25519
 
-import tmjson "github.com/tendermint/tendermint/libs/json"
+import (
+	"github.com/tendermint/tendermint/internal/jsontypes"
+	tmjson "github.com/tendermint/tendermint/libs/json"
+)
 
 const (
 	PrivKeyName = "tendermint/PrivKeySr25519"
@@ -10,4 +13,7 @@ const (
 func init() {
 	tmjson.RegisterType(PubKey{}, PubKeyName)
 	tmjson.RegisterType(PrivKey{}, PrivKeyName)
+
+	jsontypes.MustRegister(PubKey{})
+	jsontypes.MustRegister(PrivKey{})
 }
