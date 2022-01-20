f9eee5c8a22e3e2eb05a93c44cb5dc935f085c16
Anca Zamfir <zamfiranca@gmail.com>
2022-01-20 14:49:10 +0100

    Merge upstream repo into the fork

--- p2p/interceptcontroller/lib.go
+++
@@ -1,8 +0,0 @@
-package interceptcontroller
-
-type Controller interface {
-	// TODO: Need to figure out what goes into the channel
-	SendChan() chan interface{}
-	ReceiveChan() chan interface{}
-	Run()
-}
--- p2p/interceptcontroller/master.go
+++
@@ -1,4 +0,0 @@
-package interceptcontroller
-
-// TODO: Create a controller that sends messages to a central node over http
-// or, can use the file system that PCT provides, but then testing would mean all the tendermint nodes are running on the same machine
