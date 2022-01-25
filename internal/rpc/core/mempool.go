6a02714814478e631d9528c7596eb5ff4ee83a77
Sam Kleinman <garen@tychoish.com>
2022-01-25 13:35:03 -0500

    rpc: check error code for broadcast_tx_commit (#7683)

--- internal/rpc/core/mempool.go
+++ internal/rpc/core/mempool.go
@@ -73,6 +73,12 @@ func (env *Environment) BroadcastTxCommit(ctx context.Context, tx types.Tx) (*co
 	}
 
 	r := (<-resCh).GetCheckTx()
+	if r.Code != abci.CodeTypeOK {
+		return &coretypes.ResultBroadcastTxCommit{
+			CheckTx: *r,
+			Hash:    tx.Hash(),
+		}, fmt.Errorf("transaction encountered error (%s)", r.MempoolError)
+	}
 
 	if !indexer.KVSinkEnabled(env.EventSinks) {
 		return &coretypes.ResultBroadcastTxCommit{
