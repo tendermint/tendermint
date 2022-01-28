0d30a930f71f0047b26ca15efeb228d13c92ae3e
Sam Kleinman <garen@tychoish.com>
2022-01-28 14:30:55 -0500

    Update cmd/tendermint/commands/testnet.go
    Co-authored-by: William Banfield <4561443+williambanfield@users.noreply.github.com>

--- cmd/tendermint/commands/testnet.go
+++ cmd/tendermint/commands/testnet.go
@@ -277,7 +277,7 @@ type testnetPeerArgs struct {
 	randomMonikers   bool
 }
 
-func (args *testnetPeerArgs) hostnameOrIP(i int) (string, error) {
+func hostnameOrIP(i int, hostnames []string, startingIpAddr string) (string, error) {
 	if len(args.hostnames) > 0 && i < len(args.hostnames) {
 		return args.hostnames[i], nil
 	}
