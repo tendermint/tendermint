07fd2e84365cd3438c31e910b5bf7eb2f169a1f5
jay tseng <jay.tseng@crypto.com>
2022-02-10 14:48:14 -0500

    fix benchmark CheckTx for hitting GetEvictableTxs call

--- internal/mempool/mempool_bench_test.go
+++ internal/mempool/mempool_bench_test.go
@@ -15,7 +15,11 @@ func BenchmarkTxMempool_CheckTx(b *testing.B) {
 	defer cancel()
 
 	txmp := setup(ctx, b, 10000)
+	txmp.height = 100
+	txmp.config.TTLNumBlocks = 10
+	txmp.config.Size = 5000
 	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	peerID := 1
 
 	b.ResetTimer()
 
@@ -26,9 +30,11 @@ func BenchmarkTxMempool_CheckTx(b *testing.B) {
 		require.NoError(b, err)
 
 		priority := int64(rng.Intn(9999-1000) + 1000)
-		tx := []byte(fmt.Sprintf("%X=%d", prefix, priority))
+		tx := []byte(fmt.Sprintf("sender-%d-%d=%X=%d", n, peerID, prefix, priority))
+		txInfo := TxInfo{SenderID: uint16(peerID)}
+
 		b.StartTimer()
 
-		require.NoError(b, txmp.CheckTx(ctx, tx, nil, TxInfo{}))
+		require.NoError(b, txmp.CheckTx(ctx, tx, nil, txInfo))
 	}
 }
