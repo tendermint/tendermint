012c7aa369af58ef749d357b4fb7852c445502a9
HuangYi <huang@crypto.com>
2022-02-18 21:36:04 +0800

    apply review suggestions

--- internal/state/rollback.go
+++ internal/state/rollback.go
@@ -41,6 +41,7 @@ func Rollback(bs BlockStore, ss Store) (int64, []byte, error) {
 	if rollbackBlock == nil {
 		return -1, nil, fmt.Errorf("block at height %d not found", rollbackHeight)
 	}
+	// we also need to retrieve the latest block because the app hash and last results hash is only agreed upon in the following block
 	latestBlock := bs.LoadBlockMeta(invalidState.LastBlockHeight)
 	if latestBlock == nil {
 		return -1, nil, fmt.Errorf("block at height %d not found", invalidState.LastBlockHeight)
@@ -93,7 +94,7 @@ func Rollback(bs BlockStore, ss Store) (int64, []byte, error) {
 		ConsensusParams:                  previousParams,
 		LastHeightConsensusParamsChanged: paramsChangeHeight,
 
-		LastResultsHash: rollbackBlock.Header.LastResultsHash,
+		LastResultsHash: latestBlock.Header.LastResultsHash,
 		AppHash:         latestBlock.Header.AppHash,
 	}
 
