b2305b35d08cbcb9e7fbf4894bbc5db925b32b22
M. J. Fromberger <fromberger@interchain.io>
2022-02-21 06:13:19 -0800

    Resolve merge conflict.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,10 +23,6 @@ module.exports = {
     },
     versions: [
       {
-        "label": "v0.32",
-        "key": "v0.32"
-      },
-      {
         "label": "v0.33",
         "key": "v0.33"
       },
