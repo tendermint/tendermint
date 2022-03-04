c5c088b1f87bf7fdb0fcb9d01b30da1e1264d0fa
tycho garen <garen@tychoish.com>
2022-03-04 15:41:58 -0500

    reorg files

--- internal/proxy/client_test.go
+++ internal/proxy/client_test.go
@@ -2,18 +2,26 @@ package proxy
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	abciclient "github.com/tendermint/tendermint/abci/client"
+	abcimocks "github.com/tendermint/tendermint/abci/client/mocks"
 	"github.com/tendermint/tendermint/abci/example/kvstore"
 	"github.com/tendermint/tendermint/abci/server"
 	"github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/log"
 	tmrand "github.com/tendermint/tendermint/libs/rand"
+	"gotest.tools/assert"
 )
 
 //----------------------------------------
@@ -160,5 +168,74 @@ func TestInfo(t *testing.T) {
 
 	if resInfo.Data != "{\"size\":0}" {
 		t.Error("Expected ResponseInfo with one element '{\"size\":0}' but got something else")
+	}
+}
+
+type noopStoppableClientImpl struct {
+	abciclient.Client
+	count int
+}
+
+func (c *noopStoppableClientImpl) Stop() { c.count++ }
+
+func TestAppConns_Start_Stop(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	clientMock := &abcimocks.Client{}
+	clientMock.On("Start", mock.Anything).Return(nil)
+	clientMock.On("Error").Return(nil)
+	clientMock.On("IsRunning").Return(true)
+	clientMock.On("Wait").Return(nil).Times(1)
+	cl := &noopStoppableClientImpl{Client: clientMock}
+
+	appConns := New(cl, log.TestingLogger(), NopMetrics())
+
+	err := appConns.Start(ctx)
+	require.NoError(t, err)
+
+	time.Sleep(200 * time.Millisecond)
+
+	cancel()
+	appConns.Wait()
+
+	clientMock.AssertExpectations(t)
+	assert.Equal(t, 1, cl.count)
+}
+
+// Upon failure, we call tmos.Kill
+func TestAppConns_Failure(t *testing.T) {
+	ok := make(chan struct{})
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGTERM)
+	go func() {
+		for range c {
+			close(ok)
+			return
+		}
+	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	clientMock := &abcimocks.Client{}
+	clientMock.On("SetLogger", mock.Anything).Return()
+	clientMock.On("Start", mock.Anything).Return(nil)
+	clientMock.On("IsRunning").Return(true)
+	clientMock.On("Wait").Return(nil)
+	clientMock.On("Error").Return(errors.New("EOF"))
+	cl := &noopStoppableClientImpl{Client: clientMock}
+
+	appConns := New(cl, log.TestingLogger(), NopMetrics())
+
+	err := appConns.Start(ctx)
+	require.NoError(t, err)
+	t.Cleanup(func() { cancel(); appConns.Wait() })
+
+	select {
+	case <-ok:
+		t.Log("SIGTERM successfully received")
+	case <-time.After(5 * time.Second):
+		t.Fatal("expected process to receive SIGTERM signal")
 	}
 }
