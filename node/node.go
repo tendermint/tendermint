2740cdfc05bf1e2935a62ca9141ef209709a930a
tycho garen <garen@tychoish.com>
2022-03-08 12:10:25 -0500

    fix

--- node/node.go
+++ node/node.go
@@ -388,8 +388,7 @@ func makeNode(
 			mpReactor,
 			evReactor,
 		},
-		// seperate so we can start
-		// before statesync
+		// pex is separate so we can start before statesync
 		pexService: pexReactor,
 
 		stateStore:       stateStore,
