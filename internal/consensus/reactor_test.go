cb5d81f5885b9c7305c74f4390ff6ca9e5959581
tycho garen <garen@tychoish.com>
2022-03-11 12:53:22 -0500

    bigger buffers

--- internal/consensus/reactor_test.go
+++ internal/consensus/reactor_test.go
@@ -75,10 +75,10 @@ func setup(
 		blocksyncSubs: make(map[types.NodeID]eventbus.Subscription, numNodes),
 	}
 
-	rts.stateChannels = rts.network.MakeChannelsNoCleanup(ctx, t, chDesc(StateChannel, size))
-	rts.dataChannels = rts.network.MakeChannelsNoCleanup(ctx, t, chDesc(DataChannel, size))
-	rts.voteChannels = rts.network.MakeChannelsNoCleanup(ctx, t, chDesc(VoteChannel, size))
-	rts.voteSetBitsChannels = rts.network.MakeChannelsNoCleanup(ctx, t, chDesc(VoteSetBitsChannel, size))
+	rts.stateChannels = rts.network.MakeChannelsNoCleanup(ctx, t, chDesc(StateChannel, size*2))
+	rts.dataChannels = rts.network.MakeChannelsNoCleanup(ctx, t, chDesc(DataChannel, size*2))
+	rts.voteChannels = rts.network.MakeChannelsNoCleanup(ctx, t, chDesc(VoteChannel, size*2))
+	rts.voteSetBitsChannels = rts.network.MakeChannelsNoCleanup(ctx, t, chDesc(VoteSetBitsChannel, size*2))
 
 	ctx, cancel := context.WithCancel(ctx)
 	t.Cleanup(cancel)
@@ -118,14 +118,14 @@ func setup(
 		blocksSub, err := state.eventBus.SubscribeWithArgs(ctx, tmpubsub.SubscribeArgs{
 			ClientID: testSubscriber,
 			Query:    types.EventQueryNewBlock,
-			Limit:    4096,
+			Limit:    65536,
 		})
 		require.NoError(t, err)
 
 		fsSub, err := state.eventBus.SubscribeWithArgs(ctx, tmpubsub.SubscribeArgs{
 			ClientID: testSubscriber,
 			Query:    types.EventQueryBlockSyncStatus,
-			Limit:    1024,
+			Limit:    65536,
 		})
 		require.NoError(t, err)
 
