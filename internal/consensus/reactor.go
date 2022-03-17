9fef5a3aa0395509066cf325da3e35cafcb52591
William Banfield <wbanfield@gmail.com>
2022-03-17 14:46:05 -0400

    lint++

--- internal/consensus/reactor.go
+++ internal/consensus/reactor.go
@@ -426,10 +426,10 @@ func (r *Reactor) updateRoundStateRoutine() {
 	}
 }
 
-func (conR *Reactor) getRoundState() *cstypes.RoundState {
-	conR.mtx.RLock()
-	defer conR.mtx.RUnlock()
-	return conR.rs
+func (r *Reactor) getRoundState() *cstypes.RoundState {
+	r.mtx.RLock()
+	defer r.mtx.RUnlock()
+	return r.rs
 }
 
 func (r *Reactor) gossipDataForCatchup(ctx context.Context, rs *cstypes.RoundState, prs *cstypes.PeerRoundState, ps *PeerState) {
