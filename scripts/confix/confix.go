7dbb403f40bf174e138a23b38c6d8e53396bb404
M. J. Fromberger <fromberger@interchain.io>
2022-04-08 11:14:55 -0700

    Reorganize code layout.

--- scripts/confix/confix.go
+++ scripts/confix/confix.go
@@ -40,87 +40,6 @@ Options:
 var (
 	configPath = flag.String("config", "", "Config file path (required)")
 	outPath    = flag.String("out", "", "Output file path (defaults to -input)")
-
-	plan = transform.Plan{
-		{
-			Desc: "Rename everything from snake_case to kebab-case",
-			T:    transform.SnakeToKebab(),
-		},
-		{
-			Desc:    "Rename [fastsync] to [blocksync]",
-			T:       transform.Rename(parser.Key{"fastsync"}, parser.Key{"blocksync"}),
-			ErrorOK: true,
-		},
-		{
-			Desc: "Move top-level fast_sync key under [blocksync]",
-			T: transform.MoveKey(
-				parser.Key{"fast-sync"},
-				parser.Key{"blocksync"},
-				parser.Key{"fast-sync"},
-			),
-			ErrorOK: true,
-		},
-		{
-			Desc: "Move priv-validator settings under [priv-validator]",
-			T: transform.Func(func(_ context.Context, doc *tomledit.Document) error {
-				const pvPrefix = "priv-validator-"
-
-				var found []*tomledit.Entry
-				doc.Scan(func(key parser.Key, e *tomledit.Entry) bool {
-					if e.IsSection() && !e.IsGlobal() {
-						return false // no more candidates
-					} else if len(key) == 1 && strings.HasPrefix(key[0], pvPrefix) {
-						found = append(found, e)
-					}
-					return true
-				})
-				if len(found) == 0 {
-					return nil // nothing to do
-				}
-
-				// Now that we know we have work to do, find the target table.
-				var sec *tomledit.Section
-				if dst := transform.FindTable(doc, "priv-validator"); dst == nil {
-					// If the table doesn't exist, create it. Old config files
-					// probably will not have it, so plug in the comment too.
-					sec = &tomledit.Section{
-						Heading: &parser.Heading{
-							Block: parser.Comments{
-								"#######################################################",
-								"###       Priv Validator Configuration              ###",
-								"#######################################################",
-							},
-							Name: parser.Key{"priv-validator"},
-						},
-					}
-					doc.Sections = append(doc.Sections, sec)
-				} else {
-					sec = dst.Section
-				}
-
-				for _, e := range found {
-					e.Remove()
-					e.Name = parser.Key{strings.TrimPrefix(e.Name[0], pvPrefix)}
-					sec.Items = append(sec.Items, e.KeyValue)
-				}
-				return nil
-			}),
-		},
-		{
-			Desc: `Set blocksync.version to "v0"`,
-			T: transform.Func(func(_ context.Context, doc *tomledit.Document) error {
-				v := doc.First("blocksync", "version")
-				if v == nil {
-					return nil // nothing to do
-				} else if !v.IsMapping() {
-					// This shouldn't happen, but is easier to debug than a panic.
-					return fmt.Errorf("blocksync.version is weird: %v", v)
-				}
-				v.Value.X = parser.MustValue(`"v0"`).X
-				return nil
-			}),
-		},
-	}
 )
 
 func main() {
@@ -152,6 +71,87 @@ func main() {
 	} else if err := out.Close(); err != nil {
 		log.Fatalf("Closing output: %v", err)
 	}
+}
+
+var plan = transform.Plan{
+	{
+		Desc: "Rename everything from snake_case to kebab-case",
+		T:    transform.SnakeToKebab(),
+	},
+	{
+		Desc:    "Rename [fastsync] to [blocksync]",
+		T:       transform.Rename(parser.Key{"fastsync"}, parser.Key{"blocksync"}),
+		ErrorOK: true,
+	},
+	{
+		Desc: "Move top-level fast_sync key under [blocksync]",
+		T: transform.MoveKey(
+			parser.Key{"fast-sync"},
+			parser.Key{"blocksync"},
+			parser.Key{"fast-sync"},
+		),
+		ErrorOK: true,
+	},
+	{
+		Desc: "Move priv-validator settings under [priv-validator]",
+		T: transform.Func(func(_ context.Context, doc *tomledit.Document) error {
+			const pvPrefix = "priv-validator-"
+
+			var found []*tomledit.Entry
+			doc.Scan(func(key parser.Key, e *tomledit.Entry) bool {
+				if e.IsSection() && !e.IsGlobal() {
+					return false // no more candidates
+				} else if len(key) == 1 && strings.HasPrefix(key[0], pvPrefix) {
+					found = append(found, e)
+				}
+				return true
+			})
+			if len(found) == 0 {
+				return nil // nothing to do
+			}
+
+			// Now that we know we have work to do, find the target table.
+			var sec *tomledit.Section
+			if dst := transform.FindTable(doc, "priv-validator"); dst == nil {
+				// If the table doesn't exist, create it. Old config files
+				// probably will not have it, so plug in the comment too.
+				sec = &tomledit.Section{
+					Heading: &parser.Heading{
+						Block: parser.Comments{
+							"#######################################################",
+							"###       Priv Validator Configuration              ###",
+							"#######################################################",
+						},
+						Name: parser.Key{"priv-validator"},
+					},
+				}
+				doc.Sections = append(doc.Sections, sec)
+			} else {
+				sec = dst.Section
+			}
+
+			for _, e := range found {
+				e.Remove()
+				e.Name = parser.Key{strings.TrimPrefix(e.Name[0], pvPrefix)}
+				sec.Items = append(sec.Items, e.KeyValue)
+			}
+			return nil
+		}),
+	},
+	{
+		Desc: `Set blocksync.version to "v0"`,
+		T: transform.Func(func(_ context.Context, doc *tomledit.Document) error {
+			v := doc.First("blocksync", "version")
+			if v == nil {
+				return nil // nothing to do
+			} else if !v.IsMapping() {
+				// This shouldn't happen, but is easier to debug than a panic.
+				return fmt.Errorf("blocksync.version is weird: %v", v)
+			}
+			v.Value.X = parser.MustValue(`"v0"`).X
+			return nil
+		}),
+	},
 }
 
 // ApplyFixes transforms doc and reports whether it succeeded.
