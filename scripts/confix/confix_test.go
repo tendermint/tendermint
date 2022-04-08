c1309a6b3579c649c005f416b59704c20890f98d
M. J. Fromberger <fromberger@interchain.io>
2022-04-08 12:09:18 -0700

    Add a non-config TOML file for testing.

--- scripts/confix/confix_test.go
+++ scripts/confix/confix_test.go
@@ -23,6 +23,7 @@ func TestGuessConfigVersion(t *testing.T) {
 	tests := []struct {
 		path, want string
 	}{
+		{"testdata/non-config.toml", ""},
 		{"testdata/v31-config.toml", ""},
 		{"testdata/v32-config.toml", "v0.32"},
 		{"testdata/v33-config.toml", "v0.33"},
