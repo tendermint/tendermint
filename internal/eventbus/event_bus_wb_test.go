5aeb3f2cf63505dfee87ddad9aa19bf59aec8222
Ethan Reesor <ethan.reesor@gmail.com>
2022-04-12 20:16:36 -0500

    Event bus bug

---
+++ internal/eventbus/event_bus_wb_test.go
@@ -0,0 +1,24 @@
+package eventbus
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tendermint/tendermint/internal/pubsub"
+)
+
+func TestEventBusNilSubscription(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	eventBus := new(EventBus)
+	eventBus.pubsub = new(pubsub.Server)
+
+	txsSub, err := eventBus.SubscribeWithArgs(ctx, pubsub.SubscribeArgs{})
+	assert.Error(t, err)
+	assert.True(t, txsSub == nil)
+
+	// This will panic
+	txsSub.ID()
+}
