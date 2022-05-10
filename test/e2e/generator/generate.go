6fffbf94028a1ae78289abbad1b602c251f6f652
Thane Thomson <connect@thanethomson.com>
2022-05-10 15:17:37 -0400

    Disable lint
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- test/e2e/generator/generate.go
+++ test/e2e/generator/generate.go
@@ -124,6 +124,7 @@ func generateTestnet(r *rand.Rand, opt map[string]interface{}) (e2e.Manifest, er
 		numValidators = 4
 	case "large":
 		// FIXME Networks are kept small since large ones use too much CPU.
+		//nolint:staticcheck
 		numSeeds = r.Intn(1)
 		numLightClients = r.Intn(2)
 		numValidators = 4 + r.Intn(4)
