235a585fa344621202222f2fccd20a266b6b5f58
William Banfield <wbanfield@gmail.com>
2022-05-12 11:23:05 -0400

    update consensus descriptions

--- internal/consensus/metrics.go
+++ internal/consensus/metrics.go
@@ -24,7 +24,7 @@ type Metrics struct {
 	// Height of the chain.
 	Height metrics.Gauge
 
-	// Last height signed by this validator.
+	// Last height signed by this validator if the node is a validator.
 	ValidatorLastSignedHeight metrics.Gauge `metrics_labels:"validator_address"`
 
 	// Number of rounds.
@@ -66,10 +66,10 @@ type Metrics struct {
 	// Whether or not a node is state syncing. 1 if yes, 0 if no.
 	StateSyncing metrics.Gauge
 
-	// Number of blockparts transmitted by peer.
+	// Number of block parts transmitted by each peer.
 	BlockParts metrics.Counter `metrics_labels:"peer_id"`
 
-	// Histogram of step duration.
+	// Histogram of durations for each step in the consensus protocol.
 	StepDuration metrics.Histogram `metrics_labels:"step" metrics_buckettype:"exprange" metrics_bucketsizes:"0.1, 100, 8"`
 	stepStart    time.Time
 
@@ -91,43 +91,49 @@ type Metrics struct {
 	// be above 2/3 of the total voting power of the network defines the endpoint
 	// the endpoint of the interval. Subtract the proposal timestamp from this endpoint
 	// to obtain the quorum delay.
+	//metrics:Interval in seconds between the proposal timestamp and the timestamp of the earliest prevote that achieved a quorum.
 	QuorumPrevoteDelay metrics.Gauge `metrics_labels:"proposer_address"`
 
 	// FullPrevoteDelay is the interval in seconds between the proposal
 	// timestamp and the timestamp of the latest prevote in a round where 100%
 	// of the voting power on the network issued prevotes.
+	//metrics:Interval in seconds between the proposal timestamp and the timestamp of the latest prevote in a round where all validators voted.
 	FullPrevoteDelay metrics.Gauge `metrics_labels:"proposer_address"`
 
 	// ProposalTimestampDifference is the difference between the timestamp in
 	// the proposal message and the local time of the validator at the time
 	// that the validator received the message.
+	//metrics:Difference between the timestamp in the proposal message and the local time of the validator at the time it received the message.
 	ProposalTimestampDifference metrics.Histogram `metrics_labels:"is_timely" metrics_bucketsizes:"-10, -.5, -.025, 0, .1, .5, 1, 1.5, 2, 10"`
 
 	// VoteExtensionReceiveCount is the number of vote extensions received by this
 	// node. The metric is annotated by the status of the vote extension from the
 	// application, either 'accepted' or 'rejected'.
+	//metrics:Number of vote extensions received labeled by application response status.
 	VoteExtensionReceiveCount metrics.Counter `metrics_labels:"status"`
 
 	// ProposalReceiveCount is the total number of proposals received by this node
 	// since process start.
 	// The metric is annotated by the status of the proposal from the application,
 	// either 'accepted' or 'rejected'.
+	//metrics:Total number of proposals received by the node since process start labeled by application response status.
 	ProposalReceiveCount metrics.Counter `metrics_labels:"status"`
 
 	// ProposalCreationCount is the total number of proposals created by this node
 	// since process start.
-	// The metric is annotated by the status of the proposal from the application,
-	// either 'accepted' or 'rejected'.
+	//metrics:Total number of proposals created by the node since process start.
 	ProposalCreateCount metrics.Counter
 
 	// RoundVotingPowerPercent is the percentage of the total voting power received
 	// with a round. The value begins at 0 for each round and approaches 1.0 as
 	// additional voting power is observed. The metric is labeled by vote type.
+	//metrics:A value between 0 and 1.0 representing the percentage of the total voting power per vote type received within a round.
 	RoundVotingPowerPercent metrics.Gauge `metrics_labels:"vote_type"`
 
 	// LateVotes stores the number of votes that were received by this node that
 	// correspond to earlier heights and rounds than this node is currently
 	// in.
+	//metrics:Number of votes received by the node since process start that correspond to earlier heights and rounds than this node is currently in.
 	LateVotes metrics.Counter `metrics_labels:"vote_type"`
 }
 
