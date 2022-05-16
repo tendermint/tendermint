19cc03f6fd90081fae8dd44debe0e12b516b34d8
William Banfield <wbanfield@gmail.com>
2022-05-16 11:38:23 -0400

    addr -> myAddr

--- internal/consensus/state.go
+++ internal/consensus/state.go
@@ -2383,7 +2383,7 @@ func (cs *State) addVote(
 	// Verify VoteExtension if precommit and not nil
 	// https://github.com/tendermint/tendermint/issues/8487
 	if vote.Type == tmproto.PrecommitType && !vote.BlockID.IsNil() &&
-		!bytes.Equal(vote.ValidatorAddress, addr) {
+		!bytes.Equal(vote.ValidatorAddress, myAddr) {
 		// The core fields of the vote message were already validated in the
 		// consensus reactor when the vote was received.
 		// Here, we valdiate that the vote extension was included in the vote
