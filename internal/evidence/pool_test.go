65c4150c3db0f2cacc358144ec0b448fba398e22
William Banfield <wbanfield@gmail.com>
2022-05-19 13:30:11 -0400

    fix evidence test by adding extension sig

--- internal/evidence/pool_test.go
+++ internal/evidence/pool_test.go
@@ -596,6 +596,7 @@ func makeExtCommit(height int64, valAddr []byte) *types.ExtendedCommit {
 				Timestamp:        defaultEvidenceTime,
 				Signature:        []byte("Signature"),
 			},
+			ExtensionSignature: []byte("Extended Signature"),
 		}},
 	}
 }
