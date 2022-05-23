16e4a19691dad86c5ba998ddbcc2cf1ebf59c25c
William Banfield <wbanfield@gmail.com>
2022-05-23 08:59:50 -0400

    add initial height as parameter of e2e

--- test/e2e/runner/setup.go
+++ test/e2e/runner/setup.go
@@ -209,7 +209,7 @@ func MakeGenesis(testnet *e2e.Testnet) (types.GenesisDoc, error) {
 	}
 	genesis.ConsensusParams.Evidence.MaxAgeNumBlocks = e2e.EvidenceAgeHeight
 	genesis.ConsensusParams.Evidence.MaxAgeDuration = e2e.EvidenceAgeTime
-	genesis.ConsensusParams.ABCI.VoteExtensionsEnableHeight = testnet.InitialHeight + 10
+	genesis.ConsensusParams.ABCI.VoteExtensionsEnableHeight = testnet.VoteExtensionsEnableHeight
 	for validator, power := range testnet.Validators {
 		genesis.Validators = append(genesis.Validators, types.GenesisValidator{
 			Name:    validator.Name,
