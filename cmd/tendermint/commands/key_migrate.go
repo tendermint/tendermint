3b754ebb0ebbc9ca3a16630b3f62888839234b37
tycho garen <garen@tychoish.com>
2022-05-25 11:05:04 -0400

    migrate: provide function for database production

--- cmd/tendermint/commands/key_migrate.go
+++ cmd/tendermint/commands/key_migrate.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/tendermint/tendermint/config"
 	cfg "github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/libs/log"
 	"github.com/tendermint/tendermint/scripts/keymigrate"
@@ -17,54 +18,7 @@ func MakeKeyMigrateCommand(conf *cfg.Config, logger log.Logger) *cobra.Command {
 		Use:   "key-migrate",
 		Short: "Run Database key migration",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			ctx, cancel := context.WithCancel(cmd.Context())
-			defer cancel()
-
-			contexts := []string{
-				// this is ordered to put
-				// the more ephemeral tables first to
-				// forclose the possiblity of the
-				// ephemeral data overwriting later data
-				"tx_index",
-				"peerstore",
-				"light",
-				"blockstore",
-				"state",
-				"evidence",
-			}
-
-			for idx, dbctx := range contexts {
-				logger.Info("beginning a key migration",
-					"dbctx", dbctx,
-					"num", idx+1,
-					"total", len(contexts),
-				)
-
-				db, err := cfg.DefaultDBProvider(&cfg.DBContext{
-					ID:     dbctx,
-					Config: conf,
-				})
-
-				if err != nil {
-					return fmt.Errorf("constructing database handle: %w", err)
-				}
-
-				if err = keymigrate.Migrate(ctx, db); err != nil {
-					return fmt.Errorf("running migration for context %q: %w",
-						dbctx, err)
-				}
-
-				if dbctx == "blockstore" {
-					if err := scmigrate.Migrate(ctx, db); err != nil {
-						return fmt.Errorf("running seen commit migration: %w", err)
-
-					}
-				}
-			}
-
-			logger.Info("completed database migration successfully")
-
-			return nil
+			return RunDatabaseMigration(cmd.Context(), logger, conf)
 		},
 	}
 
@@ -72,4 +26,52 @@ func MakeKeyMigrateCommand(conf *cfg.Config, logger log.Logger) *cobra.Command {
 	addDBFlags(cmd, conf)
 
 	return cmd
+}
+
+func RunDatabaseMigration(ctx context.Context, logger log.Logger, conf *config.Config) error {
+	contexts := []string{
+		// this is ordered to put
+		// the more ephemeral tables first to
+		// forclose the possiblity of the
+		// ephemeral data overwriting later data
+		"tx_index",
+		"peerstore",
+		"light",
+		"blockstore",
+		"state",
+		"evidence",
+	}
+
+	for idx, dbctx := range contexts {
+		logger.Info("beginning a key migration",
+			"dbctx", dbctx,
+			"num", idx+1,
+			"total", len(contexts),
+		)
+
+		db, err := cfg.DefaultDBProvider(&cfg.DBContext{
+			ID:     dbctx,
+			Config: conf,
+		})
+
+		if err != nil {
+			return fmt.Errorf("constructing database handle: %w", err)
+		}
+
+		if err = keymigrate.Migrate(ctx, db); err != nil {
+			return fmt.Errorf("running migration for context %q: %w",
+				dbctx, err)
+		}
+
+		if dbctx == "blockstore" {
+			if err := scmigrate.Migrate(ctx, db); err != nil {
+				return fmt.Errorf("running seen commit migration: %w", err)
+
+			}
+		}
+	}
+
+	logger.Info("completed database migration successfully")
+
+	return nil
 }
