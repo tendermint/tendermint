63fa033dfaac5a17bc1b5f9e8ed385aec062d3a4
evan-forbes <evan.samuel.forbes@gmail.com>
2022-06-02 18:54:33 -0500

    add upgrade connections to max connections for max peers

--- node/setup.go
+++ node/setup.go
@@ -223,11 +223,13 @@ func createPeerManager(
 		maxConns = 64
 	}
 
+	maxUpgradeConns := uint16(4)
+
 	options := p2p.PeerManagerOptions{
 		SelfAddress:            selfAddr,
 		MaxConnected:           maxConns,
-		MaxConnectedUpgrade:    4,
-		MaxPeers:               maxConns,
+		MaxConnectedUpgrade:    maxUpgradeConns,
+		MaxPeers:               maxConns + maxUpgradeConns,
 		MinRetryTime:           250 * time.Millisecond,
 		MaxRetryTime:           30 * time.Minute,
 		MaxRetryTimePersistent: 5 * time.Minute,
