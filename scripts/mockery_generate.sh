b8e204e8e66bcabc5f13bedf63bde49a069405fc
M. J. Fromberger <fromberger@interchain.io>
2022-06-03 14:50:52 -0700

    Fix a comment typo.

--- scripts/mockery_generate.sh
+++ scripts/mockery_generate.sh
@@ -2,9 +2,9 @@
 #
 # Invoke Mockery v2 to update generated mocks for the given type.
 #
-# This script runs a locall-installed "mockery" if available, otherwise it runs
-# the published Docker container. This legerdemain is so that the CI build and
-# a local build can work off the same script.
+# This script runs a locally-installed "mockery" if available, otherwise it
+# runs the published Docker container. This legerdemain is so that the CI build
+# and a local build can work off the same script.
 #
 if ! which mockery ; then
   mockery() {
