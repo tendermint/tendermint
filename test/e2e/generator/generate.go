82cf5094f63e82f41fa3ea4fd05b3980967b8524
Thane Thomson <connect@thanethomson.com>
2022-06-14 16:43:28 -0400

    Remove staticcheck lint directive
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- test/e2e/generator/generate.go
+++ test/e2e/generator/generate.go
@@ -154,7 +154,7 @@ func generateTestnet(r *rand.Rand, opt map[string]interface{}) (e2e.Manifest, er
 		numValidators = 4
 	case "large":
 		// FIXME Networks are kept small since large ones use too much CPU.
-		numSeeds = r.Intn(1) // nolint: staticcheck
+		numSeeds = r.Intn(1)
 		numLightClients = r.Intn(2)
 		numValidators = 4 + r.Intn(4)
 		numFulls = r.Intn(4)
