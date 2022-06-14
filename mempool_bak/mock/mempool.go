70bff850651067d16163bc54c11c1687b9986432
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-06-14 13:47:50 -0400

    updates

--- mempool_bak/mock/mempool.go
+++ mempool_bak/mock/mempool.go
@@ -1,34 +1,32 @@
 package mock
 
 import (
-	"context"
-
 	abci "github.com/tendermint/tendermint/abci/types"
-	"github.com/tendermint/tendermint/internal/libs/clist"
-	"github.com/tendermint/tendermint/internal/mempool"
+	"github.com/tendermint/tendermint/libs/clist"
+	mempl "github.com/tendermint/tendermint/mempool"
 	"github.com/tendermint/tendermint/types"
 )
 
 // Mempool is an empty implementation of a Mempool, useful for testing.
 type Mempool struct{}
 
-var _ mempool.Mempool = Mempool{}
+var _ mempl.Mempool = Mempool{}
 
-func (Mempool) Lock()     {}
-func (Mempool) Unlock()   {}
-func (Mempool) Size() int { return 0 }
-func (Mempool) CheckTx(_ context.Context, _ types.Tx, _ func(*abci.Response), _ mempool.TxInfo) error {
+func (Mempool) Lock()            {}
+func (Mempool) Unlock()          {}
+func (Mempool) Size() int        { return 0 }
+func (Mempool) SizeBytes() int64 { return 0 }
+func (Mempool) CheckTx(_ types.Tx, _ func(*abci.Response), _ mempl.TxInfo) error {
 	return nil
 }
-func (Mempool) RemoveTxByKey(txKey types.TxKey) error   { return nil }
 func (Mempool) ReapMaxBytesMaxGas(_, _ int64) types.Txs { return types.Txs{} }
 func (Mempool) ReapMaxTxs(n int) types.Txs              { return types.Txs{} }
 func (Mempool) Update(
 	_ int64,
 	_ types.Txs,
 	_ []*abci.ResponseDeliverTx,
-	_ mempool.PreCheckFunc,
-	_ mempool.PostCheckFunc,
+	_ mempl.PreCheckFunc,
+	_ mempl.PostCheckFunc,
 ) error {
 	return nil
 }
@@ -36,7 +34,7 @@ func (Mempool) Flush()                        {}
 func (Mempool) FlushAppConn() error           { return nil }
 func (Mempool) TxsAvailable() <-chan struct{} { return make(chan struct{}) }
 func (Mempool) EnableTxsAvailable()           {}
-func (Mempool) SizeBytes() int64              { return 0 }
+func (Mempool) TxsBytes() int64               { return 0 }
 
 func (Mempool) TxsFront() *clist.CElement    { return nil }
 func (Mempool) TxsWaitChan() <-chan struct{} { return nil }
