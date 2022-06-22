1d9089b423be29299dd468bb3906c5d37a8eede7
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2022-06-22 17:57:46 +0200

    Fix fuzz test for priority mempool

--- test/fuzz/mempool/v1/checktx.go
+++ test/fuzz/mempool/v1/checktx.go
@@ -5,9 +5,9 @@ import (
 	"github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/libs/log"
 	mempl "github.com/tendermint/tendermint/mempool"
+	"github.com/tendermint/tendermint/proxy"
 
 	mempoolv1 "github.com/tendermint/tendermint/mempool/v1"
-	"github.com/tendermint/tendermint/proxy"
 )
 
 var mempool mempl.Mempool
@@ -20,15 +20,14 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-
 	cfg := config.DefaultMempoolConfig()
 	cfg.Broadcast = false
-
-	mempool = mempoolv1.NewTxMempool(log.TestingLogger().With("module", "mempool", "fuzz"), cfg, appConnMem, 0)
-
+	log := log.NewNopLogger()
+	mempool = mempoolv1.NewTxMempool(log, cfg, appConnMem, 0)
 }
 
 func Fuzz(data []byte) int {
+
 	err := mempool.CheckTx(data, nil, mempl.TxInfo{})
 	if err != nil {
 		return 0
