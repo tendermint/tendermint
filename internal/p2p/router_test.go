8ec9cf065a77f5b36ee98a84be3f7085ccb58d76
Sam Kleinman <garen@tychoish.com>
2022-06-24 13:57:49 -0400

    p2p: remove dial sleep and provide disconnect cooldown (#8839)
    Alternative proposal for #8826
    
    (cherry picked from commit 52b6dc19badf50938ae2b2a1d2e22813614e5ad5)
    
    # Conflicts:
    #	internal/p2p/router.go

--- internal/p2p/router_test.go
+++ internal/p2p/router_test.go
@@ -715,7 +715,6 @@ func TestRouter_DialPeers_Parallel(t *testing.T) {
 		mockTransport,
 		nil,
 		p2p.RouterOptions{
-			DialSleep: func(_ context.Context) {},
 			NumConcurrentDials: func() int {
 				ncpu := runtime.NumCPU()
 				if ncpu <= 3 {
