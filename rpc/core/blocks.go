bca737c2d3e37dd6f89f0a350779c581c5e3869b
Thane Thomson <connect@thanethomson.com>
2022-08-17 21:27:49 -0400

    docs: Minor recommendations prior to v0.34.21 release (#9267)
    * Make reindex-event cmd docs consistent with other commands
    
    Signed-off-by: Thane Thomson <connect@thanethomson.com>
    
    * Add warning regarding DiscardABCIResponses to BlockResults Go API
    
    Signed-off-by: Thane Thomson <connect@thanethomson.com>
    
    * Update OpenAPI spec to reflect discard_abci_responses change
    
    Signed-off-by: Thane Thomson <connect@thanethomson.com>
    
    * Add release highlights to CHANGELOG_PENDING
    
    Signed-off-by: Thane Thomson <connect@thanethomson.com>
    
    * Add pending changelog entry for #9033
    
    Signed-off-by: Thane Thomson <connect@thanethomson.com>
    
    * Format pending changelog entries consistently
    
    Signed-off-by: Thane Thomson <connect@thanethomson.com>
    
    * Correct and simplify comment wording
    
    Signed-off-by: Thane Thomson <connect@thanethomson.com>
    
    * Remove changelog entry regarding storage section
    
    Signed-off-by: Thane Thomson <connect@thanethomson.com>
    
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- rpc/core/blocks.go
+++ rpc/core/blocks.go
@@ -133,6 +133,7 @@ func Commit(ctx *rpctypes.Context, heightPtr *int64) (*ctypes.ResultCommit, erro
 
 // BlockResults gets ABCIResults at a given height.
 // If no height is provided, it will fetch results for the latest block.
+// When DiscardABCIResponses is enabled, an error will be returned.
 //
 // Results are for the height of the block containing the txs.
 // Thus response.results.deliver_tx[5] is the results of executing
