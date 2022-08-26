7ec3fa01696278769f3b3cb5831a153573c52127
mmsqe <mavis@crypto.com>
2022-08-26 11:00:08 +0800

    fix event type

--- internal/eventlog/item.go
+++ internal/eventlog/item.go
@@ -37,18 +37,18 @@ type ABCIEventer interface {
 type Item struct {
 	Cursor cursor.Cursor
 	Type   string
-	Data   types.EventData
+	Data   types.TMEventData
 	Events []abci.Event
 }
 
 // newItem constructs a new item with the specified cursor, type, and data.
-func newItem(cursor cursor.Cursor, etype string, data types.EventData) *Item {
+func newItem(cursor cursor.Cursor, etype string, data types.TMEventData) *Item {
 	return &Item{Cursor: cursor, Type: etype, Data: data, Events: makeEvents(etype, data)}
 }
 
 // makeEvents returns a slice of ABCI events comprising the type tag along with
 // any internal events exported by the data value.
-func makeEvents(etype string, data types.EventData) []abci.Event {
+func makeEvents(etype string, data types.TMEventData) []abci.Event {
 	base := []abci.Event{{
 		Type: tmTypeTag,
 		Attributes: []abci.EventAttribute{{
