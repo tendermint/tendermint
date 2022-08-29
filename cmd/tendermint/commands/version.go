a447db24a92b92ce2db0da0d975b2451d3aafdb2
Marko Baricevic <markobaricevic3778@gmail.com>
2022-08-29 20:48:03 +0200

    print all versions

--- cmd/tendermint/commands/version.go
+++ cmd/tendermint/commands/version.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -11,8 +12,20 @@ import (
 // VersionCmd ...
 var VersionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Show version info",
+	Short: "Show protocols' and libraries' version numbers",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version.TMCoreSemVer)
+		values, _ := json.MarshalIndent(struct {
+			Tendermint    string
+			ABCI          string
+			BlockProtocol uint64
+			P2PProtocol   uint64
+		}{
+			Tendermint:    version.TMCoreSemVer,
+			ABCI:          version.ABCIVersion,
+			BlockProtocol: version.BlockProtocol,
+			P2PProtocol:   version.P2PProtocol,
+		}, "", "  ")
+
+		fmt.Println(string(values))
 	},
 }
