e25d40f6b038e16890b4eb859cd37e7d2cf79aec
Thane Thomson <connect@thanethomson.com>
2022-08-30 20:14:37 +0000

    abci: Set version to 1.0.0
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- version/version.go
+++ version/version.go
@@ -1,15 +1,13 @@
 package version
 
-var (
-	TMCoreSemVer = TMVersionDefault
-)
+var TMCoreSemVer = TMVersionDefault
 
 const (
 	// TMVersionDefault is the used as the fallback version of Tendermint Core
 	// when not using git describe. It is formatted with semantic versioning.
 	TMVersionDefault = "0.34.20"
-	// ABCISemVer is the semantic version of the ABCI library
-	ABCISemVer = "0.18.0"
+	// ABCISemVer is the semantic version of the ABCI protocol
+	ABCISemVer = "1.0.0"
 
 	ABCIVersion = ABCISemVer
 )
