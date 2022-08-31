8960a198225bbe1fe79557b3601715ace98678ba
William Banfield <wbanfield@gmail.com>
2022-08-31 16:00:33 -0400

    add basic report main

---
+++ test/loadtime/cmd/report/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+
+	"github.com/tendermint/tendermint/store"
+	"github.com/tendermint/tendermint/test/loadtime/report"
+	dbm "github.com/tendermint/tm-db"
+)
+
+func main() {
+	// parse out the db location
+	db, err := dbm.NewDB("blockstore", dbm.GoLevelDBBackend, "/home/william/.tendermint/data")
+	if err != nil {
+		panic(err)
+	}
+	s := store.NewBlockStore(db)
+	r, err := report.GenerateFromBlockStore(s)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(r.ErrorCount)
+	fmt.Println(len(r.All))
+	fmt.Println(r.Min)
+	fmt.Println(r.Max)
+	fmt.Println(r.Avg)
+	fmt.Println(int64(time.Duration(math.MaxInt64) / (2 * time.Second)))
+}
