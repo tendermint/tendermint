01557b6f2d7e45f1a1bf9ea7f1b109277f6cff51
Sergio Mena <sergio@informal.systems>
2022-09-02 09:50:09 +0200

    Update Tendermint version

--- version/version.go
+++ version/version.go
@@ -7,9 +7,9 @@ var (
 const (
 	// TMVersionDefault is the used as the fallback version of Tendermint Core
 	// when not using git describe. It is formatted with semantic versioning.
-	TMVersionDefault = "0.34.20"
-	// ABCISemVer is the semantic version of the ABCI library
-	ABCISemVer = "0.18.0"
+	TMVersionDefault = "0.37.0"
+	// ABCISemVer is the semantic version of the ABCI protocol
+	ABCISemVer = "1.0.0"
 
 	ABCIVersion = ABCISemVer
 )
