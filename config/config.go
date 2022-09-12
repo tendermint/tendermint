51353c6eebe3b9f2d746e390aad61cc45dabf3e2
Callum Waters <cmwaters19@gmail.com>
2022-09-12 13:53:31 +0200

    gofmt

--- config/config.go
+++ config/config.go
@@ -66,7 +66,6 @@ var (
 
 	// taken from https://semver.org/
 	semverRegexp = regexp.MustCompile(`^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`)
-
 )
 
 // Config defines the top level configuration for a Tendermint node
