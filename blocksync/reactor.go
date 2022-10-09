6d19a2e3e6f071ec93f0b363cf1e09cc6e910865
Sergio Mena <sergio@informal.systems>
2022-10-09 20:15:46 +0200

    Revert "TO REVERT: reduce queue capacity"
    This reverts commit dd0fee56924c958bed2ab7733e1917eb88fb5957.

--- blocksync/reactor.go
+++ blocksync/reactor.go
@@ -140,7 +140,7 @@ func (bcR *Reactor) GetChannels() []*p2p.ChannelDescriptor {
 		{
 			ID:                  BlocksyncChannel,
 			Priority:            5,
-			SendQueueCapacity:   30,
+			SendQueueCapacity:   1000,
 			RecvBufferCapacity:  50 * 4096,
 			RecvMessageCapacity: MaxMsgSize,
 		},
