810b9c613b3cb837658a01b440c5ddd406eda9f2
William Banfield <wbanfield@gmail.com>
2022-10-18 15:40:39 -0400

    return error from infrastructure from data file function

--- test/e2e/runner/main.go
+++ test/e2e/runner/main.go
@@ -69,6 +69,9 @@ func NewCLI() *CLI {
 					return errors.New("'--infrastructure-data' must be set when using the 'digital-ocean' infrastructure-type")
 				}
 				ifd, err = e2e.InfrastructureDataFromFile(p)
+				if err != nil {
+					return err
+				}
 			default:
 				return fmt.Errorf("unknown infrastructure type '%s'", t)
 			}
