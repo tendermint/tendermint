24c1f44085953d9b4d47b091c69e35d4a51376a7
William Banfield <wbanfield@gmail.com>
2022-10-20 16:51:12 -0400

    mempool implements wrap / unwrap

--- proto/tendermint/mempool/message.go
+++ proto/tendermint/mempool/message.go
@@ -3,20 +3,17 @@ package mempool
 import (
 	"fmt"
 
-	"github.com/gogo/protobuf/proto"
+	"github.com/cosmos/gogoproto/proto"
+	"github.com/tendermint/tendermint/p2p"
 )
 
+var _ p2p.Wrapper = &Message_Txs{}
+
 // Wrap implements the p2p Wrapper interface and wraps a mempool message.
-func (m *Message) Wrap(pb proto.Message) error {
-	switch msg := pb.(type) {
-	case *Txs:
-		m.Sum = &Message_Txs{Txs: msg}
-
-	default:
-		return fmt.Errorf("unknown message: %T", msg)
-	}
-
-	return nil
+func (m *Message_Txs) Wrap() (proto.Message, error) {
+	return &Message{
+		Sum: m,
+	}, nil
 }
 
 // Unwrap implements the p2p Wrapper interface and unwraps a wrapped mempool
