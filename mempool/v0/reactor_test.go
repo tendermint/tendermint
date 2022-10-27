95d0c310a298e42a66587f9fa286307077ceb40b
William Banfield <wbanfield@gmail.com>
2022-10-27 14:17:52 -0400

    no lint errcheck

--- mempool/v0/reactor_test.go
+++ mempool/v0/reactor_test.go
@@ -279,7 +279,7 @@ func TestDontExhaustMaxActiveIDs(t *testing.T) {
 
 	for i := 0; i < mempool.MaxActiveIDs+1; i++ {
 		peer := mock.NewPeer(nil)
-		defer peer.Stop()
+		defer peer.Stop() //nolint: errcheck
 		reactor.Receive(p2p.Envelope{
 			ChannelID: mempool.MempoolChannel,
 			Src:       peer,
