5651a21adb42f8f9e409806b07fc00c0996a09b7
Callum Waters <cmwaters19@gmail.com>
2022-11-18 16:54:32 +0100

    add notes for testing

--- abci/example/orderbook/app_test.go
+++ abci/example/orderbook/app_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/tendermint/tendermint/abci/types"
 )
 
+// TODO: we should also check that CheckTx adds bids and asks to the app-side mempool
 func TestCheckTx(t *testing.T) {
 	app, err := orderbook.New(dbm.NewMemDB())
 	require.NoError(t, err)
@@ -70,14 +71,29 @@ func TestCheckTx(t *testing.T) {
 	}
 }
 
+// TODO: we should check that transactions in
+// a market are being validated and added to the proposal
+// and that other transactions get in
 // func TestPrepareProposal(t *testing.T) {
 // 	app := orderbook.New(dbm.NewMemDB())
 // }
 
+// TODO: we should test that transactions are
+// always valid i.e. ValidateTx. We could potentially 
+// combine this with PrepareProposal
 // func TestProcessProposal(t *testing.T) {
 // 	app := orderbook.New(dbm.NewMemDB())
 // }
 
+// TODO: we should test that a matched order
+// correctly updates the accounts. We should
+// also test that committing a block persists
+// it to the database and that we can now
+// query the new state
 // func TestFinalizeBlock(t *testing.T) {
 // 	app := orderbook.New(dbm.NewMemDB())
 // }
+
+// TODO: test that we can start from new
+// and from existing state
+// func TestNewStateMachine(t *testing.T) {}
