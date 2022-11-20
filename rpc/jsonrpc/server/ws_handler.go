3711ea4a922fb85eaa0ed647031bff0eea86f5e6
Adi Seredinschi <a@seredinschi.net>
2022-11-20 15:39:14 +0100

    Remove useless whitespace in Websocket output (#9720)
    * First try at #9696
    
    * Brief explanation
    
    * Removed all prettified JSON RPC responses
    
    * Fixes for failing tests.
    
    Adapted the assertions in
    - TestWriteRPCResponseHTTP
    - TestWriteRPCResponseHTTPError
    to work with non-pretty JSON-RPC output
    
    * Added changelog pending entry
    
    * Update CHANGELOG_PENDING.md
    
    Co-authored-by: Thane Thomson <connect@thanethomson.com>

--- rpc/jsonrpc/server/ws_handler.go
+++ rpc/jsonrpc/server/ws_handler.go
@@ -431,7 +431,10 @@ func (wsc *wsConnection) writeRoutine() {
 				return
 			}
 		case msg := <-wsc.writeChan:
-			jsonBytes, err := json.MarshalIndent(msg, "", "  ")
+			// Use json.MarshalIndent instead of Marshal for pretty output.
+			// Pretty output not necessary, since most consumers of WS events are
+			// automated processes, not humans.
+			jsonBytes, err := json.Marshal(msg)
 			if err != nil {
 				wsc.Logger.Error("Failed to marshal RPCResponse to JSON", "err", err)
 				continue
