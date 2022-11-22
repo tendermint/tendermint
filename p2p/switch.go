a97a7b9cc383ae65ca05321f5059aa186c4fde00
Jacob Gadikian <jacobgadikian@gmail.com>
2022-11-22 17:34:55 +0700

    make format

--- p2p/switch.go
+++ p2p/switch.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/cosmos/gogoproto/proto"
+
 	"github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/libs/cmap"
 	"github.com/tendermint/tendermint/libs/rand"
