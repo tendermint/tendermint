85ff1f2211a4be35ee477eac09dbd29f9793c2ea
William Banfield <wbanfield@gmail.com>
2022-11-29 13:15:42 -0500

    shim in start and stop methods

--- test/e2e/pkg/infra/provider.go
+++ test/e2e/pkg/infra/provider.go
@@ -1,5 +1,11 @@
 package infra
 
+import (
+	"context"
+
+	e2e "github.com/tendermint/tendermint/test/e2e/pkg"
+)
+
 // Provider defines an API for manipulating the infrastructure of a
 // specific set of testnet infrastructure.
 type Provider interface {
@@ -7,6 +13,9 @@ type Provider interface {
 	// Setup generates any necessary configuration for the infrastructure
 	// provider during testnet setup.
 	Setup() error
+
+	StartNode(context.Context, *e2e.Node) error
+	StopNode(context.Context, *e2e.Node) error
 }
 
 // NoopProvider implements the provider interface by performing noops for every
@@ -15,6 +24,8 @@ type Provider interface {
 type NoopProvider struct {
 }
 
-func (NoopProvider) Setup() error { return nil }
+func (NoopProvider) Setup() error                                   { return nil }
+func (NoopProvider) StartNode(_ context.Context, _ *e2e.Node) error { return nil }
+func (NoopProvider) StopNode(_ context.Context, _ *e2e.Node) error  { return nil }
 
 var _ Provider = NoopProvider{}
