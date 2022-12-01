8778f655172502bb7dd1d3bbd16344735d612c71
William Banfield <wbanfield@gmail.com>
2022-12-01 13:10:21 -0500

    add facility to set version in e2e runner

---
+++ test/e2e/pkg/version.go
@@ -0,0 +1,11 @@
+package e2e
+
+// Set by the makefile
+var version string
+
+func Version() string {
+	if version == "" {
+		panic("version not set. Was this program not built using the e2e package's Makefile?")
+	}
+	return version
+}
