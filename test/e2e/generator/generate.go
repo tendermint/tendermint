916010bdb0b46e341a377aaf5cf42778aa91de85
Thane Thomson <connect@thanethomson.com>
2022-12-20 19:23:27 -0500

    e2e: Cater for "dev-" prefixes in tags
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- test/e2e/generator/generate.go
+++ test/e2e/generator/generate.go
@@ -358,6 +358,9 @@ func findLatestReleaseTag(baseVer string, tags []string) (string, error) {
 	}
 	var latestVer *semver.Version
 	for _, tag := range tags {
+		if !strings.HasPrefix(tag, "v") {
+			continue
+		}
 		curVer, err := semver.NewVersion(tag)
 		if err != nil {
 			return "", fmt.Errorf("failed to parse tag \"%s\" as semantic version: %v", tag, err)
