2409939837a4687f216d4ec94ab0a377dc0e4541
Brendan Chou <brendanchou@msn.com>
2022-12-22 18:23:36 -0500

    Change logger Info->Debug in mempool v1

--- mempool/v1/mempool.go
+++ mempool/v1/mempool.go
@@ -454,7 +454,7 @@ func (txmp *TxMempool) addNewTransaction(wtx *WrappedTx, checkTxRes *abci.Respon
 	}
 
 	if err != nil || checkTxRes.Code != abci.CodeTypeOK {
-		txmp.logger.Info(
+		txmp.logger.Debug(
 			"rejected bad transaction",
 			"priority", wtx.Priority(),
 			"tx", fmt.Sprintf("%X", wtx.tx.Hash()),
