8e358c3570c6a1a73ba56b1e5ccf6103aecb6fc7
jhernandezb <contact@jhernandez.me>
2023-01-31 10:45:41 -0600

    add peer gossip sleep

--- p2p/peer.go
+++ p2p/peer.go
@@ -424,15 +424,6 @@ func (p *peer) hasChannel(chID byte) bool {
 			return true
 		}
 	}
-	// NOTE: probably will want to remove this
-	// but could be helpful while the feature is new
-	p.Logger.Debug(
-		"Unknown channel for peer",
-		"channel",
-		chID,
-		"channels",
-		p.channels,
-	)
 	return false
 }
 
