f8dfd3650589a7c5f62635469c5074175a84c77a
kilavvy <140459108+kilavvy@users.noreply.github.com>
2025-02-02 22:07:37 +0100

    Update syncer.go

--- statesync/syncer.go
+++ statesync/syncer.go
@@ -491,7 +491,7 @@ func (s *syncer) verifyApp(snapshot *snapshot, appVersion uint64) error {
 	// sanity check that the app version in the block matches the application's own record
 	// of its version
 	if resp.AppVersion != appVersion {
-		// An error here most likely means that the app hasn't inplemented state sync
+		// An error here most likely means that the app hasn't implemented state sync
 		// or the Info call correctly
 		return fmt.Errorf("app version mismatch. Expected: %d, got: %d",
 			appVersion, resp.AppVersion)
